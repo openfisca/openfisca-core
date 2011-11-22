a613e17e8ca4347966911a371c0c5ad67694f6d7
Clement Schaff <clement.schaff@gmail.com>
2011-11-22 12:01:41 +0100

    updated .settings

--- src/core/systemsf.py
+++ src/core/systemsf.py
@@ -1,9 +1,26 @@
-# -*- coding: utf-8 -*-
-'''
-Created on 16 nov. 2011
+# -*- coding:utf-8 -*-
+# Copyright © 2011 Clément Schaff, Mahdi Ben Jelloul
 
-@author: Clem
-'''
+"""
+openFisca, Logiciel libre de simulation du système socio-fiscal français
+Copyright © 2011 Clément Schaff, Mahdi Ben Jelloul
+
+This file is part of openFisca.
+
+    openFisca is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    openFisca is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with openFisca.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from __future__ import division
 from core.datatable import DataTable, Column
 import numpy as np
