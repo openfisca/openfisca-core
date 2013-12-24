36f7ff48286217f4daa1dee241ce875fa7daa94f
Emmanuel Raviart <eraviart@easter-eggs.com>
2013-12-24 22:52:59 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# OpenFisca -- A versatile microsimulation software
+# By: OpenFisca Team <contact@openfisca.fr>
+#
+# Copyright (C) 2011, 2012, 2013 OpenFisca Team
+# https://github.com/openfisca
+#
+# This file is part of OpenFisca.
+#
+# OpenFisca is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# OpenFisca is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""OpenFisca -- a versatile microsimulation free software
+
+OpenFisca includes a framework to simulate any tax and social system.
+"""
+
+
+from setuptools import setup, find_packages
+
+
+classifiers = """\
+Development Status :: 2 - Pre-Alpha
+Environment :: Web Environment
+Intended Audience :: Information Technology
+License :: OSI Approved :: GNU Affero General Public License v3
+Operating System :: POSIX
+Programming Language :: Python
+Topic :: Scientific/Engineering :: Information Analysis
+"""
+
+doc_lines = __doc__.split('\n')
+
+
+setup(
+    name = 'OpenFisca-Core',
+    version = '0.2dev',
+
+    author = 'OpenFisca Team',
+    author_email = 'contact@openfisca.fr',
+    classifiers = [classifier for classifier in classifiers.split('\n') if classifier],
+    description = doc_lines[0],
+    keywords = 'microsimulation social tax',
+    license = 'http://www.fsf.org/licensing/licenses/agpl-3.0.html',
+    long_description = '\n'.join(doc_lines[2:]),
+    url = 'https://github.com/openfisca/openfisca-core',
+
+    data_files = [
+        ('share/locale/fr/LC_MESSAGES', ['openfisca_core/i18n/fr/LC_MESSAGES/openfisca_core.mo']),
+        ],
+    install_requires = [
+        "Babel >= 0.9.4",
+        "numpy",
+        "pandas",
+        ],
+    message_extractors = {
+        'openfisca_core': [
+            ('**.py', 'python', None),
+            ],
+        },
+#    package_data = {'openfisca_core': ['i18n/*/LC_MESSAGES/*.mo']},
+    packages = find_packages(),
+    zip_safe = False,
+    )
