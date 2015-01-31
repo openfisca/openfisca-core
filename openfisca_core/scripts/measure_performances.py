148079d3e5addc752737a3dca133e88ea6940256
Emmanuel Raviart <emmanuel@raviart.com>
2015-01-31 14:55:19 +0100

    Remove useless method iter_member_persons_role_and_id from "measure_performance" script.

--- openfisca_core/scripts/measure_performances.py
+++ openfisca_core/scripts/measure_performances.py
@@ -83,22 +83,6 @@ class Familles(AbstractEntity):
         }
     symbol = 'fam'
 
-    def iter_member_persons_role_and_id(self, member):
-        role = 0
-
-        parents_id = member['parents']
-        assert 1 <= len(parents_id) <= 2
-        for parent_role, parent_id in enumerate(parents_id, role):
-            assert parent_id is not None
-            yield parent_role, parent_id
-        role += 2
-
-        enfants_id = member.get('enfants')
-        if enfants_id is not None:
-            for enfant_role, enfant_id in enumerate(enfants_id, role):
-                assert enfant_id is not None
-                yield enfant_role, enfant_id
-
 
 class Individus(AbstractEntity):
     column_by_name = collections.OrderedDict()
