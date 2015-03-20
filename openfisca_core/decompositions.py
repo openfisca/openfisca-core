b6995cd8f9c33ca1623e98949c3d51ad5a0d3047
Christophe Benz <christophe.benz@data.gouv.fr>
2015-03-20 14:47:46 +0100

    Refactor decompositions.calculate to allow multiple simulations

--- openfisca_core/decompositions.py
+++ openfisca_core/decompositions.py
@@ -32,19 +32,13 @@ from . import conv
 from . import decompositionsxml
 
 
-def calculate(simulation, decomposition_json = None, period = None):
-    assert simulation is not None
-    tax_benefit_system = simulation.tax_benefit_system
-    if decomposition_json is None:
-        xml_file_path = os.path.join(
-            tax_benefit_system.DECOMP_DIR,
-            "decomp.xml"
-            )
-        decomposition_json = get_decomposition_json(xml_file_path, tax_benefit_system)
+def calculate(simulations, decomposition_json):
+    assert decomposition_json is not None
 
     for node in iter_decomposition_nodes(decomposition_json):
-        if not node.get('children'):
-            simulation.calculate(node['code'], period = period)
+        if node.get('children') is None:
+            for simulation in simulations:
+                simulation.calculate_add(node['code'])
 
     response_json = copy.deepcopy(decomposition_json)  # Use decomposition as a skeleton for response.
     for node in iter_decomposition_nodes(response_json, children_first = True):
@@ -56,15 +50,13 @@ def calculate(simulation, decomposition_json = None, period = None):
                 ))
         else:
             node['values'] = values = []
-            holder = simulation.get_holder(node['code'])
-            column = holder.column
-            values.extend(
-                column.transform_value_to_json(value)
-                for value in holder.new_test_case_array(simulation.period).tolist()
-                )
-        column = tax_benefit_system.column_by_name.get(node['code'])
-        if column is not None and column.url is not None:
-            node['url'] = column.url
+            for simulation in simulations:
+                holder = simulation.get_holder(node['code'])
+                column = holder.column
+                values.extend(
+                    column.transform_value_to_json(value)
+                    for value in holder.new_test_case_array(simulation.period).tolist()
+                    )
     return response_json
 
 
