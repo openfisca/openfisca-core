2b2f19a27e7b3bae9abcf0efa8df8cdc590cdde6
Emmanuel Raviart <emmanuel@raviart.com>
2015-03-23 09:55:55 +0100

    Modify assert_near to work when comparing 0 with relative_error_margin.

--- openfisca_core/tools.py
+++ openfisca_core/tools.py
@@ -50,28 +50,21 @@ def assert_near(value, target_value, absolute_error_margin = 0, message = '', re
     if isinstance(message, unicode):
         message = message.encode('utf-8')
     if isinstance(value, np.ndarray):
-        if absolute_error_margin is not None and absolute_error_margin <= 0 \
-                or relative_error_margin is not None and relative_error_margin <= 0:
-            assert (target_value == value).all(), '{}{} differs from {}'.format(message, value, target_value)
-        else:
-            if absolute_error_margin is not None:
-                assert (abs(target_value - value) < absolute_error_margin).all(), \
-                    '{}{} differs from {} with an absolute margin {} >= {}'.format(message, value, target_value,
-                        abs(target_value - value), absolute_error_margin)
-            if relative_error_margin is not None:
-                assert (abs(target_value - value) < abs(relative_error_margin * target_value)).all(), \
-                    '{}{} differs from {} with a relative margin {} >= {}'.format(message, value, target_value,
-                        abs(target_value - value), abs(relative_error_margin * target_value))
-    elif absolute_error_margin is not None and absolute_error_margin <= 0 \
-            or relative_error_margin is not None and relative_error_margin <= 0:
-        assert target_value == value, '{}{} differs from {}'.format(message, value, target_value)
-    else:
         if absolute_error_margin is not None:
-            assert abs(target_value - value) < absolute_error_margin, \
+            assert (abs(target_value - value) <= absolute_error_margin).all(), \
                 '{}{} differs from {} with an absolute margin {} >= {}'.format(message, value, target_value,
                     abs(target_value - value), absolute_error_margin)
         if relative_error_margin is not None:
-            assert abs(target_value - value) < abs(relative_error_margin * target_value), \
+            assert (abs(target_value - value) <= abs(relative_error_margin * target_value)).all(), \
+                '{}{} differs from {} with a relative margin {} >= {}'.format(message, value, target_value,
+                    abs(target_value - value), abs(relative_error_margin * target_value))
+    else:
+        if absolute_error_margin is not None:
+            assert abs(target_value - value) <= absolute_error_margin, \
+                '{}{} differs from {} with an absolute margin {} >= {}'.format(message, value, target_value,
+                    abs(target_value - value), absolute_error_margin)
+        if relative_error_margin is not None:
+            assert abs(target_value - value) <= abs(relative_error_margin * target_value), \
                 '{}{} differs from {} with a relative margin {} >= {}'.format(message, value, target_value,
                     abs(target_value - value), abs(relative_error_margin * target_value))
 
