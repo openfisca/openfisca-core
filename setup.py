31f82c9dd5739efd0b260e2a15126895060fffc3
Christophe Benz <christophe.benz@data.gouv.fr>
2015-09-07 18:00:43 +0200

    Release 0.5.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-Core',
-    version = '0.5.0.dev0',
+    version = '0.5.0',
 
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
