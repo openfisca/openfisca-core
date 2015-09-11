openfisca/openfisca-core

commit 2692e32df93646442400ba4f5e35f95ddba1b753

Update Makefile

Makefile
@@ -1,3 +1,5 @@
+TESTS_DIR="openfisca_core/tests/"
+
 all: flake8 test
 
 check-syntax-errors: clean-pyc
@@ -14,7 +16,7 @@ flake8: clean-pyc
 	flake8
 
 test: check-syntax-errors
-	nosetests -v --with-doctest openfisca_core/
+	nosetests -v --with-doctest $(TESTS_DIR)
 
 test-with-coverage:
-	nosetests -v openfisca_core/ --with-coverage --cover-package=openfisca_core --cover-erase --cover-branches --cover-html
+	nosetests -v $(TESTS_DIR) --with-coverage --cover-package=openfisca_core --cover-erase --cover-branches --cover-html

commit a2e1ce4ab4ac7b105342fd7eb913390b6f56b584

Use whole source dir as tests dir, for tests in doctrings (doctest)
bug introduced in 2692e32df93646442400ba4f5e35f95ddba1b753

Makefile
@@ -1,4 +1,4 @@
-TESTS_DIR=openfisca_core/tests
+TESTS_DIR=openfisca_core
 
 all: flake8 test
 