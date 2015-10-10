c5d291f94cf770c1dd2413e6babef6427a9973a2
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2015-10-10 14:27:17 +0200

    Add trim option to marginal rate computation

--- openfisca_core/rates.py
+++ openfisca_core/rates.py
@@ -25,12 +25,19 @@
 
 from __future__ import division
 
+import numpy
+
 
 def average_rate(target = None, varying = None):
     # target: numerator, varying: denominator
     return 1 - target / (varying * (varying != 0) + (varying == 0))
 
 
-def marginal_rate(target = None, varying = None):
+def marginal_rate(target = None, varying = None, trim = None):
     # target: numerator, varying: denominator
-    return 1 - (target[:-1] - target[1:]) / (varying[:-1] - varying[1:])
+    marginal_rate = 1 - (target[:-1] - target[1:]) / (varying[:-1] - varying[1:])
+    if trim is not None:
+        marginal_rate = numpy.where(marginal_rate <= max(trim), marginal_rate, numpy.nan)
+        marginal_rate = numpy.where(marginal_rate >= min(trim), marginal_rate, numpy.nan)
+
+    return marginal_rate
