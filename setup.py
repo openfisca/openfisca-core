0b12702f21bb57175e0e67a77f746ccd926e57eb
Christophe Benz <christophe.benz@data.gouv.fr>
2015-10-28 18:25:57 +0100

    Fix syntax

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     install_requires = [
         'Babel >= 0.9.4',
         'Biryani[datetimeconv] >= 0.10.4',
-        'numpy >= 1.6 < 1.10',
+        'numpy >= 1.6,< 1.10',
         ],
     message_extractors = {
         'openfisca_core': [
