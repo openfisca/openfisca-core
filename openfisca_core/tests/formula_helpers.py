0f4ca12e524be7cbd82ac79e81a62015b47ca6ef
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2015-12-04 23:05:22 +0100

    Add more tricky case test for apply_threshold

--- openfisca_core/tests/formula_helpers.py
+++ openfisca_core/tests/formula_helpers.py
@@ -26,3 +26,10 @@ def test_apply_threshold():
     outputs = [10, 15, 20]
     result = apply_threshold(input, thresholds, outputs)
     assert_near(result, [10, 10, 15, 15, 20])
+
+def test_apply_threshold_with_variable_threshold():
+    input = numpy.array([1000, 1000, 1000])
+    thresholds = [numpy.array([500, 1500, 1000])] # Only one thresold, but varies with the person
+    outputs = [True, False] # True if input <= threshold, false otherwise
+    result = apply_threshold(input, thresholds, outputs)
+    assert_near(result, [False, True, True])
