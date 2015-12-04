128dac22b7dd7aaa2a72dca45b13122b416cef0d
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2015-12-04 23:25:21 +0100

    Rename output -> choice_list in test

--- openfisca_core/tests/formula_helpers.py
+++ openfisca_core/tests/formula_helpers.py
@@ -10,26 +10,26 @@ from openfisca_core.tools import assert_near
 def test_apply_threshold_with_too_many_thresholds():
     input = numpy.array([10])
     thresholds = [5]
-    outputs = [10]
-    return apply_threshold(input, thresholds, outputs)
+    choice_list = [10]
+    return apply_threshold(input, thresholds, choice_list)
 
 @raises(AssertionError)
 def test_apply_threshold_with_too_few_thresholds():
     input = numpy.array([10])
     thresholds = [5]
-    outputs = [10, 15, 20]
-    return apply_threshold(input, thresholds, outputs)
+    choice_list = [10, 15, 20]
+    return apply_threshold(input, thresholds, choice_list)
 
 def test_apply_threshold():
     input = numpy.array([4, 5, 6, 7, 8])
     thresholds = [5, 7]
-    outputs = [10, 15, 20]
-    result = apply_threshold(input, thresholds, outputs)
+    choice_list = [10, 15, 20]
+    result = apply_threshold(input, thresholds, choice_list)
     assert_near(result, [10, 10, 15, 15, 20])
 
 def test_apply_threshold_with_variable_threshold():
     input = numpy.array([1000, 1000, 1000])
     thresholds = [numpy.array([500, 1500, 1000])] # Only one thresold, but varies with the person
-    outputs = [True, False] # True if input <= threshold, false otherwise
-    result = apply_threshold(input, thresholds, outputs)
+    choice_list = [True, False] # True if input <= threshold, false otherwise
+    result = apply_threshold(input, thresholds, choice_list)
     assert_near(result, [False, True, True])
