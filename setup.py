708bdd784032fda5433a312dd2727956f2878075
Christophe Benz <christophe.benz@data.gouv.fr>
2016-01-19 18:05:18 +0100

    Remove numpy < 1.10 constraint

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires = [
         'Babel >= 0.9.4',
         'Biryani[datetimeconv] >= 0.10.4',
-        'numpy >= 1.6,< 1.10',
+        'numpy >= 1.6',
         ],
     message_extractors = {
         'openfisca_core': [
