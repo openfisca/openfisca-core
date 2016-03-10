017a2fbba71d4a755b4af9f43c08a2bed7d6a985
Christophe Benz <christophe.benz@data.gouv.fr>
2016-03-10 14:03:53 +0100

    Release 0.5.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,8 @@
 # Changelog
 
-## 0.5.3.dev0 - next release
+## 0.5.3
 
-* TODO Fill this changes list while developing
+* Many updates
 
 ## 0.5.2
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-Core',
-    version = '0.5.3.dev0',
+    version = '0.5.3',
 
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
