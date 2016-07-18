4e666be3aec65a929266cc31287cfa1427b3e4d3
Christophe Benz <christophe.benz@data.gouv.fr>
2016-07-18 19:03:23 +0200

    Bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-Core',
-    version = '2.0.3',
+    version = '2.0.4',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
