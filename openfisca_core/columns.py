6fc4f9f1751b313047b2bb1779f1581f0864f006
Christophe Benz <christophe.benz@data.gouv.fr>
2016-10-24 16:57:22 +0200

    Enforce DateCol.default to be a date

--- openfisca_core/columns.py
+++ openfisca_core/columns.py
@@ -218,6 +218,13 @@ class DateCol(Column):
     json_type = 'Date'
     val_type = 'date'
 
+    def __init__(self, default = None, **kwargs):
+        super(DateCol, self).__init__(**kwargs)
+        if default is None:
+            default = datetime.date.fromtimestamp(0)  # 0 == 1970-01-01
+        assert isinstance(default, datetime.date), default
+        self.default = default
+
     @property
     def input_to_dated_python(self):
         return conv.pipe(
@@ -227,7 +234,7 @@ class DateCol(Column):
             )
 
     def json_default(self):
-        return unicode(np.array(self.default, self.dtype))  # 0 = 1970-01-01
+        return unicode(np.array(self.default, self.dtype))
 
     @property
     def json_to_dated_python(self):
