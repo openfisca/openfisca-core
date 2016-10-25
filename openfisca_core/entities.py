a6ac280443a06cd6c1462296aab9b2a65dbe555e
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-10-26 00:29:05 +0200

    Refactor entities

--- openfisca_core/entities.py
+++ openfisca_core/entities.py
@@ -1,50 +1,46 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
+import warnings
 
-from .tools import empty_clone
+from enumerations import Enum
 
 
-class AbstractEntity(object):
-    count = 0
-    key_plural = None
-    key_singular = None
-    index_for_person_variable_name = None  # Class attribute. Not used for persons
-    is_persons_entity = False  # Class attribute
+class Entity(object):
+    key = None
+    plural = None
+    label = None
+    roles = None
+    is_person = False
 
-    # Maximum number of different roles in the entity.
-    # Ex : If the biggest family have 7 members, roles_count = 7 for the entity family.
-    # Not relevant for a person
-    roles_count = None
+    def __init__(self, simulation):
+        self.simulation = simulation
+        self.count = 0
+        self.step_size = 0
 
-    role_for_person_variable_name = None  # Class attribute. Not used for persons
-    step_size = 0
-    simulation = None
-    symbol = None  # Class attribute. Must be overridden by subclasses.
+class PersonEntity(Entity):
+    is_person = True
 
-    def __init__(self, simulation = None):
-        if self.is_persons_entity:
-            assert self.index_for_person_variable_name is None
-            assert self.role_for_person_variable_name is None
+    # Projection person -> person
+
+    def role_in(self, entity):
+        entity = self.simulation.get_entity(entity)
+        return entity.members_role
+class GroupEntity(Entity):
+
+    @classmethod
+    def get_role_enum(cls):
+        return Enum(map(lambda role: role['key'], cls.roles))
+
+    def __init__(self, simulation):
+        Entity.__init__(self, simulation)
+        self.members_entity_id = None
+        self.members_role = None
+        self.members_legacy_role = None
+        self.members_position = None
         else:
-            assert self.index_for_person_variable_name is not None
-            assert self.role_for_person_variable_name is not None
-        if simulation is not None:
-            self.simulation = simulation
 
-    def clone(self, simulation):
-        """Copy the entity just enough to be able to run the simulation without modifying the original simulation."""
-        new = empty_clone(self)
-        new_dict = new.__dict__
 
-        for key, value in self.__dict__.iteritems():
-            if key != 'simulation':
-                new_dict[key] = value
 
-        new_dict['simulation'] = simulation
 
-        return new
 
-    def iter_member_persons_role_and_id(self, member):
-        assert not self.is_persons_entity
-        raise NotImplementedError('Method "iter_member_persons_role_and_id" is not implemented for class {}'.format(
-            self.__class__.__name__))
