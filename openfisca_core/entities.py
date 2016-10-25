264a000547609d53625416130d7a929c51cc3cc8
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-10-26 00:32:00 +0200

    Build entity conversion functions

--- openfisca_core/entities.py
+++ openfisca_core/entities.py
@@ -27,6 +27,11 @@ class Entity(object):
                 "Variable {} is not defined for {} but for {}".format(
                     variable_name, self.key, variable_entity.key)
                 )
+
+    def check_array_compatible_with_entity(self, array):
+        if not self.count == array.size:
+            raise Exception("Input {} is not a valid value for the entity {}".format(array, self.key))
+
     def calculate(self, variable_name, period = None):
         self.check_variable_defined_for_entity(variable_name)
         return self.simulation.calculate(variable_name, period)
@@ -42,6 +47,18 @@ class Entity(object):
     def calculate_add_divide(self, variable_name, period = None):
         self.check_variable_defined_for_entity(variable_name)
         return self.simulation.calculate_add_divide(variable_name, period)
+
+    # Helpers
+
+    def empty_array(self):
+        return np.zeros(self.count)
+
+    def filled_array(self, value):
+        with warnings.catch_warnings():  # Avoid a non-relevant warning
+            warnings.simplefilter("ignore")
+            return np.full(self.count, value)
+
+
 class PersonEntity(Entity):
     is_person = True
 
@@ -50,6 +67,18 @@ class PersonEntity(Entity):
     def role_in(self, entity):
         entity = self.simulation.get_entity(entity)
         return entity.members_role
+
+    def value_from_partner(self, array, entity, role):
+        # Make sure there is only two people with the role
+        self.check_array_compatible_with_entity(array)
+        entity_filter = entity.nb_persons(role) == 2
+        higher_value = entity_filter * entity.max(array, role)
+        lower_value = entity_filter * entity.min(array, role)
+        higher_value_i = entity.project(higher_value, role)
+        lower_value_i = entity.project(lower_value, role)
+        return (array == higher_value_i) * lower_value_i + (array == lower_value_i) * higher_value_i
+
+
 class GroupEntity(Entity):
 
     @classmethod
@@ -66,9 +95,94 @@ class GroupEntity(Entity):
     @property
     def members(self):
         return self.simulation.persons
+
+    #  Aggregation persons -> entity
+
+    def sum(self, array, role = None):
+        self.simulation.persons.check_array_compatible_with_entity(array)
+        result = self.empty_array()
+        if role is not None:
+            role_filter = (self.members_role == role)
+
+            # Entities for which one person at least has the given role
+            entity_has_role_filter = np.bincount(self.members_entity_id, weights = role_filter) > 0
+
+            result[entity_has_role_filter] += np.bincount(self.members_entity_id[role_filter], weights = array[role_filter])
         else:
+            result += np.bincount(self.members_entity_id, weights = array)
+        return result
 
+    def any(self, array, role = None):
+        sum_in_entity = self.sum(array, role = role)
+        return (sum_in_entity > 0)
 
+    def reduce(self, array, reducer, neutral_element, role = None):
+        self.simulation.persons.check_array_compatible_with_entity(array)
+        position_in_entity = self.members_position
+        role_in_entity = self.members_role
+        role_filter = (role_in_entity == role) if role is not None else True
 
+        result = self.filled_array(neutral_element)  # Neutral value that will be returned if no one with the given role exists.
 
+        # We loop over the positions in the entity
+        # Looping over the entities is tempting, but potentielly slow if there are a lot of entities
+        nb_positions = np.max(position_in_entity) + 1
+        for p in range(nb_positions):
+            filter = (position_in_entity == p) * role_filter
+            entity_filter = self.any(filter)
+            result[entity_filter] = reducer(result[entity_filter], array[filter])
 
+        return result
+
+    def all(self, array, role = None):
+        return self.reduce(array, neutral_element = True, reducer = np.logical_and, role = role)
+
+    def max(self, array, role = None):
+        return self.reduce(array, neutral_element = - np.infty, reducer = np.maximum, role = role)
+
+    def min(self, array, role = None):
+        return self.reduce(array, neutral_element = np.infty, reducer = np.minimum, role = role)
+
+    def nb_persons(self, role = None):
+            role_condition = (self.members_role == role)
+            return self.sum(role_condition)
+
+    # Projection person -> entity
+
+    def value_from_person(self, array, role, default = 0):
+        # TODO: Make sure the role is unique
+        self.simulation.persons.check_array_compatible_with_entity(array)
+        result = self.filled_array(default)
+        role_filter = (self.members_role == role)
+        entity_filter = self.any(role_filter)
+
+        result[entity_filter] = array[role_filter]
+
+        return result
+
+    # Projection entity -> person(s)
+
+    def project(self, array, role = None):
+        self.check_array_compatible_with_entity(array)
+        role_condition = (self.members_role == role) if role is not None else True
+        return array[self.members_entity_id] * role_condition
+
+    def project_on_first_person(self, array):
+        self.check_array_compatible_with_entity(array)
+        entity_position_array = self.members_position
+        entity_index_array = self.members_entity_id
+        boolean_filter = (entity_position_array == 0)
+        return array[entity_index_array] * boolean_filter
+
+    def share_between_members(self, array, role = None):
+        self.check_array_compatible_with_entity(array)
+        nb_persons_per_entity = self.nb_persons(role)
+        return self.project(array / nb_persons_per_entity, role = role)
+
+    # Projection entity -> entity
+
+    def transpose(self, array, origin_entity):
+        origin_entity = self.simulation.get_entity(origin_entity)
+        origin_entity.check_array_compatible_with_entity(array)
+        input_projected = origin_entity.project_on_first_person(array)
+        return self.sum(input_projected)
---
+++ openfisca_core/tests/test_entities.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+
+from copy import deepcopy
+
+from openfisca_core.tools import assert_near
+from dummy_country import Familles, Individus
+from test_countries import tax_benefit_system
+
+TEST_CASE = {
+    'individus': [{'id': 'ind0'}, {'id': 'ind1'}, {'id': 'ind2'}, {'id': 'ind3'}, {'id': 'ind4'}, {'id': 'ind5'}],
+    'familles': [
+        {'enfants': ['ind2', 'ind3'], 'parents': ['ind0', 'ind1']},
+        {'enfants': ['ind5'], 'parents': ['ind4']}
+        ],
+    }
+
+TEST_CASE_AGES = deepcopy(TEST_CASE)
+AGES = [40, 37, 7, 9, 54, 20]
+for (individu, age) in zip(TEST_CASE_AGES['individus'], AGES):
+        individu['age'] = age
+
+ROLES_DANS_FAMILLE = Familles.get_role_enum()
+PARENT = ROLES_DANS_FAMILLE['parents']
+ENFANT = ROLES_DANS_FAMILLE['enfants']
+
+
+def new_simulation(test_case):
+    return tax_benefit_system.new_scenario().init_from_test_case(
+        period = 2013,
+        test_case = test_case
+        ).new_simulation()
+
+
+def test_role_index_and_positions():
+    simulation = new_simulation(TEST_CASE)
+    assert_near(simulation.get_entity(Familles).members_role, [PARENT, PARENT, ENFANT, ENFANT, PARENT, ENFANT])
+    assert_near(simulation.get_entity(Familles).members_legacy_role, [0, 1, 2, 3, 0, 2])
+    assert_near(simulation.get_entity(Familles).members_entity_id, [0, 0, 0, 0, 1, 1])
+    assert_near(simulation.get_entity(Familles).members_position, [0, 1, 2, 3, 0, 1])
+
+
+def test_project():
+    test_case = deepcopy(TEST_CASE)
+    test_case['familles'][0]['af'] = 20000
+
+    simulation = new_simulation(test_case)
+    familles = simulation.get_entity(Familles)
+
+    af = familles.calculate('af', 2013)
+    af_projete = familles.project(af)
+
+    assert_near(af_projete, [20000, 20000, 20000, 20000, 0, 0])
+
+    af_projete_parents = familles.project(af, role = PARENT)
+    assert_near(af_projete_parents, [20000, 20000, 0, 0, 0, 0])
+
+
+def test_project_on_first_person():
+    test_case = deepcopy(TEST_CASE)
+    test_case['familles'][0]['af'] = 20000
+    test_case['familles'][1]['af'] = 5000
+
+    simulation = new_simulation(test_case)
+    familles = simulation.get_entity(Familles)
+
+    af = familles.calculate('af')
+    af_projete = familles.project_on_first_person(af)
+
+    assert_near(af_projete, [20000, 0, 0, 0, 5000, 0])
+
+
+def test_share_between_members():
+    test_case = deepcopy(TEST_CASE)
+    test_case['familles'][0]['af'] = 20000
+    test_case['familles'][1]['af'] = 5000
+
+    simulation = new_simulation(test_case)
+    familles = simulation.get_entity(Familles)
+
+    af = familles.calculate('af')
+
+    af_shared = familles.share_between_members(af, role = PARENT)
+
+    assert_near(af_shared, [10000, 10000, 0, 0, 5000, 0])
+
+
+def test_sum():
+    test_case = deepcopy(TEST_CASE)
+    test_case['individus'][0]['salaire_net'] = 1000
+    test_case['individus'][1]['salaire_net'] = 1500
+    test_case['individus'][4]['salaire_net'] = 3000
+    test_case['individus'][5]['salaire_net'] = 500
+
+    simulation = new_simulation(test_case)
+    familles = simulation.get_entity(Familles)
+
+    salaire_net = familles.members.calculate('salaire_net')
+    salaire_total_par_famille = familles.sum(salaire_net)
+
+    assert_near(salaire_total_par_famille, [2500, 3500])
+
+    salaire_total_parents_par_famille = familles.sum(salaire_net, role = PARENT)
+
+    assert_near(salaire_total_parents_par_famille, [2500, 3000])
+
+
+def test_any():
+    test_case = deepcopy(TEST_CASE_AGES)
+    simulation = new_simulation(test_case)
+    familles = simulation.get_entity(Familles)
+
+    age = familles.members.calculate('age')
+    condition_age = (age <= 18)
+    has_famille_member_with_age_inf_18 = familles.any(condition_age)
+    assert_near(has_famille_member_with_age_inf_18, [True, False])
+
+    condition_age_2 = (age > 18)
+    has_famille_enfant_with_age_sup_18 = familles.any(condition_age_2, role = ENFANT)
+    assert_near(has_famille_enfant_with_age_sup_18, [False, True])
+
+
+def test_all():
+    test_case = deepcopy(TEST_CASE_AGES)
+    simulation = new_simulation(test_case)
+    familles = simulation.get_entity(Familles)
+
+    age = familles.members.calculate('age')
+
+    condition_age = (age >= 18)
+    all_persons_age_sup_18 = familles.all(condition_age)
+    assert_near(all_persons_age_sup_18, [False, True])
+
+    all_parents_age_sup_18 = familles.all(condition_age, role = PARENT)
+    assert_near(all_parents_age_sup_18, [True, True])
+
+
+def test_max():
+    test_case = deepcopy(TEST_CASE_AGES)
+    simulation = new_simulation(test_case)
+    familles = simulation.get_entity(Familles)
+
+    age = familles.members.calculate('age')
+
+    age_max = familles.max(age)
+    assert_near(age_max, [40, 54])
+
+    age_max_enfants = familles.max(age, role = ENFANT)
+    assert_near(age_max_enfants, [9, 20])
+
+
+def test_min():
+    test_case = deepcopy(TEST_CASE_AGES)
+    simulation = new_simulation(test_case)
+    familles = simulation.get_entity(Familles)
+
+    age = familles.members.calculate('age')
+
+    age_min = familles.min(age)
+    assert_near(age_min, [7, 20])
+
+    age_min_parents = familles.min(age, role = PARENT)
+    assert_near(age_min_parents, [37, 54])
+
+
+def test_swap():
+    test_case = deepcopy(TEST_CASE)
+    test_case['individus'][0]['salaire_net'] = 1000
+    test_case['individus'][1]['salaire_net'] = 1500
+    test_case['individus'][4]['salaire_net'] = 3000
+    test_case['individus'][5]['salaire_net'] = 500
+
+    simulation = new_simulation(test_case)
+    individus = simulation.get_entity(Individus)
+
+    salaire_net = individus.calculate('salaire_net')
+
+    salaire_conjoint = individus.value_from_partner(salaire_net, individus.famille, PARENT)
+
+    assert_near(salaire_conjoint, [1500, 1000, 0, 0, 0, 0])
