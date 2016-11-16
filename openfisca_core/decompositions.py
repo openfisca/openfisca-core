c7cc1cc4601ab73e8beeaa15a4e9be83529fdcce
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-11-16 19:38:20 +0100

    Use reference taxbenefit_system decompositon when using reform

--- openfisca_core/decompositions.py
+++ openfisca_core/decompositions.py
@@ -37,7 +37,11 @@ def calculate(simulations, decomposition_json):
 
 def get_decomposition_json(tax_benefit_system, xml_file_path = None):
     if xml_file_path is None:
-        xml_file_path = os.path.join(tax_benefit_system.DECOMP_DIR, tax_benefit_system.DEFAULT_DECOMP_FILE)
+        try:
+            xml_file_path = os.path.join(tax_benefit_system.DECOMP_DIR, tax_benefit_system.DEFAULT_DECOMP_FILE)
+        except:
+            xml_file_path = os.path.join(
+                tax_benefit_system.reference.DECOMP_DIR, tax_benefit_system.reference.DEFAULT_DECOMP_FILE)
     decomposition_tree = xml.etree.ElementTree.parse(xml_file_path)
     decomposition_xml_json = conv.check(decompositionsxml.xml_decomposition_to_json)(decomposition_tree.getroot(),
         state = conv.State)
