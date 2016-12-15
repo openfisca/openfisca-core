6ce7002d630171f9282c8c5140fbd3287c05adce
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-12-15 18:31:20 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-Core',
-    version = '4.1.4b1',
+    version = '4.1.5',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
