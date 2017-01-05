9ba68f9cf1b2dac093f9faca4d1206d947418b38
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-01-06 00:41:16 +0100

    Introduce run_tests_from_directory

--- openfisca_core/tests/test_yaml.py
+++ openfisca_core/tests/test_yaml.py
@@ -4,7 +4,7 @@ import pkg_resources
 import os
 from nose.tools import nottest, raises
 
-from openfisca_core.test_runner import run_tests_from_yaml_file
+from openfisca_core.test_runner import run_tests_from_file, run_tests_from_directory
 
 from openfisca_core.tests.dummy_country import DummyTaxBenefitSystem
 
@@ -13,11 +13,12 @@ tax_benefit_system = DummyTaxBenefitSystem()
 openfisca_core_dir = pkg_resources.get_distribution('OpenFisca-Core').location
 yamls_tests_dir = os.path.join(openfisca_core_dir, 'openfisca_core', 'tests', 'tests_yaml')
 
-nottest(run_tests_from_yaml_file)
+nottest(run_tests_from_file)
+nottest(run_tests_from_directory)
 @nottest
 def run_yaml_test(file_name, options = {}):
     yaml_path = os.path.join(yamls_tests_dir, '{}.yaml'.format(file_name))
-    run_tests_from_yaml_file(tax_benefit_system, yaml_path, options)
+    run_tests_from_file(tax_benefit_system, yaml_path, options)
 
 def test_sucess():
     run_yaml_test('test_sucess')
@@ -46,3 +47,12 @@ def test_absolute_error_margin_success():
 @raises(AssertionError)
 def test_absolute_error_margin_fail():
     run_yaml_test('test_absolute_error_margin', options = {'force': True})
+
+def test_run_tests_from_directory():
+    dir_path = os.path.join(yamls_tests_dir, 'directory')
+    run_tests_from_directory(tax_benefit_system, dir_path)
+
+@raises(AssertionError)
+def test_run_tests_from_directory_fail():
+    dir_path = os.path.join(yamls_tests_dir, 'directory')
+    run_tests_from_directory(tax_benefit_system, dir_path, options = {'force': True})
