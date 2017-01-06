95f124993063a732430415f2ec8a943c9b929017
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-01-06 01:05:26 +0100

    Test name_filter

--- openfisca_core/tests/test_yaml.py
+++ openfisca_core/tests/test_yaml.py
@@ -18,41 +18,61 @@ nottest(run_tests_from_directory)
 @nottest
 def run_yaml_test(file_name, options = {}):
     yaml_path = os.path.join(yamls_tests_dir, '{}.yaml'.format(file_name))
-    run_tests_from_file(tax_benefit_system, yaml_path, options)
+    return run_tests_from_file(tax_benefit_system, yaml_path, options)
 
-def test_sucess():
-    run_yaml_test('test_sucess')
+def test_success():
+    run_yaml_test('test_success')
+
 
 @raises(AssertionError)
 def test_fail():
     run_yaml_test('test_failure')
 
+
 def test_ignore_test():
-    run_yaml_test('test_ignore')
+    # The 2nd test is ignored
+    assert run_yaml_test('test_ignore') == 1
+
 
 @raises(AssertionError)
 def test_force():
     run_yaml_test('test_ignore', options = {'force': True})
 
+
 def test_relative_error_margin_success():
     run_yaml_test('test_relative_error_margin')
+
 
 @raises(AssertionError)
 def test_relative_error_margin_fail():
     run_yaml_test('test_relative_error_margin', options = {'force': True})
 
+
 def test_absolute_error_margin_success():
     run_yaml_test('test_absolute_error_margin')
+
 
 @raises(AssertionError)
 def test_absolute_error_margin_fail():
     run_yaml_test('test_absolute_error_margin', options = {'force': True})
 
+
 def test_run_tests_from_directory():
     dir_path = os.path.join(yamls_tests_dir, 'directory')
-    run_tests_from_directory(tax_benefit_system, dir_path)
+    assert run_tests_from_directory(tax_benefit_system, dir_path) == 4
+
 
 @raises(AssertionError)
 def test_run_tests_from_directory_fail():
     dir_path = os.path.join(yamls_tests_dir, 'directory')
     run_tests_from_directory(tax_benefit_system, dir_path, options = {'force': True})
+
+
+def test_name_filter():
+    nb_tests = run_tests_from_directory(
+        tax_benefit_system,
+        yamls_tests_dir,
+        options = {'name_filter': 'success'}
+        )
+
+    assert nb_tests == 3
