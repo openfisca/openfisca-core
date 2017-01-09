5e565e0ab32341af9aaabf7c816b8d1d861a85d2
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-01-09 15:15:05 +0100

    Handle options in run_test

--- openfisca_core/scripts/run_test.py
+++ openfisca_core/scripts/run_test.py
@@ -14,8 +14,10 @@ def main():
     parser.add_argument('-c', '--country_package', action = 'store', required = True)
     parser.add_argument('-f', '--force', action = 'store_true', default = False,
         help = 'force testing of tests with "ignore" flag and formulas belonging to "ignore_output_variables" list')
-    parser.add_argument('-n', '--name', default = None, help = "partial name of tests to execute")
+    parser.add_argument('-n', '--name_filter', default = None, help = "partial name of tests to execute")
     parser.add_argument('-v', '--verbose', action = 'store_true', default = False, help = "increase output verbosity")
+    parser.add_argument('-m', '--relative_error_margin', help = "default relative error margin", action = 'store', type = float)
+    parser.add_argument('-M', '--absolute_error_margin', help = "default absolute error margin", action = 'store', type = float)
     args = parser.parse_args()
     logging.basicConfig(level = logging.DEBUG if args.verbose else logging.WARNING, stream = sys.stdout)
 
@@ -26,11 +28,19 @@ def main():
         print('ERROR: `{}` does not seem to be a valid Openfisca country package.'.format(args.country_package))
         sys.exit(1)
 
+    options = {
+        'verbose': args.verbose,
+        'force': args.force,
+        'name_filter': args.name_filter,
+        'default_relative_error_margin': args.relative_error_margin,
+        'default_absolute_error_margin': args.absolute_error_margin,
+    }
+
     tests_found = False
 
     for path in args.path:
         path = os.path.abspath(path)
-        nb_tests = run_tests(tax_benefit_system, path)
+        nb_tests = run_tests(tax_benefit_system, path, options)
         tests_found = tests_found or nb_tests > 0
 
     if not tests_found:
