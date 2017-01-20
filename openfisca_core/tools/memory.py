ee8c7274dae17b2fe4487de762166714ef847438
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2017-01-20 08:42:31 +0100

    Add memory to tools

--- openfisca_core/tools/memory.py
+++ openfisca_core/tools/memory.py
@@ -9,13 +9,12 @@ import numpy as np
 
 def memory_usage(simulation):
     infos_by_variable = dict()
-    for column_name, holder in simulation.holder_by_name:
-        infos = infos_by_variable[column_name] = dict()
+    for column_name, holder in simulation.holder_by_name.iteritems():
         if holder is not None:
             if holder._array is not None:
                 # Only used when column.is_permanent
                 array = holder._array
-                infos.update(
+                infos_by_variable[column_name] = dict(
                     periods = ['permanent'],
                     ncells = np.prod(array.shape),
                     item_size = array.itemsize,
@@ -25,14 +24,14 @@ def memory_usage(simulation):
             elif holder._array_by_period is not None:
                 periods = sorted(holder._array_by_period.keys())
                 array = holder._array_by_period[periods[0]]
-                infos.update(
+                infos_by_variable[column_name] = dict(
                     periods = periods,
                     ncells = np.prod(array.shape),  # per period
                     item_size = array.itemsize,
                     dtype = array.dtype,
                     nbytes = array.nbytes * len(periods),
                     )
-        return infos_by_variable
+    return infos_by_variable
 
 
 def print_memory_usage(simulation):
@@ -46,8 +45,8 @@ def print_memory_usage(simulation):
             infos['dtype'],
             infos['nbytes'],
             )))
-    infos.sort()
-    for _, _, line in infos:
+    infos_lines.sort()
+    for _, _, line in infos_lines:
         print(line.rjust(100))
 
 
