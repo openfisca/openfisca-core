cf9ee9d44571a7b1548859f16e1e16886b7fe210
Michel Blancard <michel.blancard@data.gouv.fr>
2017-02-15 20:11:59 +0100

    adapt test_extra_params.py to period_behavior

--- openfisca_core/tests/test_extra_params.py
+++ openfisca_core/tests/test_extra_params.py
@@ -44,7 +44,6 @@ class formula_4(Variable):
     period_behavior = MONTH
 
     def function(self, simulation, period, choice):
-        period = period.this_year
         return period, self.zeros() + choice
 
 
@@ -52,7 +51,7 @@ class formula_4(Variable):
 tax_benefit_system = dummy_country.DummyTaxBenefitSystem()
 tax_benefit_system.add_variables(formula_1, formula_2, formula_3, formula_4)
 
-reference_period = periods.period(u'2013')
+reference_period = periods.period(u'2013-01')
 
 simulation = tax_benefit_system.new_scenario().init_single_entity(
     period = reference_period.this_month,
@@ -79,8 +78,8 @@ def test_json_conversion():
 
 
 def test_base_functions():
-    assert simulation.calculate('formula_4', 2013, extra_params = [0]) == 0
-    assert simulation.calculate('formula_4', 2013, extra_params = [1]) == 1
+    assert simulation.calculate('formula_4', '2013-01', extra_params = [0]) == 0
+    assert simulation.calculate('formula_4', '2013-01', extra_params = [1]) == 1
 
     # With the 'requested_period_last_value' base_function,
     # the value on an month can be infered from the year value, without running the function for that month
