d562cbc965baa4458ee0644633b2119224115d9e
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-02-17 16:59:57 +0100

    Handle each possible exception separately

--- openfisca_core/scripts/__init__.py
+++ openfisca_core/scripts/__init__.py
@@ -4,8 +4,15 @@ import importlib
 import logging
 import sys
 
+from openfisca_core.reforms import Reform
+
 log = logging.getLogger(__name__)
 logging.basicConfig(format='%(levelname)s: %(message)s')
+
+
+def handle_error(error_message):
+    log.error(error_message)
+    sys.exit(1)
 
 
 def add_tax_benefit_system_arguments(parser):
@@ -17,13 +24,14 @@ def add_tax_benefit_system_arguments(parser):
 
 
 def build_tax_benefit_sytem(country_package_name, extensions, reforms):
+
     if country_package_name:
         try:
             country_package = importlib.import_module(country_package_name)
-            assert hasattr(country_package, 'CountryTaxBenefitSystem')
-        except (ImportError, AssertionError):
-            log.error('`{}` does not seem to be a valid Openfisca country package.'.format(country_package_name))
-            sys.exit(1)
+        except ImportError:
+            handle_error('Could not import module `{}`. Make sure it is installed in your environment.'.format(country_package_name))
+        if not hasattr(country_package, 'CountryTaxBenefitSystem'):
+            handle_error('`{}` does not seem to be a valid Openfisca country package.'.format(country_package_name))
     else:
         country_package_name = detect_country_package()
         country_package = importlib.import_module(country_package_name)
@@ -37,13 +45,20 @@ def build_tax_benefit_sytem(country_package_name, extensions, reforms):
     if reforms:
         for reform_path in reforms:
             try:
-                [reform_package, reform_name] = reform_path.rsplit('.', 1)
+                reform_package, reform_name = reform_path.rsplit('.', 1)
+            except ValueError:
+                handle_error('`{}` does not seem to be a path pointing to a reform. A path looks like `some_country_package.reforms.some_reform.`'.format(reform_path))
+            try:
                 reform_module = importlib.import_module(reform_package)
+            except ImportError:
+                handle_error('Could not import `{}`.'.format(reform_package))
+            try:
                 reform = getattr(reform_module, reform_name)
-                tax_benefit_system = reform(tax_benefit_system)
-            except:
-                log.error('`{}` does not seem to be a valid Openfisca reform for `{}`.'.format(reform_path, country_package.__name__))
-                raise
+            except AttributeError:
+                handle_error('{} has no attribute {}'.format(reform_package, reform_name))
+            if not isinstance(reform, Reform):
+                handle_error('`{}` does not seem to be a valid Openfisca reform for `{}`.'.format(reform_path, country_package.__name__))
+            tax_benefit_system = reform(tax_benefit_system)
 
     return tax_benefit_system
 
@@ -62,8 +77,7 @@ def detect_country_package():
                 installed_country_packages.append(module_name)
 
     if len(installed_country_packages) == 0:
-        log.error('No country package has been detected on your environment. If your country package is installed but not detected, please use the --country_package option.')
-        sys.exit(1)
+        handle_error('No country package has been detected on your environment. If your country package is installed but not detected, please use the --country_package option.')
     if len(installed_country_packages) > 1:
         log.warning('Several country packages detected : `{}`. Using `{}` by default. To use another package, please use the --country_package option.'.format(', '.join(installed_country_packages), installed_country_packages[0]))
     return installed_country_packages[0]
