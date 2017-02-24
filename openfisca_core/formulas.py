b273f740ffe0aaf93efb5d71f5ee0b5df64cc16b
Michel Blancard <michel.blancard@data.gouv.fr>
2017-02-24 11:39:34 +0100

    fixup! flake8

--- openfisca_core/formulas.py
+++ openfisca_core/formulas.py
@@ -464,7 +464,6 @@ class SimpleFormula(AbstractFormula):
             else:
                 return function(entity, period, legislation, *extra_params)
 
-
     def filter_role(self, array_or_dated_holder, default = None, entity = None, role = None):
         """Convert a persons array to an entity array, copying only cells of persons having the given role."""
         holder = self.holder
