22f48bf311a787f13e369e92ba17626ff846a96c
Michel Blancard <michel.blancard@data.gouv.fr>
2017-02-24 12:13:39 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-Core',
-    version = '5.0.2',
+    version = '6.0.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
