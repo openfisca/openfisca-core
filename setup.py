412be52f95101f4a3515acbcdc9a3b6e014bcaef
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-04-18 14:55:00 +0200

    Use the right Dummy Country version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
             ],
         'test': [
             'nose',
-            'openfisca-dummy-country >= 0.1.2'
+            'openfisca-dummy-country == 0.1.2',
             ],
         },
     include_package_data = True,  # Will read MANIFEST.in
