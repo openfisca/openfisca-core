23468c354e150448db16b90ea6969c6343c2bb4d
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-04-26 14:39:32 +0200

    Upgrade dummy country

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
             ],
         'test': [
             'nose',
-            'openfisca-dummy-country == 0.1.4',
+            'openfisca-dummy-country == 0.1.5',
             ],
         },
     include_package_data = True,  # Will read MANIFEST.in
