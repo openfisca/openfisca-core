3b9a540945df17175364d530f56d7c55a681c263
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-05-09 21:43:50 +0200

    Move version bump enforcing to Circle

---
+++ check-version-bump.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+current_version=`python setup.py --version`
+
+if ! git diff-index --quiet origin/master openfisca_core openfisca_web_api
+then
+    if git rev-parse $current_version
+    then
+        set +x
+        echo "Version $version already exists. Please update version number in setup.py before merging this branch into master."
+        exit 1
+    fi
+
+    if git diff-index master --quiet CHANGELOG.md
+    then
+        set +x
+        echo "CHANGELOG.md has not been modified. Please update it before merging this branch into master."
+        exit 1
+    fi
+fi
