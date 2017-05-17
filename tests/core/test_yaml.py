cd182a7606a1a25406d376072199a300efd6d4d0
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-05-17 20:13:23 +0200

    Make test_yaml work with CI

--- tests/core/test_yaml.py
+++ tests/core/test_yaml.py
@@ -14,7 +14,13 @@ tax_benefit_system = CountryTaxBenefitSystem()
 
 
 openfisca_core_dir = pkg_resources.get_distribution('OpenFisca-Core').location
-yamls_tests_dir = os.path.join(openfisca_core_dir, 'tests', 'core', 'yaml_tests')
+yaml_tests_dir = os.path.join(openfisca_core_dir, 'tests', 'core', 'yaml_tests')
+
+# When tests are run by the CI, openfisca_core is not installed in editable mode, therefore the tests are not packaged with the ditribution. We thus get them from ~/openfisca-core/tests/core/yaml_tests
+if not os.path.isdir(yaml_tests_dir):
+    ci_yaml_tests_dir = os.path.join(os.path.expanduser('~'), 'openfisca-core', 'tests', 'core', 'yaml_tests')
+    yaml_tests_dir = ci_yaml_tests_dir if os.path.isdir(ci_yaml_tests_dir) else yaml_tests_dir
+
 
 # Declare that these two functions are not tests to run with nose
 nottest(run_tests)
@@ -23,7 +29,7 @@ nottest(generate_tests)
 
 @nottest
 def run_yaml_test(file_name, options = {}):
-    yaml_path = os.path.join(yamls_tests_dir, '{}.yaml'.format(file_name))
+    yaml_path = os.path.join(yaml_tests_dir, '{}.yaml'.format(file_name))
     return run_tests(tax_benefit_system, yaml_path, options)
 
 
@@ -55,7 +61,7 @@ def test_absolute_error_margin_fail():
 
 
 def test_run_tests_from_directory():
-    dir_path = os.path.join(yamls_tests_dir, 'directory')
+    dir_path = os.path.join(yaml_tests_dir, 'directory')
     assert run_tests(tax_benefit_system, dir_path) == 4
 
 
@@ -65,13 +71,13 @@ def test_with_reform():
 
 @raises(AssertionError)
 def test_run_tests_from_directory_fail():
-    run_tests(tax_benefit_system, yamls_tests_dir)
+    run_tests(tax_benefit_system, yaml_tests_dir)
 
 
 def test_name_filter():
     nb_tests = run_tests(
         tax_benefit_system,
-        yamls_tests_dir,
+        yaml_tests_dir,
         options = {'name_filter': 'success'}
         )
 
@@ -79,20 +85,20 @@ def test_name_filter():
 
 
 def test_nose_style():
-    dir_path = os.path.join(yamls_tests_dir, 'directory')
+    dir_path = os.path.join(yaml_tests_dir, 'directory')
     for test in generate_tests(tax_benefit_system, dir_path):
         yield test
 
 
 def test_shell_script():
-    yaml_path = os.path.join(yamls_tests_dir, 'test_success.yaml')
+    yaml_path = os.path.join(yaml_tests_dir, 'test_success.yaml')
     command = ['openfisca-run-test', yaml_path, '-c', 'openfisca_country_template']
     with open(os.devnull, 'wb') as devnull:
         subprocess.check_call(command, stdout = devnull)
 
 
 def test_shell_script_with_reform():
-    yaml_path = os.path.join(yamls_tests_dir, 'test_with_reform_2.yaml')
+    yaml_path = os.path.join(yaml_tests_dir, 'test_with_reform_2.yaml')
     command = ['openfisca-run-test', yaml_path, '-c', 'openfisca_country_template', '-r', 'openfisca_country_template.reforms.removal_basic_income.removal_basic_income']
     with open(os.devnull, 'wb') as devnull:
         subprocess.check_call(command, stdout = devnull)
