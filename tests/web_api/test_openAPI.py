31ac93bcf50198742643529059fc29da307c3a02
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-05-31 12:40:57 +0200

    Test /swagger/ route

---
+++ tests/web_api/test_openAPI.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import json
+from httplib import OK
+
+from nose.tools import assert_equal, assert_items_equal
+from . import subject
+
+openAPI_response = subject.get('/swagger')
+
+
+def test_return_code():
+    assert_equal(openAPI_response.status_code, OK)
+
+
+def test_paths():
+    assert_items_equal(
+        json.loads(openAPI_response.data)['paths'],
+        ["/parameter/{parameterID}", "/parameters", "/variable/{variableID}", "/variables"])
