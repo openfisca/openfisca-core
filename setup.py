8741b6c40ff3282182029673c596ddd03770d650
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-05-31 22:00:52 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-Core',
-    version = '13.0.1',
+    version = '14.0.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
