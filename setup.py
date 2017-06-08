f1d2dd485392b5727b7fddd23ea3a5f6ff27e8fc
Michel Blancard <michel.blancard@data.gouv.fr>
2017-06-08 13:03:58 +0200

    Update requirement for openfisca-country-template

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'test': [
             'nose',
             'flake8',
-            'openfisca-country-template == 1.2.2',
+            'openfisca-country-template >= 1.2.3rc0, <= 1.2.3',
             'openfisca-extension-template == 1.1.0',
             ],
         },
