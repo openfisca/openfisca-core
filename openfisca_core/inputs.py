8f9f1a3ec36db54248411b81b5cd971bc9c41289
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-06-19 01:17:25 +0200

    Use a JSON validator

--- openfisca_core/inputs.py
+++ openfisca_core/inputs.py
@@ -1,16 +1,36 @@
 # -*- coding: utf-8 -*-
 
-def build_simulation(situation, tax_benefit_system):
-    if not isinstance(situation, dict):
-        raise SituationParsingError({
-            'error': 'Invalid type: a situation must be of type "object".'
-            })
+from jsonschema import validate, ValidationError
 
-    for entity in situation.keys():
-        if entity not in [loaded_entity.plural for loaded_entity in tax_benefit_system.entities]:
+json_schema = {
+    'type': 'object',
+    'additionalProperties': False,
+    'properties': {
+        'persons': {'type': 'object'}
+    }
+}
+
+def build_simulation(situation, tax_benefit_system):
+    try:
+        validate(situation, json_schema)
+    except ValidationError as e:
+        if len(e.path) == 0 and e.validator == "type":
+            if not isinstance(situation, dict):
+                raise SituationParsingError({
+                    'error': 'Invalid type: a situation must be of type "object".'
+                    })
+        if len(e.path) == 0 and e.validator == "additionalProperties":
             raise SituationParsingError({
-                entity: 'This entity is not defined in the loaded tax and benefit system.'
+                e.message.split("'")[1]: 'This entity is not defined in the loaded tax and benefit system.'
                 })
+
+
+
+    #     if entity not in [loaded_entity.plural for loaded_entity in tax_benefit_system.entities]:
+    #         raise SituationParsingError({
+    #             entity: 'This entity is not defined in the loaded tax and benefit system.'
+    #             })
+
 
 
 class SituationParsingError(Exception):
