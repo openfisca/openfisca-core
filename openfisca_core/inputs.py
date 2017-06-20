70e6750b0a0bc2e7690fdf66b966f0f0b4ce4345
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-06-20 10:19:20 +0200

    Refactor

--- openfisca_core/inputs.py
+++ openfisca_core/inputs.py
@@ -57,18 +57,6 @@ def get_situation_schema(tax_benefit_system):
     }
 
 
-def check_type(input, type, path = []):
-    type_map = {
-        dict: "object",
-        list: "array",
-        str: "string"
-    }
-
-    if not isinstance(input, type):
-        raise SituationParsingError(path,
-            'Invalid type: must be of type "{}".'.format(type_map[type]))
-
-
 # def check_variable(property_name, property, entity_class, tax_benefit_system, path):
 #     variable = tax_benefit_system.get_column(property_name)
 #     if not variable:
@@ -86,33 +74,3 @@ def check_type(input, type, path = []):
 #         if not isinstance(property, dict):
 #             raise SituationParsingError(path,
 #             'Input variables need to be set for a specific period. For instance: "{salary: {"2017-06": 2000}}"')
-
-
-def build_simulation(situation, tax_benefit_system):
-    check_type(situation, dict, ['error'])
-
-    simulation = Simulation(tax_benefit_system = tax_benefit_system)
-
-    persons_json = situation.pop(tax_benefit_system.person_entity.plural, None)
-    persons_entity = tax_benefit_system.person_entity(simulation, persons_json)
-
-    entities_by_plural = {
-        entity.plural: entity
-        for entity in tax_benefit_system.entities
-    }
-
-    for entity_plural, entities_json in situation.iteritems():
-        entity_class = entities_by_plural.get(entity_plural)
-        if not entity_class:
-            raise SituationParsingError([entity_plural],
-                'This entity is not defined in the loaded tax and benefit system.')
-
-        entities = entity_class(simulation, entities_json)
-
-
-class SituationParsingError(Exception):
-    def __init__(self, path, message, code = None):
-        self.error = {}
-        dpath_path = '/'.join(path)
-        dpath.util.new(self.error, dpath_path, message)
-        self.code = code
