857d7f12e9b6befa1d863ec7f4a1d781d16871cf
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-06-21 21:13:20 +0200

    Define entities in OpenAPI spec

--- tests/web_api/test_openAPI.py
+++ tests/web_api/test_openAPI.py
@@ -3,7 +3,7 @@
 import json
 from httplib import OK
 
-from nose.tools import assert_equal, assert_items_equal
+from nose.tools import assert_equal, assert_items_equal, assert_in
 from . import subject
 
 openAPI_response = subject.get('/spec')
@@ -12,8 +12,23 @@ openAPI_response = subject.get('/spec')
 def test_return_code():
     assert_equal(openAPI_response.status_code, OK)
 
+body = json.loads(openAPI_response.data)
 
 def test_paths():
     assert_items_equal(
-        json.loads(openAPI_response.data)['paths'],
-        ["/parameter/{parameterID}", "/parameters", "/variable/{variableID}", "/variables"])
+        body['paths'],
+        ["/parameter/{parameterID}",
+        "/parameters",
+        "/variable/{variableID}",
+        "/variables",
+        "/calculate"]
+        )
+
+
+def test_entity_definition():
+    household = body['definitions']['Household']
+    person = body['definitions']['Person']
+    assert_in('salary', person['properties'])
+    assert_in('rent', household['properties'])
+    assert_in('parents', household['properties'])
+    assert_in('children', household['properties'])
