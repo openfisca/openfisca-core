92880bbb2d458fc3829b144080ac89851b970d0f
Anna-Livia <annalivia.cardin@gmail.com>
2017-07-26 11:49:06 +0200

    Log when no deployement due to same version number

--- deploy-if-version-bump.sh
+++ deploy-if-version-bump.sh
@@ -8,4 +8,6 @@ if ! git rev-parse `python setup.py --version` 2>/dev/null ; then
     python setup.py bdist_wheel
     twine upload dist/* --username openfisca-bot --password fakepassword
     ssh deploy-new-api@api-test.openfisca.fr
+else
+    echo "No deployment - Existing release with `python setup.py --version` version number"
 fi
