8458068be9f358244b977937a588597bd9914add
Anna-Livia <annalivia.cardin@gmail.com>
2017-07-26 11:49:06 +0200

    [TO REVERT] Put logs in

--- deploy-if-version-bump.sh
+++ deploy-if-version-bump.sh
@@ -3,10 +3,15 @@
 
 set -e
 if ! git rev-parse `python setup.py --version` 2>/dev/null ; then
+    echo "No existing release with `python setup.py --version` version number"
+    echo "Stating build"
     git tag `python setup.py --version`
     git push --tags
     python setup.py bdist_wheel
     twine upload dist/* --username openfisca-bot --password $PYPI_PASSWORD
     ssh deploy-new-api@api-test.openfisca.fr
+    echo "Build finished"
+else
+    echo "No deployment - Existing release with `python setup.py --version` version number"
 fi
 
