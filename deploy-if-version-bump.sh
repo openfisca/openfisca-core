1e121451dcac55ce0eeb4316f1b88263a75b38ec
Anna-Livia <annalivia.cardin@gmail.com>
2017-07-26 10:11:29 +0200

    Separate deployment instructions

---
+++ deploy-if-version-bump.sh
@@ -0,0 +1,8 @@
+set -e
+if ! git rev-parse `python setup.py --version` 2>/dev/null ; then
+    git tag `python setup.py --version`
+    git push --tags
+    python setup.py bdist_wheel
+    twine upload dist/* --username openfisca-bot --password fakepassword
+    ssh deploy-new-api@api-test.openfisca.fr
+fi
