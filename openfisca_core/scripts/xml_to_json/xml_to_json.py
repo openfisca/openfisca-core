4302f21355cdf57b90db325fbc4b9fd9fa7ede06
Michel Blancard <michel.blancard@data.gouv.fr>
2017-07-28 16:27:35 +0200

    Script to transform XML files to YAML

--- openfisca_core/scripts/xml_to_json/xml_to_json.py
+++ openfisca_core/scripts/xml_to_json/xml_to_json.py
@@ -1,139 +1,239 @@
 # -*- coding: utf-8 -*-
 
+
 ''' xml_to_json.py : Parse XML parameter files and convert them to YAML files
+
+Comments are NOT converted.
 '''
 
 import os
 
+from lxml import etree
 import yaml
 
-import legislationsxml
+from ...legislations import node_keywords
 
 
-def parse_values_history(values_input):
+# Load
+
+def load_xml_schema():
+    base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    filename_xml_schema = 'legislation.xsd'
+
+    with open(filename_xml_schema, 'r') as f:
+        xmlschema_doc = etree.parse(f)
+        xmlschema = etree.XMLSchema(xmlschema_doc)
+
+    return xmlschema
+
+
+def parse_and_validate_xml(xmlschema, legislation_xml_info_list):
+    xml_trees = []
+    for filename, path_in_legislation in legislation_xml_info_list:
+        with open(filename, 'r') as f:
+            tree = etree.parse(f)
+
+        if not xmlschema.validate(tree):
+            raise ValueError(xmlschema.error_log.filter_from_errors())
+
+        xml_trees.append(tree)
+
+    return xml_trees
+
+
+def transform_values_history(children, value_format):
     values = {}
-    for value_input in values_input:
-        assert set(value_input.keys()).issubset(set(['value', 'start'])), value_input
-        date = value_input['start']
+    for child in children:
+        if child.tag == etree.Comment:
+            print('Warning : ignoring comment "{}"'.format(child))
+            continue
+
+        date = child.attrib['deb']
         assert date not in values
-        if 'value' in value_input:
-            values[date] = {'value': value_input['value']}
-        else:
-            values[date] = {}
+
+        if child.tag == 'VALUE':
+            value = child.attrib['valeur']
+            if value_format == bool:
+                value = bool(int(value))
+            else:
+                value = float(value)
+            values[date] = {'value': value}
+        elif child.tag == 'END':
+            values[date] = {'value': None}
+        elif child.tag == 'PLACEHOLDER':
+            values[date] = "expected"
+
     return values
 
 
-def convert_node(node, path):
-    node_data = {}
+def transform_etree_to_json_recursive(xml_node):
+    json_node = dict()
+    name = None
+    value_format = None
 
-    node_type = node['@type']
-    if node_type == 'Node':
-        os.mkdir(path)
+    for key, value in xml_node.attrib.iteritems():
+        if key == 'code':
+            name = value
+        elif key == 'format':
+            assert 'unit' not in json_node
+            if value == 'percent':
+                json_node['unit'] = '/1'
+        elif key == 'type':
+            assert 'unit' not in json_node
+            if value == 'age':
+                json_node['unit'] = 'year'
+            elif value == 'monetary':
+                json_node['unit'] = 'currency'
+        elif key in {'conflicts', 'option'}:
+            pass
+        elif key == 'origin':
+            if 'reference' not in json_node:
+                json_node['reference'] = value
+        elif key in {'description', 'reference'}:
+            json_node[key] = unicode(value)
+        else:
+            raise ValueError(u'Unknown attribute "{}": "{}"'.format(key, value).encode('utf-8'))
 
-        for k, v in node.items():
-            if k == '@type':
-                pass
-            elif k == 'description':
-                node_data['description'] = v
-            elif k == 'reference':
-                node_data['reference'] = v
-            elif k == 'origin':
-                if 'reference' not in node_data:
-                    node_data['reference'] = v
-            elif k == 'children':
-                for name, child in v.items():
-                    child_path = os.path.join(path, name)
-                    convert_node(child, child_path)
+    if xml_node.tag == 'NODE':
+        json_node['type'] = 'node'
+
+        for child in xml_node:
+            child_name, new_child = transform_etree_to_json_recursive(child)
+            if child_name:
+                json_node[child_name] = new_child
             else:
-                raise ValueError('Unknown key {}'.format((k, v)))
+                assert new_child == {}  # comment
+
+    elif xml_node.tag == 'CODE':
+        json_node['type'] = 'parameter'
+
+        value_list = transform_values_history(xml_node, value_format)
+        json_node['values'] = value_list
+
+    elif xml_node.tag == 'BAREME':
+        json_node['type'] = 'scale'
+
+        brackets = []
+        for child in xml_node:
+            assert child.tag == 'TRANCHE'
+            child_name, new_child = transform_etree_to_json_recursive(child)
+            brackets.append(new_child)
+        json_node['brackets'] = brackets
+
+    elif xml_node.tag == 'TRANCHE':
+        for child in xml_node:
+            child_name, new_child = transform_etree_to_json_recursive(child)
+
+            if child.tag == 'SEUIL':
+                json_node['threshold'] = new_child
+            elif child.tag == 'TAUX':
+                json_node['rate'] = new_child
+            elif child.tag == 'ASSIETTE':
+                json_node['base'] = new_child
+            elif child.tag == 'MONTANT':
+                json_node['amount'] = new_child
+            else:
+                raise ValueError(u'Unknown TRANCHE child {}'.format(child.tag).encode('utf-8'))
+
+    elif xml_node.tag == 'TAUX':
+        json_node = transform_values_history(xml_node, value_format)
+
+    elif xml_node.tag == 'SEUIL':
+        json_node = transform_values_history(xml_node, value_format)
+
+    elif xml_node.tag == 'ASSIETTE':
+        json_node = transform_values_history(xml_node, value_format)
+
+    elif xml_node.tag == 'MONTANT':
+        json_node = transform_values_history(xml_node, value_format)
+
+    elif xml_node.tag is etree.Comment:
+        pass
+
+    else:
+        raise ValueError(u'Unknown tag {}'.format(xml_node.tag).encode('utf-8'))
+
+    return name, json_node
+
+
+def transform_etree_to_json_root(xml_trees):
+    name_list = []
+    json_list = []
+    for tree in xml_trees:
+        root = tree.getroot()
+        name, json_data = transform_etree_to_json_recursive(root)
+        name_list.append(name)
+        json_list.append(json_data)
+    return name_list, json_list
+
+
+# Merge
+
+def merge(name_list, json_list, path_list):
+    # The first json tree is special
+    merged_json = json_list[0]
+    assert not path_list[0]
+
+    for name, json_tree, path in zip(name_list, json_list, path_list)[1:]:
+        pointer = merged_json
+        for key in path:
+            if key in pointer:
+                pointer = pointer[key]
+            else:
+                pointer[key] = {
+                    'type': 'node',
+                    }
+
+        assert name not in pointer, u'{} is defined twice'.format('.'.join(path) + '.' + 'name').encode('utf-8')
+        pointer[name] = json_tree
+
+    return merged_json
+
+
+def load_legislation(legislation_xml_info_list):
+    xmlschema = load_xml_schema()
+
+    xml_trees = parse_and_validate_xml(xmlschema, legislation_xml_info_list)
+
+    name_list, json_list = transform_etree_to_json_root(xml_trees)
+
+    path_list = [path for filename, path in legislation_xml_info_list]
+    merged_json = merge(name_list, json_list, path_list)
+
+    return merged_json
+
+
+# Write YAML
+
+def write_yaml(node, path):
+    node_type = node['type']
+    if node_type == 'node':
+        os.mkdir(path)
+        metadata = {k: node[k] for k in node_keywords if k in node}
+        children = {k: node[k] for k in node if k not in node_keywords}
 
         yaml_filename = os.path.join(path, '_.yaml')
         with open(yaml_filename, 'w') as f:
-            yaml.dump(node_data, f, default_flow_style=False)
+            yaml.safe_dump(metadata, f, default_flow_style=False)
 
-    elif node_type == 'Parameter':
-        for k, v in node.items():
-            if k == '@type':
-                node_data['type'] = 'parameter'
-            elif k == 'description':
-                node_data['description'] = v
-            elif k == 'format':
-                assert 'unit' not in node_data
-                if v == 'rate':
-                    node_data['unit'] = '/1'
-            elif k == 'unit':
-                assert 'unit' not in node_data
-                if v == 'currency':
-                    node_data['unit'] = 'currency'
-                elif v == 'year':
-                    node_data['unit'] = 'year'
-            elif k == 'reference':
-                node_data['reference'] = v
-            elif k == 'origin':
-                if 'reference' not in node_data:
-                    node_data['reference'] = v
-            elif k == 'values':
-                node_data['values'] = parse_values_history(v)
-            elif k == 'conflicts':
-                pass
-            else:
-                raise ValueError('Unknown key {}'.format((k, v)))
+        for child_name, child in children.items():
+            write_yaml(child, os.path.join(path, child_name))
 
-            yaml_filename = path + '.yaml'
-            with open(yaml_filename, 'w') as f:
-                yaml.dump(node_data, f, default_flow_style=False)
+    elif node_type == 'parameter':
+        yaml_filename = path + '.yaml'
+        with open(yaml_filename, 'w') as f:
+            yaml.safe_dump(node, f, default_flow_style=False, allow_unicode=True)
 
-    elif node_type == 'Scale':
-        for k, v in node.items():
-            if k == '@type':
-                node_data['type'] = 'scale'
-            elif k == 'description':
-                node_data['description'] = v
-            elif k == 'option':
-                node_data['option'] = v
-            elif k == 'reference':
-                node_data['reference'] = v
-            elif k == 'origin':
-                if 'reference' not in node_data:
-                    node_data['reference'] = v
-            elif k == 'unit':
-                node_data['unit'] = v
-            elif k == 'conflicts':
-                pass
-            elif k == 'brackets':
-                brackets = []
-                for bracket_input in v:
-                    bracket_data = {}
-                    for bracket_key, bracket_value in bracket_input.items():
-                        if bracket_key == 'origin':
-                            if 'reference' not in bracket_data:
-                                bracket_data['reference'] = bracket_value
-                        elif bracket_key == 'threshold':
-                            bracket_data['threshold'] = parse_values_history(bracket_value)
-                        elif bracket_key == 'rate':
-                            bracket_data['rate'] = parse_values_history(bracket_value)
-                        elif bracket_key == 'amount':
-                            bracket_data['amount'] = parse_values_history(bracket_value)
-                        elif bracket_key == 'base':
-                            bracket_data['base'] = parse_values_history(bracket_value)
-                        else:
-                            raise ValueError('Unknown key {}'.format((bracket_key, bracket_value)))
-
-                    brackets.append(bracket_data)
-                node_data['brackets'] = brackets
-
-            else:
-                raise ValueError('Unknown key {}'.format((k, v)))
-
-            yaml_filename = path + '.yaml'
-            with open(yaml_filename, 'w') as f:
-                yaml.dump(node_data, f, default_flow_style=False)
+    elif node_type == 'scale':
+        yaml_filename = path + '.yaml'
+        with open(yaml_filename, 'w') as f:
+            yaml.safe_dump(node, f, default_flow_style=False)
 
     else:
-        raise ValueError('Unknown @type {}'.format(node_type))
+        raise ValueError('Unknown type {}'.format(node_type))
 
 
-def convert_files(legislation_xml_info_list, path):
-    params_tree = legislationsxml.load_legislation(legislation_xml_info_list)
+def write_legislation(legislation_xml_info_list, path):
+    params_tree = load_legislation(legislation_xml_info_list)
 
-    convert_node(params_tree, path)
+    write_yaml(params_tree, path)
