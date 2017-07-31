2baf0dc625e416aa90998cbd744fdcf9003a0b61
Anna-Livia <annalivia.cardin@gmail.com>
2017-07-31 10:48:12 +0200

    [To revert] Change version number and password

--- deploy-if-version-bump.sh
+++ deploy-if-version-bump.sh
@@ -5,7 +5,7 @@ if ! git rev-parse `python setup.py --version` 2>/dev/null ; then
     git tag `python setup.py --version`
     git push --tags
     python setup.py bdist_wheel
-    twine upload dist/* --username openfisca-bot --password $PYPI_PASSWORD
+    twine upload dist/* --username openfisca-bot --password fakepassword
     ssh deploy-new-api@api-test.openfisca.fr
 else
     echo "No deployment - Existing release with `python setup.py --version` version number"
