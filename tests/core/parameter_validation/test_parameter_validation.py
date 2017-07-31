773b82b3b8ffa95656aa6099fa70b4d3cfd64ccc
Michel Blancard <michel.blancard@data.gouv.fr>
2017-07-31 16:02:08 +0200

    Test YAML validation

--- tests/core/parameter_validation/test_parameter_validation.py
+++ tests/core/parameter_validation/test_parameter_validation.py
@@ -2,8 +2,8 @@
 
 import os
 
-from nose.tools import raises
-from lxml.etree import XMLSyntaxError
+import yaml
+import jsonschema
 
 from openfisca_core.taxbenefitsystems import TaxBenefitSystem
 from openfisca_country_template.entities import entities
@@ -15,75 +15,84 @@ year = 2016
 
 
 class TestTaxBenefitSystem(TaxBenefitSystem):
-    def __init__(self, path_to_xml):
+    def __init__(self, path_to_json):
         TaxBenefitSystem.__init__(self, entities)
-        self.add_legislation_params(path_to_xml)
+        self.add_legislation_params(path_to_json)
 
 
-@raises(XMLSyntaxError)
-def test_malformed():
-    path_to_xml = os.path.join(BASE_DIR, 'malformed.xml')
-    tbs = TestTaxBenefitSystem(path_to_xml)
-    tbs.compute_legislation()
-
-
-def test_free_text():
-    path_to_xml = os.path.join(BASE_DIR, 'free_text.xml')
-    tbs = TestTaxBenefitSystem(path_to_xml)
+def test_indentation():
+    path_to_json = os.path.join(BASE_DIR, 'indentation')
+    tbs = TestTaxBenefitSystem(path_to_json)
     try:
         tbs.compute_legislation()
-    except ValueError as e:
+    except yaml.scanner.ScannerError as e:
         content = str(e)
         assert len(content) < 1000
-        for keyword in {'free_text.xml:3', 'content', 'not allowed', 'element-only'}:
+        for keyword in {'indentation/param.yaml', 'line 2', 'mapping values are not allowed here'}:
             assert keyword in content
     else:
-        assert False, "This test should raise a ValueError."
+        assert False, "This test should raise a ScannerError."
 
 
-def test_unknown_node():
-    path_to_xml = os.path.join(BASE_DIR, 'unknown_node.xml')
-    tbs = TestTaxBenefitSystem(path_to_xml)
+def test_wrong_scale():
+    path_to_json = os.path.join(BASE_DIR, 'wrong_scale')
+    tbs = TestTaxBenefitSystem(path_to_json)
     try:
         tbs.compute_legislation()
-    except ValueError as e:
+    except jsonschema.exceptions.ValidationError as e:
         content = str(e)
-        assert len(content) < 1000
-        for keyword in {'unknown_node.xml:4', 'VALEU', 'not expected', 'VALUE'}:
+        assert len(content) < 1500
+        for keyword in {'is not valid under any of the given schemas'}:
             assert keyword in content
     else:
-        assert False, "This test should raise a ValueError."
+        assert False, "This test should raise a ValidationError."
+
+
+def test_wrong_type():
+    path_to_json = os.path.join(BASE_DIR, 'wrong_type')
+    tbs = TestTaxBenefitSystem(path_to_json)
+    try:
+        tbs.compute_legislation()
+    except jsonschema.exceptions.ValidationError as e:
+        content = str(e)
+        assert len(content) < 1000
+        for keyword in {'thistypedoesnotexist', 'node'}:
+            assert keyword in content
+    else:
+        assert False, "This test should raise a ValidationError."
 
 
 def test_wrong_value():
-    path_to_xml = os.path.join(BASE_DIR, 'wrong_value.xml')
-    tbs = TestTaxBenefitSystem(path_to_xml)
+    path_to_json = os.path.join(BASE_DIR, 'wrong_value')
+    tbs = TestTaxBenefitSystem(path_to_json)
     try:
         tbs.compute_legislation()
-    except ValueError as e:
+    except jsonschema.exceptions.ValidationError as e:
         content = str(e)
         assert len(content) < 1000
-        for keyword in {'wrong_value.xml:4', '0.3a', 'not a valid value', 'float'}:
+        for keyword in {'is not valid under any of the given schemas'}:
             assert keyword in content
     else:
-        assert False, "This test should raise a ValueError."
-
-
-def test_wrong_bareme():
-    path_to_xml = os.path.join(BASE_DIR, 'wrong_bareme.xml')
-    tbs = TestTaxBenefitSystem(path_to_xml)
-    try:
-        tbs.compute_legislation()
-    except ValueError as e:
-        content = str(e)
-        assert len(content) < 1000
-        for keyword in {'wrong_bareme.xml:15', 'TRANCHE', 'Missing child', 'SEUIL'}:
-            assert keyword in content
-    else:
-        assert False, "This test should raise a ValueError."
+        assert False, "This test should raise a ValidationError."
 
 
 def test_references():
     path_to_xml = os.path.join(BASE_DIR, 'references.xml')
     tbs = TestTaxBenefitSystem(path_to_xml)
     tbs.compute_legislation()
+
+
+def test_filesystem_hierarchy():
+    path_to_json = os.path.join(BASE_DIR, 'filesystem_hierarchy')
+    tbs = TestTaxBenefitSystem(path_to_json)
+    tbs.compute_legislation()
+    legislation = tbs.get_legislation_at_instant('2016-01-01')
+    assert legislation.node1.node2 == 1.0
+
+
+def test_yaml_hierarchy():
+    path_to_json = os.path.join(BASE_DIR, 'yaml_hierarchy')
+    tbs = TestTaxBenefitSystem(path_to_json)
+    tbs.compute_legislation()
+    legislation = tbs.get_legislation_at_instant('2016-01-01')
+    assert legislation.node1.node2 == 1.0
