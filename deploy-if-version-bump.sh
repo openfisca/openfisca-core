583b69890a315c8a769b7c9cfd5451bb72e94e60
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-08-01 15:27:54 +0200

    Uniformize circle CI config trough repos

--- deploy-if-version-bump.sh
+++ deploy-if-version-bump.sh
@@ -3,9 +3,9 @@
 set -e
 if ! git rev-parse `python setup.py --version` 2>/dev/null ; then
     git tag `python setup.py --version`
-    git push --tags
-    python setup.py bdist_wheel
-    twine upload dist/* --username openfisca-bot --password $PYPI_PASSWORD
+    git push --tags  # update the repository version
+    python setup.py bdist_wheel  # build this package in the dist directory
+    twine upload dist/* --username $PYPI_USERNAME --password $PYPI_PASSWORD  # publish
     ssh deploy-new-api@api-test.openfisca.fr
 else
     echo "No deployment - Existing release with `python setup.py --version` version number"
