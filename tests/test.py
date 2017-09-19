b87d2695a1955267bc66a88edcee713674e4e040
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-09-19 20:03:54 +0200

    Add more tests

--- tests/test.py
+++ tests/test.py
@@ -87,14 +87,33 @@ def test_on_leaf():
 
 def test_on_node():
     housing_occupancy_status = np.asarray(['owner', 'owner', 'tenant', 'tenant'])
-    assert_near(P.single[housing_occupancy_status].z1, [100,  100, 300, 300])
-    assert_near(P.single[housing_occupancy_status]['z1'], [100,  100, 300, 300])
+    node = P.single[housing_occupancy_status]
+    assert_near(node.z1, [100,  100, 300, 300])
+    assert_near(node['z1'], [100,  100, 300, 300])
 
 
 def test_double_fancy_indexing():
     zone = np.asarray(['z1', 'z2', 'z2', 'z1'])
     housing_occupancy_status = np.asarray(['owner', 'owner', 'tenant', 'tenant'])
     assert_near(P.single[housing_occupancy_status][zone], [100, 200, 400, 300])
+
+
+def test_double_fancy_indexing_on_node():
+    family_status = np.asarray(['single', 'couple', 'single', 'couple'])
+    housing_occupancy_status = np.asarray(['owner', 'owner', 'tenant', 'tenant'])
+    node = P[family_status][housing_occupancy_status]
+    assert_near(node.z1, [100, 500, 300, 700])
+    assert_near(node['z1'], [100, 500, 300, 700])
+    assert_near(node.z2, [200, 600, 400, 800])
+    assert_near(node['z2'], [200, 600, 400, 800])
+
+
+def test_triple_fancy_indexing():
+    family_status = np.asarray(['single', 'single', 'single', 'single', 'couple', 'couple', 'couple', 'couple'])
+    housing_occupancy_status = np.asarray(['owner', 'owner', 'tenant', 'tenant', 'owner', 'owner', 'tenant', 'tenant'])
+    zone = np.asarray(['z1', 'z2', 'z1', 'z2', 'z1', 'z2', 'z1', 'z2'])
+    assert_near(P[family_status][housing_occupancy_status][zone], [100, 200, 300, 400, 500, 600, 700, 800])
+
 
 # @raises(KeyError)
 # def test_wrong_key():
