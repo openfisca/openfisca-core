203c5571b5b0d121ed2067ae1c130798b39daf75
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-09-19 18:33:46 +0200

    Test fancy indexing on node

--- tests/test.py
+++ tests/test.py
@@ -6,25 +6,94 @@ from nose.tools import raises
 from openfisca_core.tools import assert_near
 from openfisca_core.parameters import ParameterNode, ParameterNodeAtInstant
 
-node = ParameterNode('taux', data = {
-    'z1': {
-      'values': {
-        "2015-01-01": {'value': 600},
+node = ParameterNode('rate', data = {
+  "single": {
+    "owner": {
+      "z1": {
+        "values": {
+          "2015-01-01": {
+            "value": 100
+          },
         }
       },
-    'z2': {
-      'values': {
-        "2015-01-01": {'value': 400},
+      "z2": {
+        "values": {
+          "2015-01-01": {
+            "value": 200
+          },
         }
       },
-    })
+    },
+    "tenant": {
+      "z1": {
+        "values": {
+          "2015-01-01": {
+            "value": 300
+          },
+        }
+      },
+      "z2": {
+        "values": {
+          "2015-01-01": {
+            "value": 400
+          },
+        }
+      },
+    }
+  },
+  "couple": {
+    "owner": {
+      "z1": {
+        "values": {
+          "2015-01-01": {
+            "value": 500
+          },
+        }
+      },
+      "z2": {
+        "values": {
+          "2015-01-01": {
+            "value": 600
+          },
+        }
+      },
+    },
+    "tenant": {
+      "z1": {
+        "values": {
+          "2015-01-01": {
+            "value": 700
+          },
+        }
+      },
+      "z2": {
+        "values": {
+          "2015-01-01": {
+            "value": 800
+          },
+        }
+      },
+    }
+  }
+})
 
-parameters = node._get_at_instant('2015-01-01')
+P = node._get_at_instant('2015-01-01')
+
 
 def test_on_leaf():
-    vector = np.asarray(['z1', 'z2', 'z2', 'z1'])
-    assert_near(parameters[vector], [600, 400, 400, 600])
+    zone = np.asarray(['z1', 'z2', 'z2', 'z1'])
+    assert_near(P.single.owner[zone], [100, 200, 200, 100])
 
+
+def test_on_node():
+    housing_occupancy_status = np.asarray(['owner', 'owner', 'tenant', 'tenant'])
+    assert_near(P.single[housing_occupancy_status].z1, [100,  100, 300, 300])
+    assert_near(P.single[housing_occupancy_status]['z1'], [100,  100, 300, 300])
+
+# @raises(KeyError)
+# def test_wrong_key():
+#     vector = np.asarray(['personnes_seules', 'couples', 'toto'])
+#     loyers_plafond.zone1[vector]
 
 
 # def test_inhomogenous():
