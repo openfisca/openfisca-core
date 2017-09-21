7593661135d97d5138ab8057a790222e3544ea88
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-09-21 09:46:24 +0200

    Add concat to the model api

--- openfisca_core/model_api.py
+++ openfisca_core/model_api.py
@@ -39,7 +39,7 @@ from .base_functions import (   # noqa analysis:ignore
     requested_period_last_value,
     )
 from .variables import Variable  # noqa analysis:ignore
-from .formula_helpers import apply_thresholds, switch  # noqa analysis:ignore
+from .formula_helpers import apply_thresholds, concat, switch  # noqa analysis:ignore
 from .periods import MONTH, YEAR, ETERNITY, period  # noqa analysis:ignore
 from .reforms import Reform  # noqa analysis:ignore
 from .parameters import load_parameter_file, ParameterNode, Scale, Bracket, Parameter, ValuesHistory  # noqa analysis:ignore
