8f59e7bba3db2200cb5725ddbcb18ecb9d0c76f2
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-09-29 18:37:47 +0200

    Build JSON trace

--- openfisca_core/tracers.py
+++ openfisca_core/tracers.py
@@ -2,8 +2,21 @@
 
 class Tracer(object):
 
+    def __init__(self):
+        self.trace = {}
+        self.stack = [self.trace]
+
     def start(self, variable_name, period):
-        print("Starting to compute '{}' for period '{}'".format(variable_name, period).encode('utf-8'))
+        key = "{}<{}>".format(variable_name, period).encode('utf-8')
+        current = self.stack[-1]
+        if not current.get(key):
+            current[key] = {'dependencies' : {}}
+        self.stack.append(current[key])
+        self.stack.append(current[key]['dependencies'])
 
     def stop(self, variable_name, period, result):
-        print("Finished computing '{}' for period '{}'. Result: '{}'".format(variable_name, period, result).encode('utf-8'))
+        self.stack.pop()  # Get out of dependencies
+        current = self.stack[-1]
+        current['value'] = result.tolist()
+        self.stack.pop()  # Get out of current node
+
