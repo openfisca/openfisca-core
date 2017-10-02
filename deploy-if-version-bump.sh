bfe7d873d9a2ec02e57f54760ecae03d2b5e76d7
sandcha <sandcha@users.noreply.github.com>
2017-10-02 18:24:56 +0200

    Remove reference to api-test url

--- deploy-if-version-bump.sh
+++ deploy-if-version-bump.sh
@@ -6,7 +6,7 @@ if ! git rev-parse `python setup.py --version` 2>/dev/null ; then
     git push --tags  # update the repository version
     python setup.py bdist_wheel  # build this package in the dist directory
     twine upload dist/* --username $PYPI_USERNAME --password $PYPI_PASSWORD  # publish
-    ssh deploy-new-api@api-test.openfisca.fr
+    ssh deploy-new-api@fr.openfisca.org
 else
     echo "No deployment - Only non-functional elements were modified in this change"
 fi
