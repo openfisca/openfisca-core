81a8a3c023887ba676d84199f6575f1203a11bdd
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-10-07 11:31:46 +0200

    Enable computation log printing

--- openfisca_core/tracers.py
+++ openfisca_core/tracers.py
@@ -6,6 +6,7 @@ class Tracer(object):
         self.requested_calculations = set()
         self.stack = []
         self.trace = {}
+        self._computation_log = []
 
     @staticmethod
     def _get_key(variable_name, period, **parameters):
@@ -14,6 +15,9 @@ class Tracer(object):
         return u"{}<{}>".format(variable_name, period).encode('utf-8')
 
     def start(self, variable_name, period, **parameters):
+        """
+            Record that OpenFisca started computing a variable.
+        """
         key = self._get_key(variable_name, period, **parameters)
 
         if self.stack:  # The variable is a dependency of another variable
@@ -25,8 +29,12 @@ class Tracer(object):
         if not self.trace.get(key):
             self.trace[key] = {'dependencies' : []}
         self.stack.append(key)
+        self._computation_log.append((key, len(self.stack)))
 
     def stop(self, variable_name, period, result, **parameters):
+        """
+            Record that OpenFisca finished computing a variable.
+        """
         key = self._get_key(variable_name, period, **parameters)
         expected_key = self.stack.pop()
 
@@ -35,4 +43,10 @@ class Tracer(object):
                 u"Something went wrong with the simulation tracer: result of '{0}' was expected, got results for '{1}' instead. This does not make sense as the last variable we started computing was '{0}'."
                 .format(expected_key, key).encode('utf-8')
                 )
-        self.trace[key]['value'] = result.tolist()
+        self.trace[key]['value'] = result.tolist()  # Cast numpy array into a python list
+
+
+    def print_computation_log(self):
+        for node, depth in self._computation_log:
+            value = self.trace[node]['value']
+            print("{}{} >> {}".format('  ' * depth, node, value))
