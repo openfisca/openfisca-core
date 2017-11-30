a6432743068053e299e8da434290c621339bc222
Anna-Livia <annalivia.cardin@gmail.com>
2017-11-30 10:59:19 +0100

    Extension tests are back

--- tests/web_api/__init__.py
+++ tests/web_api/__init__.py
@@ -5,9 +5,7 @@ from openfisca_core.scripts import build_tax_benefit_system
 
 TEST_COUNTRY_PACKAGE_NAME = 'openfisca_country_template'
 
-TEST_EXTENSION_PACKAGE_NAMES = ['openfisca_extension_template']
-
-tax_benefit_system = build_tax_benefit_system(TEST_COUNTRY_PACKAGE_NAME, extensions = TEST_EXTENSION_PACKAGE_NAMES, reforms = None)
+tax_benefit_system = build_tax_benefit_system(TEST_COUNTRY_PACKAGE_NAME, extensions = None, reforms = None)
 
 distribution = pkg_resources.get_distribution(TEST_COUNTRY_PACKAGE_NAME)
 
