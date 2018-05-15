611218e4fa3e8ea626fe67bcea6d95201007372a
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-05-15 14:35:53 -0400

    Fetch before publishing tag

--- deploy-if-version-bump.sh
+++ deploy-if-version-bump.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-echo "Publishing on Pypi"
+echo "**Publishing on Pypi**"
 
 python setup.py bdist_wheel  # build this package in the dist directory
 if ! twine upload dist/* --username $PYPI_USERNAME --password $PYPI_PASSWORD; then  # publish
   echo "Could not upload this package version on Pypi. This usually means that the version already exists, and that only non-functional elements were modified in this change. If this it not the case, check the error message above."
 fi
 
-echo "Publishing Github tag and deploying the web API"
+echo "**Publishing Github tag and deploying the web API**"
 
+git fetch
 if ! git rev-parse --quiet `python setup.py --version` 2>/dev/null ; then
     git tag `python setup.py --version`
     git push --tags  # update the repository version
