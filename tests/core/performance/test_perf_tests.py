d7ba1a4e7b6f6f2cc21419309124ecdd51bd70d5
Anna-Livia <annalivia.gomart@gmail.com>
2018-06-05 15:46:37 +0200

    Enhance formating

--- tests/core/performance/test_perf_tests.py
+++ tests/core/performance/test_perf_tests.py
@@ -1,7 +1,6 @@
 """
 This files tests the performance of the test runner (openfisca-run-test).
 It uses the existing tests from openfisca-france because it is the largest set we currently have.
-
 """
 
 import os
@@ -14,15 +13,14 @@ from openfisca_france import CountryTaxBenefitSystem
 start_time = time.time()
 tbs = CountryTaxBenefitSystem()
 time_spent = time.time() - start_time
-print("Generate TBS: --- %s seconds ---" % (time_spent))
+print("Generate Tax Benefit System: --- {}s seconds ---".format(time_spent))
 
 start_time = time.time()
 openfisca_france_dir = pkg_resources.get_distribution('OpenFisca-France').location
 yaml_tests_dir = os.path.join(openfisca_france_dir, 'tests', 'mes-aides.gouv.fr')
 time_spent = time.time() - start_time
-print("Locate directories: --- %s seconds ---" % (time_spent))
 
 start_time = time.time()
 run_tests(tbs, yaml_tests_dir)
 time_spent = time.time() - start_time
-print("Pass Mes-aides tests: --- %s seconds ---" % (time_spent))
+print("Pass Mes-aides tests: --- {}s seconds ---".format(time_spent))
