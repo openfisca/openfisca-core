c934de6b8d05eeada3affe1762061ad5f684ec0c
sandcha <sandcha@users.noreply.github.com>
2018-08-27 15:41:27 +0200

    Add Variable doc attribute to web api

--- openfisca_web_api/loader/variables.py
+++ openfisca_web_api/loader/variables.py
@@ -79,6 +79,9 @@ def build_variable(variable, country_package_metadata, tax_benefit_system):
             ),
         }
 
+    if variable.doc:
+        result['documentation'] = variable.doc
+
     if variable.reference:
         result['references'] = variable.reference
 
