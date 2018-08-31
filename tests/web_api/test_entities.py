97aea2ed231a4556e2446f3255c0d3595693d7d2
Anna-Livia <annalivia.gomart@gmail.com>
2018-08-31 11:05:23 +0200

    Make tests more robust

--- tests/web_api/test_entities.py
+++ tests/web_api/test_entities.py
@@ -4,8 +4,9 @@ from __future__ import unicode_literals, print_function, division, absolute_impo
 from http.client import OK
 from nose.tools import assert_equal
 import json
+import openfisca_country_template
+from openfisca_core.commons import to_unicode
 from . import subject
-
 
 entities_response = subject.get('/entities')
 
@@ -18,20 +19,8 @@ def test_return_code():
 
 def test_response_data():
     entities = json.loads(entities_response.data.decode('utf-8'))
-    test_documentation = '''
-Household is an example of a group entity.
-A group entity contains one or more individual·s.
-Each individual in a group entity has a role (e.g. parent or children). Some roles can only be held by a limited number of individuals (e.g. a 'first_parent' can only be held by one individual), while others can have an unlimited number of individuals (e.g. 'children').
+    test_documentation = to_unicode(openfisca_country_template.entities.Household.doc)
 
-Example:
-Housing variables (e.g. housing_tax') are usually defined for a group entity such as 'Household'.
-
-Usage:
-Check the number of individuals of a specific role (e.g. check if there is a 'second_parent' with household.nb_persons(Household.SECOND_PARENT)).
-Calculate a variable applied to each individual of the group entity (e.g. calculate the 'salary' of each member of the 'Household' with salaries = household.members('salary', period = MONTH); sum_salaries = household.sum(salaries)).
-
-For more information, see: https://openfisca.org/doc/coding-the-legislation/50_entities.html
-'''
     assert_equal(
         entities['household'],
         {
@@ -39,16 +28,16 @@ For more information, see: https://openfisca.org/doc/coding-the-legislation/50_e
             'documentation': test_documentation,
             'plural': 'households',
             'roles': {
+                'child': {
+                    'description': 'Other individuals living in the household.',
+                    'mandatory': False,
+                    'plural': 'children'
+                },
                 'parent': {
                     'description': 'The one or two adults in charge of the household.',
                     'mandatory': False,
                     'max': 2,
                     'plural': "parents"
-                    },
-                'child': {
-                    'description': 'Other individuals living in the household.',
-                    'mandatory': False,
-                    'plural': 'children'
                     }
                 }
             }
