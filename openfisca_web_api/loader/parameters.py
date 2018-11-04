c810ec16131e9e1919eda902de05bf0f6f048934
sandcha <sandcha@users.noreply.github.com>
2018-11-04 19:13:04 +0100

    Use same api builing function for scale with rates or amounts

--- openfisca_web_api/loader/parameters.py
+++ openfisca_web_api/loader/parameters.py
@@ -32,58 +32,25 @@ def get_value(date, values):
         return None
 
 
-def build_api_scale(scale):
-    # preprocess brackets
-
-    # try:
-    # brackets = [{
-    #     'thresholds': build_api_values_history(bracket.threshold),
-    #     'rates': build_api_values_history(bracket.rate) if 'rate' in bracket.children else None,
-    #     'amounts': build_api_values_history(bracket.amount) if 'amount' in bracket.children else None,
-    # } for bracket in scale.brackets]
-    # log.debug("---")
-    # log.debug(locals())
-    # log.debug("---")
-
-    brackets = []
-    for bracket in scale.brackets:
-        api_toto_bracket = { 'thresholds': build_api_values_history(bracket.threshold) }
-        
-        if 'rate' in bracket.children:
-            api_toto_bracket['rates'] = build_api_values_history(bracket.rate) 
-        elif 'amount' in bracket.children:
-            api_toto_bracket['amounts'] = build_api_values_history(bracket.amount)
-        
-        brackets.append(api_toto_bracket) 
-
-    log.debug("---")
-    log.debug(locals())
-    log.debug("---")
-
-    # except AttributeError:
-        #log.debug(locals())
-
-
+def build_api_scale(scale, value_key_name):
+    # preprocess brackets for a scale with 'rates' or 'amounts'
+    brackets = [{
+        'thresholds': build_api_values_history(bracket.threshold),
+        'values': build_api_values_history(getattr(bracket, value_key_name))
+    } for bracket in scale.brackets]
 
     dates = set(sum(
         [list(bracket['thresholds'].keys()) 
-        + list(bracket['rates'].keys() if 'rates' in bracket else [])
-        + list(bracket['amounts'].keys() if 'amounts' in bracket else []) for bracket in brackets],
+        + list(bracket['values'].keys()) for bracket in brackets],
         []))  # flatten the dates and remove duplicates
 
     # We iterate on all dates as we need to build the whole scale for each of them
     api_scale = {}
     for date in dates:
         for bracket in brackets:
-            rates_or_amounts = {}
-            if 'rates' in bracket:
-                rates_or_amounts = bracket['rates']
-            elif 'amounts' in bracket:
-                rates_or_amounts = bracket['amounts']
-
             threshold_value = get_value(date, bracket['thresholds'])
             if threshold_value is not None:
-                rate_or_amount_value = get_value(date, rates_or_amounts)
+                rate_or_amount_value = get_value(date, bracket['values'])
                 api_scale[date] = api_scale.get(date) or {}
                 api_scale[date][threshold_value] = rate_or_amount_value
 
@@ -122,7 +89,10 @@ def walk_node(node, parameters, path_fragments, country_package_metadata):
                 api_parameter['documentation'] = child.documentation.strip()
             api_parameter['values'] = build_api_values_history(child)
         elif isinstance(child, Scale):
-            api_parameter['brackets'] = build_api_scale(child)
+            if 'rate' in child.brackets[0].children:
+                api_parameter['brackets'] = build_api_scale(child, 'rate')
+            elif 'amount' in child.brackets[0].children:
+                api_parameter['brackets'] = build_api_scale(child, 'amount')
         elif isinstance(child, ParameterNode):
             if child.documentation:
                 api_parameter['documentation'] = child.documentation.strip()
