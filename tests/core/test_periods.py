377f10b0121f8c682f2e62db6af9163992cd58e5
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2018-11-12 00:10:33 +0100

    Rewrite test_periods with pytest

--- tests/core/test_periods.py
+++ tests/core/test_periods.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals, print_function, division, absolute_import
-from nose.tools import assert_equal, raises
+
+import pytest
 
 from openfisca_core.periods import Period, Instant, YEAR, MONTH, period
 from openfisca_core.commons import to_unicode
@@ -13,102 +14,102 @@ first_march = Instant((2014, 3, 1))
 # Test Period -> String
 
 def test_year():
-    assert_equal(to_unicode(Period((YEAR, first_jan, 1))), '2014')
+    assert to_unicode(Period((YEAR, first_jan, 1))) == '2014'
 
 
 def test_12_months_is_a_year():
-    assert_equal(to_unicode(Period((MONTH, first_jan, 12))), '2014')
+    assert to_unicode(Period((MONTH, first_jan, 12))) == '2014'
 
 
 def test_rolling_year():
-    assert_equal(to_unicode(Period((MONTH, first_march, 12))), 'year:2014-03')
-    assert_equal(to_unicode(Period((YEAR, first_march, 1))), 'year:2014-03')
+    assert to_unicode(Period((MONTH, first_march, 12))) == 'year:2014-03'
+    assert to_unicode(Period((YEAR, first_march, 1))) == 'year:2014-03'
 
 
 def test_month():
-    assert_equal(to_unicode(Period((MONTH, first_jan, 1))), '2014-01')
+    assert to_unicode(Period((MONTH, first_jan, 1))) == '2014-01'
 
 
 def test_several_months():
-    assert_equal(to_unicode(Period((MONTH, first_jan, 3))), 'month:2014-01:3')
-    assert_equal(to_unicode(Period((MONTH, first_march, 3))), 'month:2014-03:3')
+    assert to_unicode(Period((MONTH, first_jan, 3))) == 'month:2014-01:3'
+    assert to_unicode(Period((MONTH, first_march, 3))) == 'month:2014-03:3'
 
 
 def test_several_years():
-    assert_equal(to_unicode(Period((YEAR, first_jan, 3))), 'year:2014:3')
-    assert_equal(to_unicode(Period((YEAR, first_march, 3))), 'year:2014-03:3')
+    assert to_unicode(Period((YEAR, first_jan, 3))) == 'year:2014:3'
+    assert to_unicode(Period((YEAR, first_march, 3))) == 'year:2014-03:3'
 
 # Test String -> Period
 
 
 def test_parsing_year():
-    assert_equal(period('2014'), Period((YEAR, first_jan, 1)))
+    assert period('2014') == Period((YEAR, first_jan, 1))
 
 
 def test_parsing_month():
-    assert_equal(period('2014-01'), Period((MONTH, first_jan, 1)))
+    assert period('2014-01') == Period((MONTH, first_jan, 1))
 
 
 def test_parsing_rolling_year():
-    assert_equal(period('year:2014-03'), Period((YEAR, first_march, 1)))
+    assert period('year:2014-03') == Period((YEAR, first_march, 1))
 
 
 def test_parsing_several_months():
-    assert_equal(period('month:2014-03:3'), Period((MONTH, first_march, 3)))
+    assert period('month:2014-03:3') == Period((MONTH, first_march, 3))
 
 
 def test_parsing_several_years():
-    assert_equal(period('year:2014:2'), Period((YEAR, first_jan, 2)))
+    assert period('year:2014:2') == Period((YEAR, first_jan, 2))
 
 
-@raises(ValueError)
 def test_wrong_syntax_several_years():
-    period('2014:2')
+    with pytest.raises(ValueError):
+        period('2014:2')
 
 
-@raises(ValueError)
 def test_wrong_syntax_several_months():
-    period('2014-2:2')
+    with pytest.raises(ValueError):
+        period('2014-2:2')
 
 
-@raises(ValueError)
 def test_daily_period():
-    period('2014-2-3')
+    with pytest.raises(ValueError):
+        period('2014-2-3')
 
 
-@raises(ValueError)
 def test_daily_period_2():
-    period('2014-2-3:2')
+    with pytest.raises(ValueError):
+        period('2014-2-3:2')
 
 
-@raises(ValueError)
 def test_ambiguous_period():
-    period('month:2014')
+    with pytest.raises(ValueError):
+        period('month:2014')
 
 
-@raises(TypeError)
 def test_deprecated_signature():
-    period(MONTH, 2014)
+    with pytest.raises(TypeError):
+        period(MONTH, 2014)
 
 
-@raises(ValueError)
 def test_wrong_argument():
-    period({})
+    with pytest.raises(ValueError):
+        period({})
 
 
-@raises(ValueError)
 def test_wrong_argument_1():
-    period([])
+    with pytest.raises(ValueError):
+        period([])
 
 
-@raises(ValueError)
 def test_none():
-    period(None)
+    with pytest.raises(ValueError):
+        period(None)
 
 
-@raises(ValueError)
 def test_empty_string():
-    period('')
+    with pytest.raises(ValueError):
+        period('')
 
 
 def test_subperiods():
