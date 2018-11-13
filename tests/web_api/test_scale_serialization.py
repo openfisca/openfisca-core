758f0f9c81a49e26f28951b7ab4b314b8a72ae40
Laurent Bossavit <laurent.git@bossavit.com>
2018-11-13 09:47:31 +0100

    Make linter happy

--- tests/web_api/test_scale_serialization.py
+++ tests/web_api/test_scale_serialization.py
@@ -3,11 +3,12 @@
 from openfisca_web_api.loader.parameters import walk_node
 from openfisca_core.parameters import ParameterNode, Scale
 
+
 def test_amount_scale():
     parameters = []
-    metadata = {'location':'foo', 'version':'1', 'repository_url':'foo'}
+    metadata = {'location': 'foo', 'version': '1', 'repository_url': 'foo'}
     root_node = ParameterNode(data = {})
-    amount_scale_data = {'brackets':[{'amount':{'2014-01-01':{'value':0}},'threshold':{'2014-01-01':{'value':1}}}]}
+    amount_scale_data = {'brackets': [{'amount': {'2014-01-01': {'value': 0}}, 'threshold': {'2014-01-01': {'value': 1}}}]}
     scale = Scale('scale', amount_scale_data, 'foo')
     root_node.children['scale'] = scale
     walk_node(root_node, parameters, [], metadata)
