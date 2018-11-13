5d4ed71ae418de6930724c35f74a3842ceb10995
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-11-13 17:43:26 -0500

    Refactor tests

--- tests/core/test_tracer.py
+++ tests/core/test_tracer.py
@@ -34,34 +34,20 @@ zone = np.asarray(['z1', 'z2', 'z2', 'z1'])
 housing_occupancy_status = np.asarray(['owner', 'owner', 'tenant', 'tenant'])
 family_status = np.asarray(['single', 'couple', 'single', 'couple'])
 
-
-def test_parameters_fancy_indexing():
+def check_tracing(accessor, param_key):
     tracer = Tracer()
     tracer.record_calculation_start('A', '2015-01')
     tracingParams = TracingParameterNodeAtInstant(parameters('2015-01-01'), tracer)
-    param = tracingParams.rate.single.owner[zone]
-    assert_near(tracer.trace['A<2015-01>']['parameters']['rate.single.owner<2015-01-01>'], param)
+    param = accessor(tracingParams)
+    assert_near(tracer.trace['A<2015-01>']['parameters'][param_key], param)
 
-
-def test_parameters_fancy_indexing_node():
-    tracer = Tracer()
-    tracer.record_calculation_start('A', '2015-01')
-    tracingParams = TracingParameterNodeAtInstant(parameters('2015-01-01'), tracer)
-    param = tracingParams.rate.single[housing_occupancy_status].z1
-    assert_near(tracer.trace['A<2015-01>']['parameters']['rate.single<2015-01-01>'], param)
-
-
-def test_parameters_fancy_indexing_double():
-    tracer = Tracer()
-    tracer.record_calculation_start('A', '2015-01')
-    tracingParams = TracingParameterNodeAtInstant(parameters('2015-01-01'), tracer)
-    param = tracingParams.rate.single[housing_occupancy_status][zone]
-    assert_near(tracer.trace['A<2015-01>']['parameters']['rate.single<2015-01-01>'], param)
-
-
-def test_parameters_fancy_indexing_double_with_node():
-    tracer = Tracer()
-    tracer.record_calculation_start('A', '2015-01')
-    tracingParams = TracingParameterNodeAtInstant(parameters('2015-01-01'), tracer)
-    param = tracingParams.rate[family_status][housing_occupancy_status].z2
-    assert_near(tracer.trace['A<2015-01>']['parameters']['rate<2015-01-01>'], param)
+def test_x():
+    tests = [
+        (lambda P: P.rate.single.owner[zone], 'rate.single.owner<2015-01-01>'), # fancy indexing on leaf
+        (lambda P: P.rate.single[housing_occupancy_status].z1, 'rate.single<2015-01-01>'), # on a node
+        (lambda P: P.rate.single[housing_occupancy_status][zone], 'rate.single<2015-01-01>'), # double fancy indexing
+        (lambda P: P.rate[family_status][housing_occupancy_status].z2, 'rate<2015-01-01>'), # double + node
+        (lambda P: P.rate[family_status][housing_occupancy_status][zone], 'rate<2015-01-01>'), # triple
+        ]
+    for test in tests:
+        yield (check_tracing,) + test
