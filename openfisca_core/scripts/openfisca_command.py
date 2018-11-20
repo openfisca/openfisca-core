1a08a22a7e5a2f5ae22ed1ac6aba20db6cafab90
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-11-20 16:45:55 -0500

    Crash if no command is provided

--- openfisca_core/scripts/openfisca_command.py
+++ openfisca_core/scripts/openfisca_command.py
@@ -13,8 +13,9 @@ from openfisca_web_api.scripts.serve import define_command_line_options, main as
 def get_parser():
     parser = argparse.ArgumentParser()
 
-    subparsers = parser.add_subparsers(help='Available commands')
-    parser_serve = subparsers.add_parser('serve', help='Run the OpenFisca Web API')
+    subparsers = parser.add_subparsers(help = 'Available commands', dest = 'command')
+    subparsers.required = True  # Can be added as an argument of add_subparsers in Python 3
+    parser_serve = subparsers.add_parser('serve', help = 'Run the OpenFisca Web API')
     parser_serve = define_command_line_options(parser_serve)
 
     return parser
