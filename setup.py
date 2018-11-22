218696ffb9e8fdd63522287f610deb7b7e451c73
Philippe Blayo <philippe.blayo@gmail.com>
2018-11-22 15:34:43 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ dev_requirements = [
 
 setup(
     name = 'OpenFisca-Core',
-    version = '24.9.5',
+    version = '24.9.6',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.org',
     classifiers = [
