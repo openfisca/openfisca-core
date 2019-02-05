8b54e4e9f9a19014c3a15e2936bfdc6cce7bb3e4
Laurent Bossavit <laurent.git@bossavit.com>
2019-02-05 13:16:42 +0100

    Fix flake8 errors

--- tests/core/test_runner/test_yaml_runner.py
+++ tests/core/test_runner/test_yaml_runner.py
@@ -1,19 +1,24 @@
 from openfisca_core.tools.test_runner import _run_test
 
+
 import pytest
 
+
 class TaxBenefitSystem:
-	def __init__(self):
-		self.variables = {}
+    def __init__(self):
+        self.variables = {}
+
 
 class Simulation:
-	def __init__(self):
-		self.tax_benefit_system = TaxBenefitSystem()
-		self.entities = {}
-	def get_entity(self, plural = None):
-		return None
+    def __init__(self):
+        self.tax_benefit_system = TaxBenefitSystem()
+        self.entities = {}
+
+    def get_entity(self, plural = None):
+        return None
+
 
 def test_variable_not_found():
-	test = {"output": {"unknown_variable": 0}}	
-	with pytest.raises(ValueError):
-		_run_test(Simulation(), test)
+    test = {"output": {"unknown_variable": 0}}
+    with pytest.raises(ValueError):
+        _run_test(Simulation(), test)
