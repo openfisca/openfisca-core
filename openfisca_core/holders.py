a8c0fbd67b5eadcf0314c45db66dd3798e8abe37
Claire Leroy <cleroy@pse.local>
2019-02-11 10:43:38 +0100

    Correct typo

--- openfisca_core/holders.py
+++ openfisca_core/holders.py
@@ -88,7 +88,7 @@ class Holder(object):
 
             If the value is not known, return ``None``.
         """
-        if self.variable.is_neutralized or ((self.variable.end is not None) and (period.startd.date > self.variable.end)):
+        if self.variable.is_neutralized or ((self.variable.end is not None) and (period.start.date > self.variable.end)):
             return self.default_array()
         value = self._memory_storage.get(period, extra_params)
         if value is not None:
