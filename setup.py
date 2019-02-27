1482c97fcb4aa76abd94f936d42ff493bc4a6cac
Laurent Bossavit <laurent.git@bossavit.com>
2019-02-27 10:59:35 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ dev_requirements = [
 
 setup(
     name = 'OpenFisca-Core',
-    version = '26.0.1',
+    version = '26.0.2',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.org',
     classifiers = [
