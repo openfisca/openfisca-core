2d6cbfdcccce5e2bfc00a6e24da996d191fd3825
sandcha <sandcha@users.noreply.github.com>
2019-03-27 15:33:31 +0100

    Fix AttributeError on Simulation.debug for cloned simulations

--- openfisca_core/simulations.py
+++ openfisca_core/simulations.py
@@ -489,10 +489,8 @@ class Simulation(object):
             new.entities[entity.key] = entity
             setattr(new, entity_class.key, entity)  # create shortcut simulation.household (for instance)
 
-        if debug:
-            new_dict['debug'] = True
-        if trace:
-            new_dict['trace'] = True
+        new_dict['debug'] = debug
+        new_dict['trace'] = trace
         if debug or trace:
             if self.debug or self.trace:
                 new_dict['tracer'] = self.tracer.clone()
