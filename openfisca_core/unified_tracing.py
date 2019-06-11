b58c91ab94180039b540c3c2cee7ec7f7a25f5cb
sandcha <sandcha@users.noreply.github.com>
2019-06-11 12:14:35 +0200

    Focus unit tests on simple tracer

--- openfisca_core/unified_tracing.py
+++ openfisca_core/unified_tracing.py
@@ -1,27 +1,10 @@
 # -*- coding: utf-8 -*-
 
-class Frame:
-
-    def __init__(self, name, period):
-        self.name = name
-        self.period = period
-        self.stack = {}
-
-    def __enter__(self):
-        # to json
-        self.stack['name'] = self.name
-        self.stack['period'] = self.period  
-
-    def __exit__(self, type, value, traceback):
-        self.stack = {}
-
-
-
 
 class SimpleTracer:
 
     def __init__(self):
-        self._stack = {}
+        self._stack = []
 
     @property
     def stack(self):
@@ -33,42 +16,9 @@ class SimpleTracer:
         self._stack = stack
 
 
-    def new_frame(self, variable, period):
-        return Frame(variable, period)
+    def enter_calculation(self, variable, period):
+        self.stack.append({'name': variable, 'period': period})
 
 
-    def record(self, variable, period):
-        frame = self.new_frame(variable, period)
-        with frame:
-            if self.stack == {}:
-                self.stack.update(frame.stack)
-            else:
-                if 'children' not in self.stack:
-                    self.stack['children'] = []
-                self.stack['children'].append(frame.stack)
-
-
-    def pop(self):
-        print("simple pop")
-        print(self.stack)
-
-        if 'children' in self.stack:
-            children = self.stack['children']
-            children.pop()
-            if len(children) == 0:
-                self.stack.pop('children')
-        else:    
-            element = self.stack.pop('name')
-            print('The popped element is:', element)
-
-            element = self.stack.pop('period')
-            print('The popped element is:', element)
-
-        print('The dictionary is:', self.stack)
-
-
-class FullTracer(SimpleTracer):
-
-    def pop(self):
-        print("do not pop for full")
-        pass
+    def exit_calculation(self):
+        self.stack.pop()
--- tests/core/test_unified_tracing.py
+++ tests/core/test_unified_tracing.py
@@ -1,93 +1,52 @@
 # -*- coding: utf-8 -*-
 
-from openfisca_core.unified_tracing import SimpleTracer, FullTracer
-from pytest import fixture
+from openfisca_core.simulations import Simulation
+from openfisca_core.unified_tracing import SimpleTracer
 
 
-class DummySimulation:
-
-    def __init__(self, tracer):
-        self.tracer = tracer
-
-    @property
-    def stack(self):
-        return self.tracer.stack
-
+class StubSimulation:
+    
     def calculate(self, variable, period):
-        print("> calculate ", variable.__class__.__name__)
-
-        self.tracer.record(variable.__class__.__name__, period)     
-        variable.formula(period)
-        print("end formula", variable.__class__.__name__)
-        self.tracer.pop()
+        self.tracer.enter_calculation(variable, period)
+        self.tracer.exit_calculation()
 
 
-class v0:
+class MockTracer:
 
-    def __init__(self, simulation):
-        self.simulation = simulation
+    def enter_calculation(self, variable, period):
+        self.entered = True
 
-    def formula(self, period):
-        self.simulation.calculate(v1(), period) # v0 v1
-        self.simulation.calculate(v2(), period) # v0 v2
+    def exit_calculation(self):
+        self.exited = True
 
-
-class v1:
-
-    def formula(self, period):
-        pass
-
-
-class v2:
-
-    def formula(self, period):
-        pass
-
-
-@fixture
-def simulation_simple_tracing():
-    return DummySimulation(SimpleTracer())
-
-
-@fixture
-def simulation_full_tracing():
-    return DummySimulation(FullTracer())
 
 
 def test_stack_one_level():
     tracer = SimpleTracer()
-    frame = tracer.new_frame('toto', 2017)
-    with frame:
-        assert frame.stack == {'name': 'toto', 'period': 2017}  # [('toto', 2017)]
-    assert frame.stack == {}
-
-
-def test_record(simulation_full_tracing):
-    variable = v0(simulation_full_tracing)
-    period = '2019-01'
-
-    simulation_full_tracing.calculate(variable, period)
-
-    assert simulation_full_tracing.stack == {
-        'name': 'v0',
-        'period': '2019-01',  
-        'children': [
-            {
-                'name': 'v1',
-                'period': '2019-01'
-            },
-            {
-                'name': 'v2',
-                'period': '2019-01'
-            }
-        ]
-    }
-
-
-def test_pop(simulation_simple_tracing):
-    variable = v0(simulation_simple_tracing)
-    period = '2019-01'
-
-    simulation_simple_tracing.calculate(variable, period)
     
-    assert simulation_simple_tracing.stack == {}
+    tracer.enter_calculation('toto', 2017)
+    assert len(tracer.stack) == 1
+    assert tracer.stack == [{'name': 'toto', 'period': 2017}]
+
+    tracer.exit_calculation()
+    assert tracer.stack == []
+
+
+def test_stack_two_levels():
+    tracer = SimpleTracer()
+    
+    tracer.enter_calculation('toto', 2017)
+    tracer.enter_calculation('tata', 2017)
+    assert len(tracer.stack) == 2
+    assert tracer.stack == [{'name': 'toto', 'period': 2017}, {'name': 'tata', 'period': 2017}]
+
+    tracer.exit_calculation()
+    assert tracer.stack == [{'name': 'toto', 'period': 2017}]
+
+
+def test_tracer_contract():
+    simulation = StubSimulation()
+    simulation.tracer = MockTracer()
+    simulation.calculate('toto', 2017)
+    assert simulation.tracer.entered
+    assert simulation.tracer.exited
