a731178fc310c86b83ef02fd41997bb7d55f99de
Florian Pagnoux <florian.pagnoux@gmail.com>
2019-07-16 15:44:11 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ dev_requirements = [
 
 setup(
     name = 'OpenFisca-Core',
-    version = '34.3.0',
+    version = '34.3.1',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.org',
     classifiers = [
