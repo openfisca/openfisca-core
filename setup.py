b642bc4ed7b586eefb3aba9d4b3d90c8f595b675
sandcha <sandcha@users.noreply.github.com>
2019-08-19 16:50:37 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ dev_requirements = [
 
 setup(
     name = 'OpenFisca-Core',
-    version = '34.3.2',
+    version = '34.3.3',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.org',
     classifiers = [
