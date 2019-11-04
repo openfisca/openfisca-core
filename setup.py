cbd677ea5d18bf047510d84627d2ae143b035c5c
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2019-11-04 12:55:11 +0100

    Remove incompatible library enum34

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ from setuptools import setup, find_packages
 
 general_requirements = [
     'dpath == 1.4.2',
-    'enum34 >= 1.1.6',
     'pytest >= 4.4.1, < 6.0.0',  # For openfisca test
     'numpy >= 1.11, < 1.18',
     'psutil >= 5.4.7, < 6.0.0',
