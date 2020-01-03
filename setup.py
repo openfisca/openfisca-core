7af407635ff01a3533fa57f993352164328e3c36
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-01-03 15:07:01 +0100

    Bump version and changelog

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ dev_requirements = [
 
 setup(
     name = 'OpenFisca-Core',
-    version = '34.6.4',
+    version = '34.6.5',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.org',
     classifiers = [
