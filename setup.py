0026365c65632440d627fec6c115f6879e2be6ac
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-01-03 11:07:59 +0100

    Downgrade Numpy to 1.17

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 general_requirements = [
     'dpath == 1.4.2',
     'pytest >= 4.4.1, < 6.0.0',  # For openfisca test
-    'numpy >= 1.11, < 1.19',
+    'numpy >= 1.11, < 1.18',
     'psutil >= 5.4.7, < 6.0.0',
     'PyYAML >= 3.10',
     'sortedcontainers == 2.1.0',
