1c3c354ab685db18eb4d40f4109ec6fd471cfb15
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-02-08 12:46:34 +0100

    Fix style

--- openfisca_web_api/app.py
+++ openfisca_web_api/app.py
@@ -50,7 +50,8 @@ def create_app(tax_benefit_system,
         tracker = init_tracker(tracker_url, tracker_idsite, tracker_token)
 
     app = Flask(__name__)
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1)  # Fix request.remote_addr to get the real client IP address
+    # Fix request.remote_addr to get the real client IP address
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for = 1, x_host = 1)
     CORS(app, origins = '*')
 
     app.config['JSON_AS_ASCII'] = False  # When False, lets jsonify encode to utf-8
