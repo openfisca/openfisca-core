2d410a3e4ad883db420aaebe86e308799ac0a177
sandcha <sandcha@users.noreply.github.com>
2020-02-10 17:51:31 +0100

    Make flake8 happy

--- tests/web_api/test_calculate.py
+++ tests/web_api/test_calculate.py
@@ -288,6 +288,7 @@ def test_str_variable():
 
     assert response.status_code == OK
 
+
 def test_periods():
     simulation_json = json.dumps({
         "persons": {
@@ -311,8 +312,8 @@ def test_periods():
 
     response_json = json.loads(response.data.decode('utf-8'))
 
-    yearly_variable = dpath.get(response_json, 'households/_/housing_tax') # web api year is an int
+    yearly_variable = dpath.get(response_json, 'households/_/housing_tax')  # web api year is an int
     assert yearly_variable == {'2017': 200.0}
-    
-    monthly_variable = dpath.get(response_json, 'households/_/housing_occupancy_status') # web api month is a string
+
+    monthly_variable = dpath.get(response_json, 'households/_/housing_occupancy_status')  # web api month is a string
     assert monthly_variable == {'2017-01': 'tenant'}
