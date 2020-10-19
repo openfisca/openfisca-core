f3cdd03f1e02f7fd0614d4421b831794c01de66d
Thomas Guillet <guillet.thomas@gmail.com>
2020-10-19 18:38:07 +0200

    Update test reform to make it fail without a fix

--- tests/web_api/case_with_reform/reforms.py
+++ tests/web_api/case_with_reform/reforms.py
@@ -2,14 +2,18 @@ from openfisca_core.model_api import Variable, Reform, MONTH
 from openfisca_country_template.entities import Person
 
 
-class goes_to_school(Variable):
-    value_type = bool
-    default_value = True
-    entity = Person
-    label = "The person goes to school (only relevant for children)"
-    definition_period = MONTH
+def test_dynamic_variable():
+    class NewDynamicClass(Variable):
+        value_type = bool
+        default_value = True
+        entity = Person
+        label = "The person goes to school (only relevant for children)"
+        definition_period = MONTH
+
+    NewDynamicClass.__name__ = "goes_to_school"
+    return NewDynamicClass
 
 
 class add_variable_reform(Reform):
     def apply(self):
-        self.add_variable(goes_to_school)
+        self.add_variable(test_dynamic_variable())
