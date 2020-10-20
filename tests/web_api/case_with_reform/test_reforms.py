ad547cc503e5c8e61f3a7be0e9502c0f4de450ea
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-10-20 07:41:58 +0200

    Test reforms with country template ones

--- tests/web_api/case_with_reform/test_reforms.py
+++ tests/web_api/case_with_reform/test_reforms.py
@@ -1,16 +1,32 @@
-from http.client import OK
-from openfisca_core.scripts import build_tax_benefit_system
-from openfisca_web_api.app import create_app
+import http
+import pytest
+
+from openfisca_core import scripts
+from openfisca_web_api import app
+
+TEST_COUNTRY_PACKAGE_NAME = "openfisca_country_template"
+TEST_REFORMS_PATHS = [f"{TEST_COUNTRY_PACKAGE_NAME}.reforms.add_new_tax.add_new_tax"]
 
 
-TEST_COUNTRY_PACKAGE_NAME = 'openfisca_country_template'
-TEST_REFORMS_PATHS = ['tests.web_api.case_with_reform.reforms.add_variable_reform']
+# Create app as in 'openfisca serve' script
+@pytest.fixture
+def client():
+    tax_benefit_system = scripts.build_tax_benefit_system(
+        TEST_COUNTRY_PACKAGE_NAME,
+        extensions = None,
+        reforms = TEST_REFORMS_PATHS,
+        )
 
-# create app as in 'openfisca serve' script
-tax_benefit_system = build_tax_benefit_system(TEST_COUNTRY_PACKAGE_NAME, extensions = None, reforms = TEST_REFORMS_PATHS)
-reformed_subject = create_app(tax_benefit_system).test_client()
+    return app.create_app(tax_benefit_system).test_client()
 
 
-def test_return_code_existing_variable():
-    reform_variable_response = reformed_subject.get('/variable/goes_to_school')
-    assert reform_variable_response.status_code == OK
+def test_return_code_of_has_car_variable(client):
+    result = client.get("/variable/has_car")
+
+    assert result.status_code == http.client.OK
+
+
+def test_return_code_of_new_tax_variable(client):
+    result = client.get("/variable/new_tax")
+
+    assert result.status_code == http.client.OK
