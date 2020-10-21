d528cf8c1e357208d3fb1a0f510dc40d9d6ee5b2
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-10-21 12:34:46 +0200

    Test all country template's reforms on WebAPI

--- tests/web_api/case_with_reform/test_reforms.py
+++ tests/web_api/case_with_reform/test_reforms.py
@@ -8,6 +8,9 @@ TEST_COUNTRY_PACKAGE_NAME = "openfisca_country_template"
 TEST_REFORMS_PATHS = [
     f"{TEST_COUNTRY_PACKAGE_NAME}.reforms.add_dynamic_variable.add_dynamic_variable",
     f"{TEST_COUNTRY_PACKAGE_NAME}.reforms.add_new_tax.add_new_tax",
+    f"{TEST_COUNTRY_PACKAGE_NAME}.reforms.flat_social_security_contribution.flat_social_security_contribution",
+    f"{TEST_COUNTRY_PACKAGE_NAME}.reforms.modify_social_security_taxation.modify_social_security_taxation",
+    f"{TEST_COUNTRY_PACKAGE_NAME}.reforms.removal_basic_income.removal_basic_income",
     ]
 
 
@@ -37,5 +40,23 @@ def test_return_code_of_has_car_variable(client):
 
 def test_return_code_of_new_tax_variable(client):
     result = client.get("/variable/new_tax")
+
+    assert result.status_code == http.client.OK
+
+
+def test_return_code_of_social_security_contribution_variable(client):
+    result = client.get("/variable/social_security_contribution")
+
+    assert result.status_code == http.client.OK
+
+
+def test_return_code_of_social_security_contribution_parameter(client):
+    result = client.get("/parameter/taxes.social_security_contribution")
+
+    assert result.status_code == http.client.OK
+
+
+def test_return_code_of_basic_income_variable(client):
+    result = client.get("/variable/basic_income")
 
     assert result.status_code == http.client.OK
