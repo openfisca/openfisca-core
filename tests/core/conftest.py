88adca2693015eb85efbc1f8288a1c4fb5cee851
cescofran <francesco>
2021-01-19 22:40:48 +0100

    Refactor test_simulation_builder fixtures

--- tests/core/conftest.py
+++ tests/core/conftest.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
 
+from enum import Enum
+from datetime import date
 from pytest import fixture
 
 from openfisca_core.simulation_builder import SimulationBuilder
 from openfisca_country_template import CountryTaxBenefitSystem
+
+from openfisca_core.variables import Variable
+from openfisca_core.periods import ETERNITY
+from openfisca_core.entities import Entity, GroupEntity
+from openfisca_country_template.entities import Household
+from openfisca_core.indexed_enums import Enum as OFEnum
+
+
+class TestVariable(Variable):
+    definition_period = ETERNITY
+    value_type = float
+
+    def __init__(self, entity):
+        self.__class__.entity = entity
+        super().__init__()
 
 
 @fixture
@@ -32,3 +49,93 @@ def make_isolated_simulation(period):
         builder.default_period = period
         return builder.build_from_variables(tbs, data)
     return _make_simulation
+
+
+@fixture
+def simulation_builder():
+    return SimulationBuilder()
+
+
+@fixture
+def int_variable(persons):
+
+    class intvar(Variable):
+        definition_period = ETERNITY
+        value_type = int
+        entity = persons
+
+        def __init__(self):
+            super().__init__()
+
+    return intvar()
+
+
+@fixture
+def date_variable(persons):
+
+    class datevar(Variable):
+        definition_period = ETERNITY
+        value_type = date
+        entity = persons
+
+        def __init__(self):
+            super().__init__()
+
+    return datevar()
+
+
+@fixture
+def enum_variable():
+
+    class TestEnum(Variable):
+        definition_period = ETERNITY
+        value_type = OFEnum
+        dtype = 'O'
+        default_value = '0'
+        is_neutralized = False
+        set_input = None
+        possible_values = Enum('foo', 'bar')
+        name = "enum"
+
+        def __init__(self):
+            pass
+
+    return TestEnum()
+
+
+@fixture
+def persons():
+    class TestPersonEntity(Entity):
+        def get_variable(self, variable_name):
+            result = TestVariable(self)
+            result.name = variable_name
+            return result
+
+        def check_variable_defined_for_entity(self, variable_name):
+            return True
+
+    return TestPersonEntity("person", "persons", "", "")
+
+
+@fixture
+def group_entity():
+    class Household(GroupEntity):
+        def get_variable(self, variable_name):
+            result = TestVariable(self)
+            result.name = variable_name
+            return result
+
+        def check_variable_defined_for_entity(self, variable_name):
+            return True
+
+    roles = [{
+        'key': 'parent',
+        'plural': 'parents',
+        'max': 2
+        }, {
+        'key': 'child',
+        'plural': 'children'
+        }]
+
+    return Household("household", "households", "", "", roles)
+
--- tests/core/test_simulation_builder.py
+++ tests/core/test_simulation_builder.py
@@ -1,121 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from typing import Iterable
+from pytest import raises, approx
 
-from enum import Enum
-from datetime import date
-
-from pytest import raises, fixture, approx
-
-from openfisca_core.simulation_builder import SimulationBuilder, Simulation
+from openfisca_core.simulation_builder import Simulation
 from openfisca_core.tools import assert_near
 from openfisca_core.tools.test_runner import yaml
-from openfisca_core.entities import Entity, GroupEntity
 from openfisca_core.populations import Population
-from openfisca_core.variables import Variable
 from openfisca_country_template.entities import Household
 from openfisca_country_template.situation_examples import couple
 from openfisca_core.errors import SituationParsingError
-from openfisca_core.periods import ETERNITY
-from openfisca_core.indexed_enums import Enum as OFEnum
-
-
-class TestVariable(Variable):
-    definition_period = ETERNITY
-    value_type = float
-
-    def __init__(self, entity):
-        self.__class__.entity = entity
-        super().__init__()
-
-
-@fixture
-def simulation_builder():
-    return SimulationBuilder()
-
-
-@fixture
-def int_variable(persons):
-
-    class intvar(Variable):
-        definition_period = ETERNITY
-        value_type = int
-        entity = persons
-
-        def __init__(self):
-            super().__init__()
-
-    return intvar()
-
-
-@fixture
-def date_variable(persons):
-
-    class datevar(Variable):
-        definition_period = ETERNITY
-        value_type = date
-        entity = persons
-
-        def __init__(self):
-            super().__init__()
-
-    return datevar()
-
-
-@fixture
-def enum_variable():
-
-    class TestEnum(Variable):
-        definition_period = ETERNITY
-        value_type = OFEnum
-        dtype = 'O'
-        default_value = '0'
-        is_neutralized = False
-        set_input = None
-        possible_values = Enum('foo', 'bar')
-        name = "enum"
-
-        def __init__(self):
-            pass
-
-    return TestEnum()
-
-
-@fixture
-def persons():
-    class TestPersonEntity(Entity):
-        def get_variable(self, variable_name):
-            result = TestVariable(self)
-            result.name = variable_name
-            return result
-
-        def check_variable_defined_for_entity(self, variable_name):
-            return True
-
-    return TestPersonEntity("person", "persons", "", "")
-
-
-@fixture
-def group_entity():
-    class Household(GroupEntity):
-        def get_variable(self, variable_name):
-            result = TestVariable(self)
-            result.name = variable_name
-            return result
-
-        def check_variable_defined_for_entity(self, variable_name):
-            return True
-
-    roles = [{
-        'key': 'parent',
-        'plural': 'parents',
-        'max': 2
-        }, {
-        'key': 'child',
-        'plural': 'children'
-        }]
-
-    return Household("household", "households", "", "", roles)
 
 
 def test_build_default_simulation(tax_benefit_system, simulation_builder):
