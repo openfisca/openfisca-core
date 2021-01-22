9244c4e2a6633b1b8d937ad8e80301fe411ba2df
cescofran <francesco>
2021-01-22 22:00:10 +0100

    Refactor fixtures in test_opt_out_cache

--- tests/core/test_opt_out_cache.py
+++ tests/core/test_opt_out_cache.py
@@ -2,8 +2,6 @@
 
 from pytest import fixture
 
-from openfisca_core.simulation_builder import SimulationBuilder
-from openfisca_country_template import CountryTaxBenefitSystem
 from openfisca_country_template.entities import Person
 from openfisca_core.variables import Variable
 from openfisca_core.periods import MONTH
@@ -12,15 +10,6 @@ from openfisca_core.periods import MONTH
 @fixture
 def month():
     return '2016-05'
-
-
-@fixture
-def make_isolated_simulation(month):
-    def _make_simulation(tbs, data):
-        builder = SimulationBuilder()
-        builder.default_period = month
-        return builder.build_from_variables(tbs, data)
-    return _make_simulation
 
 
 class input(Variable):
@@ -50,31 +39,20 @@ class output(Variable):
         return person('intermediate', period)
 
 
-def get_filled_tbs():
-    tax_benefit_system = CountryTaxBenefitSystem()
+@fixture
+def simulation(tax_benefit_system, make_isolated_simulation):
+    tax_benefit_system.cache_blacklist = set(['intermediate'])
     tax_benefit_system.add_variables(input, intermediate, output)
-
-    return tax_benefit_system
-
-
-# TaxBenefitSystem instance declared after formulas
+    return make_isolated_simulation(tax_benefit_system, {'input': 1})
 
 
-tax_benefit_system = get_filled_tbs()
-
-
-tax_benefit_system.cache_blacklist = set(['intermediate'])
-
-
-def test_without_cache_opt_out(make_isolated_simulation, month):
-    simulation = make_isolated_simulation(tax_benefit_system, {'input': 1})
+def test_without_cache_opt_out(simulation, month):
     simulation.calculate('output', period = month)
     intermediate_cache = simulation.persons.get_holder('intermediate')
     assert(intermediate_cache.get_array(month) is not None)
 
 
-def test_with_cache_opt_out(make_isolated_simulation, month):
-    simulation = make_isolated_simulation(tax_benefit_system, {'input': 1})
+def test_with_cache_opt_out(simulation, month):
     simulation.debug = True
     simulation.opt_out_cache = True
     simulation.calculate('output', period = month)
@@ -82,11 +60,7 @@ def test_with_cache_opt_out(make_isolated_simulation, month):
     assert(intermediate_cache.get_array(month) is None)
 
 
-tax_benefit_system2 = get_filled_tbs()
-
-
-def test_with_no_blacklist(make_isolated_simulation, month):
-    simulation = make_isolated_simulation(tax_benefit_system2, {'input': 1})
+def test_with_no_blacklist(simulation, month):
     simulation.calculate('output', period = month)
     intermediate_cache = simulation.persons.get_holder('intermediate')
     assert(intermediate_cache.get_array(month) is not None)
