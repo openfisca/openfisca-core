40f1d0fb666e6e52659f40d4a06c59650c54048a
Sara Falamaki <sara@falamaki.id.au>
2021-01-29 21:35:21 +1100

    missing comma!

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ dev_requirements = [
     'flake8-print >= 3.1.0, < 4.0.0',
     'pytest-cov >= 2.6.1, < 3.0.0',
     'mypy >= 0.701, < 0.800',
-    'openfisca-country-template >= 3.10.0, < 4.0.0'
+    'openfisca-country-template >= 3.10.0, < 4.0.0',
     'openfisca-extension-template >= 1.2.0rc0, < 2.0.0'
     ] + api_requirements
 
