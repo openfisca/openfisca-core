cb0c9e697036bd72d048bb4e1a422feeca6c408d
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2021-03-09 02:23:36 +0100

    Parametrize 'test_shell_script'

--- tests/core/test_yaml.py
+++ tests/core/test_yaml.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import pkg_resources
 import os
+import pkg_resources
 import subprocess
 
-from pytest import mark, raises
+from pytest import fixture, mark
 
 import openfisca_extension_template
 
@@ -13,63 +13,53 @@ from openfisca_core.tools.test_runner import run_tests
 from .test_countries import tax_benefit_system
 
 
-openfisca_core_dir = pkg_resources.get_distribution("OpenFisca-Core").location
-yaml_tests_dir = os.path.join(openfisca_core_dir, "tests", "core", "yaml_tests")
+core_dir = pkg_resources.get_distribution("OpenFisca-Core").location
+yaml_dir = os.path.join(core_dir, "tests", "core", "yaml_tests")
+
 EXIT_OK = 0
 EXIT_KO = 1
 
 
-@mark.parametrize("path, options, expected", [
-    ("test_success.yaml", {}, EXIT_OK),
-    ("test_failure.yaml", {}, EXIT_KO),
-    ("test_relative_error_margin.yaml", {}, EXIT_OK),
-    ("failing_test_relative_error_margin.yaml", {}, EXIT_KO),
-    ("test_absolute_error_margin.yaml", {}, EXIT_OK),
-    ("failing_test_absolute_error_margin.yaml", {}, EXIT_KO),
-    (os.path.join(yaml_tests_dir, "directory"), {}, EXIT_OK),
-    ("test_with_reform.yaml", {}, EXIT_OK),
-    ("test_with_extension.yaml", {}, EXIT_OK),
-    ("test_with_anchors.yaml", {}, EXIT_OK),
-    (yaml_tests_dir, {}, EXIT_KO),
-    (yaml_tests_dir, {"name_filter": "success"}, EXIT_OK),
-    ])
+@fixture
+def path(request):
+    return os.path.join(*request.param)
+
+
+@fixture
+def make_call():
+    with open(os.devnull, "wb") as devnull:
+        def _make_call(command):
+            return subprocess.call(command, stdout = devnull, stderr = devnull)
+
+        yield _make_call
+
+
+@mark.parametrize("expected, path, options", [
+    (EXIT_OK, (yaml_dir, "test_success.yaml"), {}),
+    (EXIT_KO, (yaml_dir, "test_failure.yaml"), {}),
+    (EXIT_OK, (yaml_dir, "test_relative_error_margin.yaml"), {}),
+    (EXIT_KO, (yaml_dir, "failing_test_relative_error_margin.yaml"), {}),
+    (EXIT_OK, (yaml_dir, "test_absolute_error_margin.yaml"), {}),
+    (EXIT_KO, (yaml_dir, "failing_test_absolute_error_margin.yaml"), {}),
+    (EXIT_OK, (yaml_dir, os.path.join(yaml_dir, "directory")), {}),
+    (EXIT_OK, (yaml_dir, "test_with_reform.yaml"), {}),
+    (EXIT_OK, (yaml_dir, "test_with_extension.yaml"), {}),
+    (EXIT_OK, (yaml_dir, "test_with_anchors.yaml"), {}),
+    (EXIT_KO, (yaml_dir, yaml_dir), {}),
+    (EXIT_OK, (yaml_dir, yaml_dir), {"name_filter": "success"}),
+    ], indirect = ["path"])
 def test_yaml(path, options, expected):
-    yaml_path = os.path.join(yaml_tests_dir, path)
-    result = run_tests(tax_benefit_system, yaml_path, options)
+    result = run_tests(tax_benefit_system, path, options)
     assert result == expected
 
 
-def test_shell_script():
-    yaml_path = os.path.join(yaml_tests_dir, "test_success.yaml")
-    command = ["openfisca", "test", yaml_path, "-c", "openfisca_country_template"]
-
-    with open(os.devnull, "wb") as devnull:
-        result = subprocess.call(command, stdout = devnull, stderr = devnull)
-        assert result == EXIT_OK
-
-
-def test_failing_shell_script():
-    yaml_path = os.path.join(yaml_tests_dir, "test_failure.yaml")
-    command = ["openfisca", "test", yaml_path, "-c", "openfisca_dummy_country"]
-
-    with open(os.devnull, "wb") as devnull:
-        result = subprocess.call(command, stdout = devnull, stderr = devnull)
-        assert result == EXIT_KO
-
-
-def test_shell_script_with_reform():
-    yaml_path = os.path.join(yaml_tests_dir, "test_with_reform_2.yaml")
-    command = ["openfisca", "test", yaml_path, "-c", "openfisca_country_template", "-r", "openfisca_country_template.reforms.removal_basic_income.removal_basic_income"]
-
-    with open(os.devnull, "wb") as devnull:
-        result = subprocess.call(command, stdout = devnull, stderr = devnull)
-        assert result == EXIT_OK
-
-
-def test_shell_script_with_extension():
-    tests_dir = os.path.join(openfisca_extension_template.__path__[0], "tests")
-    command = ["openfisca", "test", tests_dir, "-c", "openfisca_country_template", "-e", "openfisca_extension_template"]
-
-    with open(os.devnull, "wb") as devnull:
-        result = subprocess.call(command, stdout = devnull, stderr = devnull)
-        assert result == EXIT_OK
+@mark.parametrize("expected, path, make_command", [
+    (EXIT_OK, (yaml_dir, "test_success.yaml"), lambda path: ["openfisca", "test", path, "-c", "openfisca_country_template"]),
+    (EXIT_KO, (yaml_dir, "test_failure.yaml"), lambda path: ["openfisca", "test", path, "-c", "openfisca_dummy_country"]),
+    (EXIT_OK, (yaml_dir, "test_with_reform_2.yaml"), lambda path: ["openfisca", "test", path, "-c", "openfisca_country_template", "-r", "openfisca_country_template.reforms.removal_basic_income.removal_basic_income"]),
+    (EXIT_OK, (openfisca_extension_template.__path__[0], "tests"), lambda path: ["openfisca", "test", path, "-c", "openfisca_country_template", "-e", "openfisca_extension_template"]),
+    ], indirect = ["path"])
+def test_shell_script(expected, path, make_command, make_call):
+    command = make_command(path)
+    result = make_call(command)
+    assert result == expected
