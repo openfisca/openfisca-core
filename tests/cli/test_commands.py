4d196df9c62d88a3a7e20d542a004897647dd3e1
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2021-03-10 04:42:55 +0100

    Replce call_count with assert_called

--- tests/cli/test_commands.py
+++ tests/cli/test_commands.py
@@ -15,10 +15,10 @@ def test_openfisca(cli_runner):
 def test_openfisca_serve(mocker, cli_runner):
     run_serve = mocker.patch("openfisca_cli.commands.run_serve")
     cli_runner.invoke(serve)
-    assert run_serve.call_count == 1
+    run_serve.assert_called_once()
 
 
 def test_openfisca_test(mocker, cli_runner):
     run_test = mocker.patch("openfisca_cli.commands.run_test")
     cli_runner.invoke(test)
-    assert run_test.call_count == 1
+    run_test.assert_called_once()
