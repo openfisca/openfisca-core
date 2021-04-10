6201b846fefaf9c64309718d887e9f2fcb65d8b2
Mauko Quiroga <hello@mauko.me>
2021-04-10 10:18:31 +0200

    Explicit case is one of variable with input

--- tests/core/test_axes.py
+++ tests/core/test_axes.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+import pytest
 from pytest import fixture, approx
 
 from openfisca_core.simulation_builder import SimulationBuilder
@@ -10,14 +10,7 @@ def simulation_builder():
     return SimulationBuilder()
 
 
-def test_add_axis_on_persons(simulation_builder, persons):
-    simulation_builder.add_person_entity(persons, {'Alicia': {}})
-    simulation_builder.register_variable('salary', persons)
-    simulation_builder.add_parallel_axis({'count': 3, 'name': 'salary', 'min': 0, 'max': 3000, 'period': '2018-11'})
-    simulation_builder.expand_axes()
-    assert simulation_builder.get_input('salary', '2018-11') == approx([0, 1500, 3000])
-    assert simulation_builder.get_count('persons') == 3
-    assert simulation_builder.get_ids('persons') == ['Alicia0', 'Alicia1', 'Alicia2']
+# With periods
 
 
 def test_add_axis_without_period(simulation_builder, persons):
@@ -27,6 +20,40 @@ def test_add_axis_without_period(simulation_builder, persons):
     simulation_builder.add_parallel_axis({'count': 3, 'name': 'salary', 'min': 0, 'max': 3000})
     simulation_builder.expand_axes()
     assert simulation_builder.get_input('salary', '2018-11') == approx([0, 1500, 3000])
+
+
+# With variables
+
+
+def test_add_axis_on_a_non_existing_variable(simulation_builder, persons):
+    simulation_builder.add_person_entity(persons, {'Alicia': {}})
+    simulation_builder.add_parallel_axis({'count': 3, 'name': 'ubi', 'min': 0, 'max': 3000, 'period': '2018-11'})
+
+    with pytest.raises(KeyError):
+        simulation_builder.expand_axes()
+
+
+def test_add_axis_on_an_existing_variable_with_input(simulation_builder, persons):
+    simulation_builder.add_person_entity(persons, {'Alicia': {'salary': {'2018-11': 1000}}})
+    simulation_builder.register_variable('salary', persons)
+    simulation_builder.add_parallel_axis({'count': 3, 'name': 'salary', 'min': 0, 'max': 3000, 'period': '2018-11'})
+    simulation_builder.expand_axes()
+    assert simulation_builder.get_input('salary', '2018-11') == approx([0, 1500, 3000])
+    assert simulation_builder.get_count('persons') == 3
+    assert simulation_builder.get_ids('persons') == ['Alicia0', 'Alicia1', 'Alicia2']
+
+
+# With entities
+
+
+def test_add_axis_on_persons(simulation_builder, persons):
+    simulation_builder.add_person_entity(persons, {'Alicia': {}})
+    simulation_builder.register_variable('salary', persons)
+    simulation_builder.add_parallel_axis({'count': 3, 'name': 'salary', 'min': 0, 'max': 3000, 'period': '2018-11'})
+    simulation_builder.expand_axes()
+    assert simulation_builder.get_input('salary', '2018-11') == approx([0, 1500, 3000])
+    assert simulation_builder.get_count('persons') == 3
+    assert simulation_builder.get_ids('persons') == ['Alicia0', 'Alicia1', 'Alicia2']
 
 
 def test_add_two_axes(simulation_builder, persons):
@@ -130,16 +157,6 @@ def test_add_perpendicular_axes(simulation_builder, persons):
     simulation_builder.expand_axes()
     assert simulation_builder.get_input('salary', '2018-11') == approx([0, 1500, 3000, 0, 1500, 3000])
     assert simulation_builder.get_input('pension', '2018-11') == approx([0, 0, 0, 2000, 2000, 2000])
-
-
-def test_axis_on_existing_variable(simulation_builder, persons):
-    simulation_builder.register_variable('salary', persons)
-    simulation_builder.add_person_entity(persons, {
-        'Alicia': {},
-        'Javier': {'salary': {'2018-11': 1000}},
-        })
-    simulation_builder.add_parallel_axis({'count': 10, 'name': 'salary', 'index': 0, 'min': 0, 'max': 3000, 'period': '2018-11'})
-    simulation_builder.expand_axes()
 
 
 # Integration test
