7311ceaeae5b7fd4da307e46c123aa5a3c18618a
Mauko Quiroga <hello@mauko.me>
2021-04-14 01:46:10 +0200

    Bump version to 35.3.5

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ dev_requirements = [
 
 setup(
     name = 'OpenFisca-Core',
-    version = '35.3.4',
+    version = '35.3.5',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.org',
     classifiers = [
