2b77189c7c873edd54ca788354da4059cbf5f04c
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2021-04-13 22:31:53 +0200

    Add ParameterScale initialisation from brackets

---
+++ tests/core/parameter_validation/test_parameter_scale_from_brackets.py
@@ -0,0 +1,21 @@
+from ..test_countries import tax_benefit_system
+
+import os
+from openfisca_core.parameters import ParameterScale
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+year = 2016
+
+
+def test_init_from_brackets():
+    scale = tax_benefit_system.parameters.taxes.social_security_contribution
+    brackets = scale.brackets.copy()
+
+    assert brackets is not scale.brackets
+
+    cloned_scale = ParameterScale(
+        name = "clone",
+        data = dict(description = "clone"),
+        brackets = brackets,
+        )
+    assert cloned_scale.brackets is not scale.brackets
+    assert cloned_scale.brackets == scale.brackets
