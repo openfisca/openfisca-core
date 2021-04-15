aa3157c04dd07165dab8fb25405db6a20aa356b5
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-04-15 17:26:28 +1000

    Remove `tax_benefit_system` and `subject` from "tests/web_api/__init__.py" since we now use the fixtures defined in "tests/fixtures"

--- tests/web_api/__init__.py
+++ tests/web_api/__init__.py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
-
 import pkg_resources
-from openfisca_web_api.app import create_app
-from openfisca_core.scripts import build_tax_benefit_system
 
 TEST_COUNTRY_PACKAGE_NAME = 'openfisca_country_template'
 distribution = pkg_resources.get_distribution(TEST_COUNTRY_PACKAGE_NAME)
-tax_benefit_system = build_tax_benefit_system(TEST_COUNTRY_PACKAGE_NAME, extensions = None, reforms = None)
-subject = create_app(tax_benefit_system).test_client()
