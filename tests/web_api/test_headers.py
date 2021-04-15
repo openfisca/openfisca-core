f3dbee3beecb0d55f5967620922a6672a0265ec7
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-04-15 17:10:50 +1000

    Modularise test_headers.py

--- tests/web_api/test_headers.py
+++ tests/web_api/test_headers.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from . import distribution, subject
-
-parameters_response = subject.get('/parameters')
+from . import distribution
 
 
-def test_package_name_header():
+def test_package_name_header(test_client):
+    parameters_response = test_client.get('/parameters')
     assert parameters_response.headers.get('Country-Package') == distribution.key
 
 
-def test_package_version_header():
+def test_package_version_header(test_client):
+    parameters_response = test_client.get('/parameters')
     assert parameters_response.headers.get('Country-Package-Version') == distribution.version
