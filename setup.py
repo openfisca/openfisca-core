390f194b295228f297c14b11ab5948ea50f0a986
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-04-15 17:56:55 +1000

    bump version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ dev_requirements = [
 
 setup(
     name = 'OpenFisca-Core',
-    version = '35.3.5',
+    version = '35.3.6',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.org',
     classifiers = [
