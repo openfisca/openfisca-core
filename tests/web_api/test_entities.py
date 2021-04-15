f906dbf3098e4670c9eee4108ba91af393b635f2
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-04-15 16:57:54 +1000

    Modularise `test_entities.py`

--- tests/web_api/test_entities.py
+++ tests/web_api/test_entities.py
@@ -1,24 +1,56 @@
 # -*- coding: utf-8 -*-
 
-from http.client import OK
+from http import client
 import json
-import openfisca_country_template
-from . import subject
+import pytest
 
-entities_response = subject.get('/entities')
+from openfisca_country_template import entities
+
+from openfisca_web_api import app
+
 
 # /entities
 
 
-def test_return_code():
-    assert entities_response.status_code == OK
+@pytest.fixture(scope="module")
+def test_client(tax_benefit_system):
+    """ This module-scoped fixture creates an API client for the TBS defined in the `tax_benefit_system`
+        fixture. This `tax_benefit_system` is mutable, so you can add/update variables. Example:
+
+        ```
+            from openfisca_country_template import entities
+            from openfisca_core import periods
+            from openfisca_core.variables import Variable
+            ...
+
+            class new_variable(Variable):
+                value_type = float
+                entity = entities.Person
+                definition_period = periods.MONTH
+                label = "New variable"
+                reference = "https://law.gov.example/new_variable"  # Always use the most official source
+
+            tax_benefit_system.add_variable(new_variable)
+            flask_app = app.create_app(tax_benefit_system)
+        ```
+    """
+
+    # Create the test API client
+    flask_app = app.create_app(tax_benefit_system)
+    return flask_app.test_client()
 
 
-def test_response_data():
-    entities = json.loads(entities_response.data.decode('utf-8'))
-    test_documentation = openfisca_country_template.entities.Household.doc.strip()
+def test_return_code(test_client):
+    entities_response = test_client.get('/entities')
+    assert entities_response.status_code == client.OK
 
-    assert entities['household'] == {
+
+def test_response_data(test_client):
+    entities_response = test_client.get('/entities')
+    entities_dict = json.loads(entities_response.data.decode('utf-8'))
+    test_documentation = entities.Household.doc.strip()
+
+    assert entities_dict['household'] == {
         'description': 'All the people in a family or group who live together in the same place.',
         'documentation': test_documentation,
         'plural': 'households',
