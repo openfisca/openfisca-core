e31735c5f7d8858d8f3bcef4670c7d1f2c3a5950
sandcha <sandcha@users.noreply.github.com>
2021-04-29 19:01:51 +0100

    Reapply 'Extend numpy support to v1.20'
    Done by a revert on 08bcaa5c revert: revert of Drop latest NumPy supported version to 1.18.x

--- openfisca_core/tracers/trace_node.py
+++ openfisca_core/tracers/trace_node.py
@@ -3,9 +3,14 @@ from __future__ import annotations
 import dataclasses
 import typing
 
-import numpy
+if typing.TYPE_CHECKING:
+    import numpy
 
-from openfisca_core.periods import Period
+    from openfisca_core.indexed_enums import EnumArray
+    from openfisca_core.periods import Period
+
+    Array = typing.Union[EnumArray, numpy.typing.ArrayLike]
+    Time = typing.Union[float, int]
 
 
 @dataclasses.dataclass
@@ -15,23 +20,35 @@ class TraceNode:
     parent: typing.Optional[TraceNode] = None
     children: typing.List[TraceNode] = dataclasses.field(default_factory = list)
     parameters: typing.List[TraceNode] = dataclasses.field(default_factory = list)
-    value: numpy.ndarray = None
+    value: typing.Optional[Array] = None
     start: float = 0
     end: float = 0
 
-    def calculation_time(self, round_ = True):
+    def calculation_time(self, round_: bool = True) -> Time:
         result = self.end - self.start
+
         if round_:
             return self.round(result)
+
         return result
 
-    def formula_time(self):
-        result = self.calculation_time(round_ = False) - sum(child.calculation_time(round_ = False) for child in self.children)
+    def formula_time(self) -> float:
+        children_calculation_time = sum(
+            child.calculation_time(round_ = False)
+            for child
+            in self.children
+            )
+
+        result = (
+            + self.calculation_time(round_ = False)
+            - children_calculation_time
+            )
+
         return self.round(result)
 
-    def append_child(self, node: TraceNode):
+    def append_child(self, node: TraceNode) -> None:
         self.children.append(node)
 
     @staticmethod
-    def round(time):
+    def round(time: Time) -> float:
         return float(f'{time:.4g}')  # Keep only 4 significant figures
