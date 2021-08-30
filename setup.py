a3b5b6f806ef9a42e189ebc5cd1f898f7871848a
Mauko Quiroga <hello@mauko.me>
2021-08-30 16:35:18 +0200

    Add flake8-rst-docstrings
    To check for any invalid rst syntax that could break the
    documentation.

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ dev_requirements = [
     'flake8 >= 3.9.0, < 4.0.0',
     'flake8-bugbear >= 19.3.0, < 20.0.0',
     'flake8-print >= 3.1.0, < 4.0.0',
+    'flake8-rst-docstrings < 1.0.0',
     'pytest-cov >= 2.6.1, < 3.0.0',
     'mypy >= 0.701, < 0.800',
     'openfisca-country-template >= 3.10.0, < 4.0.0',
