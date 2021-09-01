df51e0b5b799149c82faaec3cbfefe8952429653
Mauko Quiroga <hello@mauko.me>
2021-09-01 18:03:48 +0200

    Bump minor to 35.5.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ dev_requirements = [
 
 setup(
     name = 'OpenFisca-Core',
-    version = '35.4.2',
+    version = '35.5.0',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.org',
     classifiers = [
