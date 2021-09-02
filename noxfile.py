362d5d12a27bf6219e5110805ca885ecc393e07d
Mauko Quiroga <hello@mauko.me>
2021-09-02 11:15:42 +0200

    Run tests in parallel

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 
-# nox.options.reuse_existing_virtualenvs = True
+nox.options.reuse_existing_virtualenvs = True
 nox.options.sessions = ["test"]
 python_versions = ["3.7.11", "3.8.12", "3.9.7"]
 numpy_versions = ["1.18.5", "1.19.5", "1.20.3", "1.21.2"]
@@ -10,5 +10,6 @@ numpy_versions = ["1.18.5", "1.19.5", "1.20.3", "1.21.2"]
 @nox.parametrize("numpy", numpy_versions)
 def test(session, numpy):
     session.install(".[dev]")
+    session.install("pytest-parallel-39")
     session.install(f"numpy=={numpy}")
-    session.run("pytest", "-qx", *session.posargs)
+    session.run("pytest", "-qx", "--workers", "auto", *session.posargs)
