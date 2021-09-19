dcf0504f2141bda3a12454892e896f97b823a0e5
Mauko Quiroga <hello@mauko.me>
2021-09-19 22:46:21 +0200

    Add darglint for docstring lints

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ api_requirements = [
 
 dev_requirements = [
     'autopep8 >= 1.4.0, < 1.6.0',
+    'darglint == 1.8.0',
     'flake8 >= 3.9.0, < 4.0.0',
     'flake8-bugbear >= 19.3.0, < 20.0.0',
     'flake8-print >= 3.1.0, < 4.0.0',
