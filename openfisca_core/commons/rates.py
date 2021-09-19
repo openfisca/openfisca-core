8cec182937516475d2b337fbd5bfc7c8cd8435d1
Mauko Quiroga <hello@mauko.me>
2021-09-20 00:29:32 +0200

    Fix commons/rates doctests

--- openfisca_core/commons/rates.py
+++ openfisca_core/commons/rates.py
@@ -1,27 +1,124 @@
+from typing import Optional
+
 import numpy
 
+from openfisca_core.types import ArrayLike, ArrayType
 
-def average_rate(target = None, varying = None, trim = None):
-    '''
-    Computes the average rate of a targeted net income, according to the varying gross income.
 
-    :param target: Targeted net income, numerator
-    :param varying: Varying gross income, denominator
-    :param trim: Lower and upper bound of average rate to return
-    '''
+def average_rate(
+        target: ArrayType[float],
+        varying: ArrayLike[float],
+        trim: Optional[ArrayLike[float]] = None,
+        ) -> ArrayType[float]:
+    """Computes the average rate of a target net income.
+
+    Given a ``target`` net income, and according to the ``varying`` gross
+    income. Optionally, a ``trim`` can be applied consisting on the lower and
+    upper bounds of the average rate to be computed.
+
+    Note:
+        Usually, ``target`` and ``varying`` are the same size.
+
+    Args:
+        target: The targeted net income.
+        varying: The varying gross income.
+        trim: The lower and upper bounds of the average rate.
+
+    Returns:
+        :obj:`numpy.ndarray` of :obj:`float`:
+
+        The average rate for each target.
+
+        When ``trim`` is provided, values that are out of the provided bounds
+        are replaced by :obj:`numpy.nan`.
+
+    Examples:
+        >>> target = numpy.array([1, 2, 3])
+        >>> varying = [2, 2, 2]
+        >>> trim = [-1, .25]
+        >>> average_rate(target, varying, trim)
+        array([ nan,  0. , -0.5])
+
+    """
+
+    average_rate: ArrayType[float]
+
     average_rate = 1 - target / varying
+
     if trim is not None:
-        average_rate = numpy.where(average_rate <= max(trim), average_rate, numpy.nan)
-        average_rate = numpy.where(average_rate >= min(trim), average_rate, numpy.nan)
+
+        average_rate = numpy.where(
+            average_rate <= max(trim),
+            average_rate,
+            numpy.nan,
+            )
+
+        average_rate = numpy.where(
+            average_rate >= min(trim),
+            average_rate,
+            numpy.nan,
+            )
 
     return average_rate
 
 
-def marginal_rate(target = None, varying = None, trim = None):
-    # target: numerator, varying: denominator
-    marginal_rate = 1 - (target[:-1] - target[1:]) / (varying[:-1] - varying[1:])
+def marginal_rate(
+        target: ArrayType[float],
+        varying: ArrayType[float],
+        trim: Optional[ArrayLike[float]] = None,
+        ) -> ArrayType[float]:
+    """Computes the marginal rate of a target net income.
+
+    Given a ``target`` net income, and according to the ``varying`` gross
+    income. Optionally, a ``trim`` can be applied consisting of the lower and
+    upper bounds of the marginal rate to be computed.
+
+    Note:
+        Usually, ``target`` and ``varying`` are the same size.
+
+    Args:
+        target: The targeted net income.
+        varying: The varying gross income.
+        trim: The lower and upper bounds of the marginal rate.
+
+    Returns:
+        :obj:`numpy.ndarray` of :obj:`float`:
+
+        The marginal rate for each target.
+
+        When ``trim`` is provided, values that are out of the provided bounds
+        are replaced by :obj:`numpy.nan`.
+
+    Examples:
+        >>> target = numpy.array([1, 2, 3])
+        >>> varying = numpy.array([1, 2, 4])
+        >>> trim = [.25, .75]
+        >>> marginal_rate(target, varying, trim)
+        array([nan, 0.5])
+
+    """
+
+    marginal_rate: ArrayType[float]
+
+    marginal_rate = (
+        + 1
+        - (target[:-1]
+        - target[1:]) / (varying[:-1]
+        - varying[1:])
+        )
+
     if trim is not None:
-        marginal_rate = numpy.where(marginal_rate <= max(trim), marginal_rate, numpy.nan)
-        marginal_rate = numpy.where(marginal_rate >= min(trim), marginal_rate, numpy.nan)
+
+        marginal_rate = numpy.where(
+            marginal_rate <= max(trim),
+            marginal_rate,
+            numpy.nan,
+            )
+
+        marginal_rate = numpy.where(
+            marginal_rate >= min(trim),
+            marginal_rate,
+            numpy.nan,
+            )
 
     return marginal_rate
