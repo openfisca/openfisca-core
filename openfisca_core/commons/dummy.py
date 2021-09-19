df8d981064eaea62c871799a0d81a94542c32926
Mauko Quiroga <hello@mauko.me>
2021-09-20 00:27:40 +0200

    Fix commons/dummy doctests

--- openfisca_core/commons/dummy.py
+++ openfisca_core/commons/dummy.py
@@ -1,13 +1,18 @@
-import warnings
+from .decorators import deprecated
 
 
 class Dummy:
-    """A class that does nothing."""
+    """A class that did nothing.
 
+    Examples:
+        >>> Dummy()
+        <openfisca_core.commons.dummy.Dummy object...
+
+    .. deprecated:: 34.7.0
+        :class:`.Dummy` has been deprecated and it will be removed in 36.0.0.
+
+    """
+
+    @deprecated(since = "34.7.0", expires = "36.0.0")
     def __init__(self) -> None:
-        message = [
-            "The 'Dummy' class has been deprecated since version 34.7.0,",
-            "and will be removed in the future.",
-            ]
-        warnings.warn(" ".join(message), DeprecationWarning)
-        pass
+        ...
