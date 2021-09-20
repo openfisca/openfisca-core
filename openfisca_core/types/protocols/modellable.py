f37caee32eaccd10b42a607fa5576d835831726b
Mauko Quiroga <hello@mauko.me>
2021-09-20 03:43:41 +0200

    Add the modellable protocol

---
+++ openfisca_core/types/protocols/modellable.py
@@ -0,0 +1,15 @@
+from typing_extensions import Protocol
+
+
+class Modellable(Protocol):
+    """Base type for any modelable element of the legislation.
+
+    Type-checking against abstractions rather than implementations helps in
+    (a) decoupling the codebse, thanks to structural subtyping, and
+    (b) documenting/enforcing the blueprints of the different OpenFisca models.
+
+    .. versionadded:: 35.7.0
+
+    """
+
+    ...
