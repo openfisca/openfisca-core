d74d5a7d38f6effafaaff0104c2345adb73a4798
Mauko Quiroga <hello@mauko.me>
2021-09-20 03:33:29 +0200

    Add the rolifiable protocol

---
+++ openfisca_core/types/protocols/rolifiable.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+import abc
+from typing import Any, Optional, Iterator, Sequence, Tuple
+
+import typing_extensions
+from typing_extensions import Protocol
+
+from ..data_types import RoleLike
+from ._documentable import Documentable
+from .personifiable import Personifiable
+
+R = RoleLike
+G = Personifiable
+
+
+@typing_extensions.runtime_checkable
+class Rolifiable(Documentable, Protocol):
+    """Base type for any role-like model.
+
+    Type-checking against abstractions rather than implementations helps in
+    (a) decoupling the codebse, thanks to structural subtyping, and
+    (b) documenting/enforcing the blueprints of the different OpenFisca models.
+
+    .. versionadded:: 35.7.0
+
+    """
+
+    max: Optional[int]
+    subroles: Optional[Sequence[Rolifiable]]
+
+    @abc.abstractmethod
+    def __repr__(self) -> str:
+        ...
+
+    @abc.abstractmethod
+    def __str__(self) -> str:
+        ...
+
+    @abc.abstractmethod
+    def __iter__(self) -> Iterator[Tuple[str, Any]]:
+        ...
+
+    @abc.abstractmethod
+    def __init__(self, __arg1: R, __arg2: G) -> None:
+        ...
