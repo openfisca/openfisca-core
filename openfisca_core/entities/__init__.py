6b602bc53fc7c60c2d443c6cfec1c0aaf6b12716
Mauko Quiroga <hello@mauko.me>
2021-09-20 23:57:40 +0200

    Add richer description of entities

--- openfisca_core/entities/__init__.py
+++ openfisca_core/entities/__init__.py
@@ -4,6 +4,38 @@ Each rule system is comprised by legislation and regulations to be applied upon
 "someone". In legal and economical terms, "someone" is referred to as people:
 individuals, families, tax households, companies, and so on.
 
+People can be either human or non-human, that is a legal entity, also referred
+to as a legal person. Human or non-human, a person is an atomic element of a
+rule system: for example, in most legislations, a salary is invariably owed
+to an indivual, and payroll taxes by a company, as a juridical person. In
+OpenFisca, that atomic element is represented as an :class:`.Entity`.
+
+In other cases, legal and regulatory rules are defined for groups or clusters
+of people: for example, income tax is usually due by a tax household, that is
+a group of individuals. There may also be fiduciary entities where the members,
+legal entities, are collectively liable for a property tax. In OpenFisca, those
+cluster elements are represented as a :class:`.GroupEntity`.
+
+In the latter case, the atomic members of a given group may have a different
+:class:`Role` in the context of a specific rule: for example, income tax
+is due, in some legislations, by a tax household, where we find different
+roles as the declarant, the spouse, the children, and so on…
+
+What's important is that each rule, or in OpenFisca, a :class:`.Variable`,
+is defined either for an :class:`.Entity` or for a :class:`.GroupEntity`,
+and in the latter case, the way the rule is going to be applied depends
+on the attributes and roles of the members of the group.
+
+Finally, there is a distiction to be made between the "abstract" entities
+described by in a rule system, for example an individual, as in "any"
+individual, and an actual individual, like Mauko, Andrea, Mehdi, Seiko,
+or José.
+
+This module provides tools for modelling the former. For the actual
+"simulation" or "application" of any given :class:`.Variable` to a
+concrete individual or group of individuals, see :class:`.Population`
+and :class:`.GroupPopulation`.
+
 Official Public API:
     * :class:`.Entity`
     * :class:`.GroupEntity`
