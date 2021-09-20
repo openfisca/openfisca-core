c04f410c63c6eb71ee700e6b1b2569f5af58c3db
Mauko Quiroga <hello@mauko.me>
2021-09-20 03:46:42 +0200

    Move entity => variable logic to proxy

---
+++ openfisca_core/entities/_variable_proxy.py
@@ -0,0 +1,176 @@
+from __future__ import annotations
+
+import functools
+import os
+from typing import Any, Optional, Type
+
+from typing_extensions import Protocol
+
+from openfisca_core.types import Modellable, Personifiable, Representable
+
+DOC_URL = "https://openfisca.org/doc/coding-the-legislation"
+
+E = Personifiable
+T = Representable
+V = Modellable
+
+
+class Query(Protocol):
+    """A dummy class to "duck-check" :meth:`.TaxBenefitSystem.get_variable`."""
+
+    def __call__(
+            self,
+            __arg1: str,
+            __arg2: bool = False,
+            ) -> Optional["VariableProxy"]:
+        """See comment above."""
+
+        ...
+
+
+class VariableProxy:
+    """A `descriptor`_ to find an :obj:`.Entity`'s :obj:`.Variable`.
+
+    Attributes:
+        entity (:obj:`.Entity`, optional):
+            The :obj:`.Entity` ``owner`` of the descriptor.
+        tax_benefit_system (:obj:`.TaxBenefitSystem`, optional):
+            The :obj:`.Entity`'s :obj:`.TaxBenefitSystem`.
+        query (:meth:`.TaxBenefitSystem.get_variable`):
+            The method used to query the :obj:`.TaxBenefitSystem`.
+
+    Examples:
+        >>> from openfisca_core.entities import Entity
+        >>> from openfisca_core.taxbenefitsystems import TaxBenefitSystem
+        >>> from openfisca_core.variables import Variable
+
+        >>> entity = Entity(
+        ...     "individual",
+        ...     "individuals",
+        ...     "An individual",
+        ...     "The minimal legal entity on which a rule can be applied.",
+        ...    )
+
+        >>> class Variable(Variable):
+        ...     definition_period = "month"
+        ...     value_type = float
+        ...     entity = entity
+
+        >>> tbs = TaxBenefitSystem([entity])
+        >>> tbs.add_variable(Variable)
+        <openfisca_core.entities._variable_proxy.Variable...
+
+        >>> entity.tax_benefit_system = tbs
+
+        >>> entity.variables.get("Variable")
+        <...Variable...
+
+        >>> entity.variables.exists().get("Variable")
+        <...Variable...
+
+        >>> entity.variables.isdefined().get("Variable")
+        <...Variable...
+
+    .. _descriptor: https://docs.python.org/3/howto/descriptor.html
+
+    .. versionadded:: 35.7.0
+
+    """
+
+    entity: Optional[E] = None
+    tax_benefit_system: Optional[T] = None
+    query: Query
+
+    def __get__(self, entity: E, type: Type[E]) -> Optional[VariableProxy]:
+        """Binds :meth:`.TaxBenefitSystem.get_variable`."""
+
+        self.entity = entity
+
+        self.tax_benefit_system = getattr(
+            self.entity,
+            "tax_benefit_system",
+            None,
+            )
+
+        if self.tax_benefit_system is None:
+            return None
+
+        self.query = self.tax_benefit_system.get_variable
+
+        return self
+
+    def __set__(self, entity: E, value: Any) -> None:
+        NotImplemented
+
+    def get(self, variable_name: str) -> Optional[V]:
+        """Runs the query for ``variable_name``, based on the options given.
+
+        Args:
+            variable_name: The :obj:`.Variable` to be found.
+
+        Returns:
+            :obj:`.Variable` or :obj:`None`:
+            :obj:`.Variable` when the :obj:`.Variable` exists.
+            :obj:`None` when the :attr:`.tax_benefit_system` is not set.
+
+        Raises:
+            :exc:`.VariableNotFoundError`: When the :obj:`.Variable` doesn't exist.
+            :exc:`.ValueError`: When the :obj:`.Variable` exists but is defined
+            for another :obj:`.Entity`.
+
+        .. versionadded:: 35.7.0
+
+        """
+
+        if self.entity is None:
+            return NotImplemented
+
+        return self.query(variable_name)
+
+    def exists(self) -> VariableProxy:
+        """Sets ``check_existence`` to ``True``."""
+
+        self.query = functools.partial(
+            self.query,
+            check_existence = True,
+            )
+
+        return self
+
+    def isdefined(self) -> VariableProxy:
+        """Checks that ``variable_name`` is defined for :attr:`.entity`."""
+
+        # We assume that we're also checking for existance.
+        self.exists()
+
+        self.query = functools.partial(
+            self._isdefined,
+            self.query,
+            )
+
+        return self
+
+    def _isdefined(self, query: Query, variable_name: str, **any: Any) -> Any:
+        variable = query(variable_name)
+
+        if self.entity is None:
+            return None
+
+        if variable is None:
+            return None
+
+        if variable.entity is None:
+            return None
+
+        if self.entity != variable.entity:
+            message = os.linesep.join([
+                f"You tried to compute the variable '{variable_name}' for",
+                f"the entity '{self.entity.plural}'; however the variable",
+                f"'{variable_name}' is defined for the entity",
+                f"'{variable.entity.plural}'. Learn more about entities",
+                f"in our documentation: <{DOC_URL}/50_entities.html>.",
+                ])
+
+            raise ValueError(message)
+
+        return variable
---
+++ openfisca_core/entities/tests/test_variable_proxy.py
@@ -0,0 +1,160 @@
+import pytest
+
+from openfisca_core.entities import Entity
+from openfisca_core.errors import VariableNotFoundError
+from openfisca_core.taxbenefitsystems import TaxBenefitSystem
+from openfisca_core.variables import Variable
+
+from .._variable_proxy import VariableProxy
+
+
+@pytest.fixture()
+def variables():
+    """A variable descriptor."""
+
+    return VariableProxy()
+
+
+@pytest.fixture
+def entity():
+    """An entity."""
+
+    return Entity("", "individuals", "", "")
+
+
+@pytest.fixture
+def ThisVar(entity):
+    """A variable."""
+
+    return type(
+        "ThisVar",
+        (Variable,),
+        {
+            "definition_period": "month",
+            "value_type": float,
+            "entity": entity,
+            },
+        )
+
+
+@pytest.fixture
+def ThatVar():
+    """Another variable."""
+
+    class ThatVar(Variable):
+        definition_period = "month"
+        value_type = float
+        entity = Entity("", "martians", "", "")
+
+    return ThatVar
+
+
+@pytest.fixture
+def tbs(entity, ThisVar, ThatVar):
+    """A tax-benefit system."""
+
+    tbs = TaxBenefitSystem([entity])
+    tbs.add_variable(ThisVar)
+    tbs.add_variable(ThatVar)
+    return tbs
+
+
+def test_variables_without_variable_name(variables):
+    """Raises a TypeError when called without ``variable_name``."""
+
+    with pytest.raises(TypeError, match = "'variable_name'"):
+        variables.get()
+
+
+def test_variables_without_owner(variables):
+    """Returns NotImplemented when called without an ``owner``."""
+
+    assert variables.get("ThisVar") == NotImplemented
+
+
+def test_variables_setter(entity):
+    """Does nothing when tryng to set `variables`` to something else."""
+
+    obj = object()
+    entity.variables = obj
+    assert entity.variables != obj
+
+
+def test_variables_without_tax_benefit_system(entity):
+    """Returns None when called without a TaxBenefitSystem."""
+
+    assert not entity.variables
+
+
+def test_variables_when_exists(entity, tbs, ThisVar):
+    """Returns the variable when it exists."""
+
+    entity.tax_benefit_system = tbs
+    variable = entity.variables.get("ThisVar")
+    assert isinstance(variable, ThisVar)
+
+
+def test_variables_when_doesnt_exist(entity, tbs):
+    """Returns None when it does not."""
+
+    entity.tax_benefit_system = tbs
+    assert not entity.variables.get("OtherVar")
+
+
+def test_variables_when_exists_and_check_exists(entity, tbs, ThisVar):
+    """Raises a VariableNotFoundError when checking for existance."""
+
+    entity.tax_benefit_system = tbs
+    variable = entity.variables.exists().get("ThisVar")
+    assert isinstance(variable, ThisVar)
+
+
+def test_variables_when_doesnt_exist_and_check_exists(entity, tbs):
+    """Raises a VariableNotFoundError when checking for existance."""
+
+    entity.tax_benefit_system = tbs
+
+    with pytest.raises(VariableNotFoundError, match = "'OtherVar'"):
+        entity.variables.exists().get("OtherVar")
+
+
+def test_variables_when_exists_and_defined_for(entity, tbs, ThisVar):
+    """Returns the variable when it exists and defined for the entity."""
+
+    entity.tax_benefit_system = tbs
+    variable = entity.variables.isdefined().get("ThisVar")
+    assert isinstance(variable, ThisVar)
+
+
+def test_variables_when_exists_and_not_defined_for(entity, tbs):
+    """Raises a ValueError when it exists but defined for another var."""
+
+    entity.tax_benefit_system = tbs
+
+    with pytest.raises(ValueError, match = "'martians'"):
+        entity.variables.isdefined().get("ThatVar")
+
+
+def test_variables_when_doesnt_exist_and_check_defined_for(entity, tbs):
+    """Raises a VariableNotFoundError when it doesn't exist."""
+
+    entity.tax_benefit_system = tbs
+
+    with pytest.raises(VariableNotFoundError, match = "'OtherVar'"):
+        entity.variables.isdefined().get("OtherVar")
+
+
+def test_variables_composition(entity, tbs):
+    """Conditions can be composed."""
+
+    entity.tax_benefit_system = tbs
+
+    assert entity.variables.exists().isdefined().get("ThisVar")
+
+
+def test_variables_permutation(entity, tbs):
+    """Conditions can be permuted."""
+
+    entity.tax_benefit_system = tbs
+
+    assert entity.variables.isdefined().exists().get("ThisVar")
