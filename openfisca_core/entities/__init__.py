db9ce358f8efdd13d9226e600f86578498d6dee7
Mauko Quiroga <hello@mauko.me>
2021-09-20 03:06:07 +0200

    Add doc to entities module

--- openfisca_core/entities/__init__.py
+++ openfisca_core/entities/__init__.py
@@ -1,27 +1,50 @@
-# Transitional imports to ensure non-breaking changes.
-# Could be deprecated in the next major release.
-#
-# How imports are being used today:
-#
-#   >>> from openfisca_core.module import symbol
-#
-# The previous example provokes cyclic dependency problems
-# that prevent us from modularizing the different components
-# of the library so to make them easier to test and to maintain.
-#
-# How could them be used after the next major release:
-#
-#   >>> from openfisca_core import module
-#   >>> module.symbol()
-#
-# And for classes:
-#
-#   >>> from openfisca_core import module
-#   >>> module.Symbol()
-#
-# See: https://www.python.org/dev/peps/pep-0008/#imports
+"""Provides a way of representing the entities of a rule system.
 
-from .helpers import build_entity  # noqa: F401
-from .role import Role  # noqa: F401
+Each rule system is comprised by legislation and regulations to be applied upon
+"someone". In legal and economical terms, "someone" is referred to as people:
+individuals, families, tax households, companies, and so on.
+
+Official Public API:
+    * :class:`.Entity`
+    * :class:`.GroupEntity`
+    * :class:`.Role`
+    * :func:`.build_entity`
+
+Note:
+    How imports are being used today::
+
+        from openfisca_core.entities import *  # Bad
+        from openfisca_core.entities.helpers import build_entity  # Bad
+        from openfisca_core.entities.role import Role  # Bad
+
+    The previous examples provoke cyclic dependency problems, that prevents us
+    from modularizing the different components of the library, so as to make
+    them easier to test and to maintain.
+
+    How could them be used after the next major release::
+
+        from openfisca_core import entities
+        from openfisca_core.entities import Role
+
+        Role()  # Good: import classes as publicly exposed
+        entities.build_entity()  # Good: use functions as publicly exposed
+
+    .. seealso:: `PEP8#Imports`_ and `OpenFisca's Styleguide`_.
+
+    .. _PEP8#Imports:
+        https://www.python.org/dev/peps/pep-0008/#imports
+
+    .. _OpenFisca's Styleguide:
+        https://github.com/openfisca/openfisca-core/blob/master/STYLEGUIDE.md
+
+"""
+
+# Official Public API
+
 from .entity import Entity  # noqa: F401
 from .group_entity import GroupEntity  # noqa: F401
+from .role import Role  # noqa: F401
+from .helpers import build_entity  # noqa: F401
+
+__all__ = ["Entity", "GroupEntity", "Role"]
+__all__ = ["build_entity", *__all__]
