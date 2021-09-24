e96dd4640417b712be38a5e8e16cdf5c3f227b33
Mauko Quiroga <hello@mauko.me>
2021-09-24 19:38:05 +0200

    Remove deprecation expiration

--- openfisca_core/commons/dummy.py
+++ openfisca_core/commons/dummy.py
@@ -9,10 +9,11 @@ class Dummy:
         <openfisca_core.commons.dummy.Dummy object...
 
     .. deprecated:: 34.7.0
-        :class:`.Dummy` has been deprecated and it will be removed in 36.0.0.
+        :class:`.Dummy` has been deprecated and it will be removed in the
+        future.
 
     """
 
-    @deprecated(since = "34.7.0", expires = "36.0.0")
+    @deprecated(since = "34.7.0", expires = "in the future")
     def __init__(self) -> None:
         ...
