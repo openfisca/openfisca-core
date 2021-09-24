1117f07a214dfbea660d4ad6c8d13f6463c22cf9
Mauko Quiroga <hello@mauko.me>
2021-09-24 14:12:35 +0200

    Use decorator with already deprecated Dummy

--- openfisca_core/commons/dummy.py
+++ openfisca_core/commons/dummy.py
@@ -1,13 +1,19 @@
-import warnings
+from .decorators import deprecated
 
 
 class Dummy:
-    """A class that does nothing."""
+    """A class that did nothing.
 
+    Examples:
+        >>> Dummy()
+        <openfisca_core.commons.dummy.Dummy object...
+
+    .. deprecated:: 34.7.0
+        :class:`.Dummy` has been deprecated and it will be removed in the
+        future.
+
+    """
+
+    @deprecated(since = "34.7.0", expires = "in the future")
     def __init__(self) -> None:
-        message = [
-            "The 'Dummy' class has been deprecated since version 34.7.0,",
-            "and will be removed in the future.",
-            ]
-        warnings.warn(" ".join(message), DeprecationWarning)
-        pass
+        ...
