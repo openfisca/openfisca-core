dbbc29fd81f337a43771038644085d5700b4605b
Mauko Quiroga <hello@mauko.me>
2021-09-25 16:43:07 +0200

    Add the task protocol

--- openfisca_tasks/__main__.py
+++ openfisca_tasks/__main__.py
@@ -1,8 +1,21 @@
+import abc
 import sys
+
+from typing_extensions import Protocol
 
 import openfisca_tasks as tasks
 
+
+class HasExit(Protocol):
+    exit: int
+
+    @abc.abstractmethod
+    def __call__(self) -> None:
+        ...
+
+
 if __name__ == "__main__":
+    task: HasExit
     task = tasks.__getattribute__(sys.argv[1])()
     task()
     sys.exit(task.exit)
