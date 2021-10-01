11b216501c05daedf63ff0025a6e708155ea50f2
Mauko Quiroga <hello@mauko.me>
2021-10-01 18:26:39 +0200

    Fix install-deps

--- tasks/publish.mk
+++ tasks/publish.mk
@@ -10,16 +10,13 @@ install-publisher:
 
 ## Install build dependencies.
 install-deps:
-	@## Create a requirements file & install openfisca's dependencies.
 	@$(call print_help,$@:)
-	python setup.py egg_info
-	pip install $(shell grep -v "^\[" *.egg-info/requires.txt)
+	@python setup.py egg_info
+	@pip install $$(grep -v "^\[" *.egg-info/requires.txt)
 	@$(call print_pass,$@:)
 
 ## Build & install openfisca-core for deployment and publishing.
 build:
-	@## This allows us to be sure tests are run against the packaged version
-	@## of openfisca-core, the same we put in the hands of users and reusers.
 	@$(call print_help,$@:)
 	@python setup.py bdist_wheel
 	@find dist -name "*.whl" -exec pip install --force-reinstall --no-dependencies {} \;
