e731d20425da2d017aa3c81e10bfe87baab4577d
Mauko Quiroga <hello@mauko.me>
2021-10-01 21:46:38 +0200

    Read files

--- openfisca_tasks/_check_version.py
+++ openfisca_tasks/_check_version.py
@@ -1,5 +1,9 @@
+from __future__ import annotations
+
+import ast
+import textwrap
 import subprocess
-from typing import Sequence
+from typing import Generator, Sequence
 
 from typing_extensions import Literal
 
@@ -15,10 +19,8 @@ IGNORE_DIFF_ON: Sequence[str]
 IGNORE_DIFF_ON = (
     ".circleci/*",
     ".github/*",
-    "openfisca_tasks/*",
     "tests/*",
     ".gitignore"
-    "conftest.py",
     "CONTRIBUTING.md",
     "LICENSE*",
     "Makefile",
@@ -48,6 +50,37 @@ CHANGED = \
     .decode("utf-8") \
     .split()
 
+ACTUAL: Sequence[str]
+ACTUAL = [
+    file
+    for file in
+    subprocess
+    .run(
+        ["git", "ls-tree", "-r", "HEAD", "--name-only"],
+        stdout = subprocess.PIPE,
+        )
+    .stdout
+    .decode("utf-8")
+    .split()
+    if file.endswith(".py")
+    ]
+
+BEFORE: Sequence[str]
+BEFORE = [
+    file
+    for file in
+    subprocess
+    .run(
+        ["git", "ls-tree", "-r", VERSION, "--name-only"],
+        stdout = subprocess.PIPE,
+        )
+    .stdout
+    .decode("utf-8")
+    .split()
+    if file.endswith(".py")
+    ]
+
+
 class CheckVersion:
 
     exit: Literal[0, 1]
@@ -57,6 +90,8 @@ class CheckVersion:
 
     def __call__(self, progress: SupportsProgress) -> None:
         self.progress = progress
+        self.actual = tuple(self._parse_actual())
+        self.before = tuple(self._parse_before())
 
         if self._haschanges():
             self.exit = EXIT_KO
@@ -71,3 +106,43 @@ class CheckVersion:
                 changed = True
 
         return changed
+
+    def _parse_actual(self) -> Generator[ast.Module, None, None]:
+        source: str
+        total: int
+
+        total = len(ACTUAL)
+
+        self.progress.init()
+
+        for count, filename in enumerate(ACTUAL):
+            with open(filename, "r") as file:
+                source = textwrap.dedent(file.read())
+                yield ast.parse(source, filename, "exec")
+                self.progress.push(count, total)
+
+        self.progress.wipe()
+
+    def _parse_before(self) -> Generator[ast.Module, None, None]:
+        content: str
+        source: str
+        total: int
+
+        total = len(BEFORE)
+
+        self.progress.init()
+
+        for count, filename in enumerate(BEFORE):
+            content = \
+                subprocess \
+                .run(
+                    ["git", "show", f"master:{filename}"],
+                    stdout = subprocess.PIPE) \
+                .stdout \
+                .decode("utf-8")
+
+            source = textwrap.dedent(content)
+            yield ast.parse(source, filename, "exec")
+            self.progress.push(count, total)
+
+        self.progress.wipe()
