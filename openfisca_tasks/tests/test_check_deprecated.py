6f44c2988fc7736bf13718549d6b5890428296bf
Mauko Quiroga <hello@mauko.me>
2021-10-01 13:13:04 +0200

    Fix check deprecated test

--- openfisca_tasks/tests/test_check_deprecated.py
+++ openfisca_tasks/tests/test_check_deprecated.py
@@ -1,10 +1,14 @@
+import inspect
 import os
 import sys
 import tempfile
+from typing import NamedTuple
 
 import pytest
 
 from openfisca_tasks import CheckDeprecated
+
+from .._protocols import SupportsProgress
 
 
 class Module:
@@ -31,31 +35,62 @@ class Module:
         self.file.close()
 
 
-def test_find_deprecated(capsys):
+@pytest.fixture
+def progress():
+
+    def name():
+        return inspect.stack()[1][3]
+
+    class Call(NamedTuple):
+        name: str
+        args: str = None
+
+    class ProgressBar(SupportsProgress):
+
+        def init(self):
+            self.called = []
+
+        def push(self, __count, __total):
+            ...
+
+        def warn(self, message):
+            self.called.append(Call(name(), message))
+
+        def fail(self):
+            self.called.append(Call(name()))
+
+        def wipe(self):
+            ...
+
+    return ProgressBar()
+
+
+def test_find_deprecated(progress):
     """Prints out the features marked as deprecated."""
 
     with Module() as (file, name):
         checker = CheckDeprecated([file.name])
-        checker()
+        checker(progress)
 
     with pytest.raises(SystemExit) as exit:
         sys.exit(checker.exit)
 
     assert exit.value.code == os.EX_OK
-    assert f"[i] {name}.function:5" in capsys.readouterr().out
+    assert progress.called[-1].name == "warn"
+    assert f"{name}.function:5" in progress.called[-1].args
 
 
-def test_find_deprecated_when_expired(capsys):
+def test_find_deprecated_when_expired(progress):
     """Raises an error when at least one deprecation has expired."""
 
     version = "1.0.0"
 
     with Module(version) as (file, _):
         checker = CheckDeprecated([file.name], version)
-        checker()
+        checker(progress)
 
     with pytest.raises(SystemExit) as exit:
         sys.exit(checker.exit)
 
     assert exit.value.code != os.EX_OK
-    assert "[!]" in capsys.readouterr().out
+    assert progress.called[-1].name == "fail"
