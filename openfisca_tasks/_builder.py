a3633debd3129170b63710994359bae6d758dae7
Mauko Quiroga <hello@mauko.me>
2021-10-03 20:01:37 +0200

    Document builder

--- openfisca_tasks/_builder.py
+++ openfisca_tasks/_builder.py
@@ -12,6 +12,7 @@ from typing import (
     Optional,
     Sequence,
     Tuple,
+    Union,
     )
 
 from openfisca_core.indexed_enums import Enum
@@ -19,16 +20,22 @@ from openfisca_core.indexed_enums import Enum
 
 @dataclasses.dataclass(frozen = True)
 class ArgType:
+    """An argument type."""
+
     name: str
 
 
 @dataclasses.dataclass(frozen = True)
 class RetType:
+    """A return type."""
+
     name: str
 
 
 @dataclasses.dataclass(frozen = True)
 class Argument:
+    """An argument."""
+
     name: str
     types: Optional[Tuple[ArgType, ...]] = None
     default: Optional[str] = None
@@ -36,6 +43,8 @@ class Argument:
 
 @dataclasses.dataclass(frozen = True)
 class Contract:
+    """A contract, that is arguments and returns."""
+
     name: str
     file: str
     arguments: Optional[Sequence[Argument]] = None
@@ -43,6 +52,8 @@ class Contract:
 
 
 class Suffix(Enum):
+    """An enum to find unique contract names."""
+
     SEMEL = ""
     BIS = "(bis)"
     TER = "(ter)"
@@ -52,10 +63,18 @@ class Suffix(Enum):
 
 @dataclasses.dataclass
 class ContractBuilder(ast.NodeVisitor):
-    """
+    """Builds contracts from the abstract syntax-tree of a revision.
+
+    Attributes:
+        files: The files to build contracts from.
+        count: An iteration counter.
+        contracts: The built contracts.
+
     Examples:
         >>> ContractBuilder(["file.py"])
         ContractBuilder(files=['file.py'], count=0, contracts=())
+
+    .. versionadded:: 36.1.0
 
     """
 
@@ -65,18 +84,28 @@ class ContractBuilder(ast.NodeVisitor):
 
     @property
     def total(self) -> int:
-        """
+        """The total number of files to build contracts from.
+
+        Returns:
+            int: The number of files.
+
         Examples:
             >>> builder = ContractBuilder(["file.py"])
             >>> builder.total
             1
+
+        .. versionadded:: 36.1.0
 
         """
 
         return len(self.files)
 
     def __call__(self, source: str) -> None:
-        """
+        """Builds all contracts from the passed source code.
+
+        Arguments:
+            source: The source code to build contracts from.
+
         Examples:
             >>> builder = ContractBuilder(["file.py"])
             >>> source = [
@@ -114,6 +143,8 @@ class ContractBuilder(ast.NodeVisitor):
             >>> builder.count
             1
 
+        .. versionadded:: 36.1.0
+
         """
 
         node = ast.parse(source, self.files[self.count], "exec")
@@ -121,6 +152,16 @@ class ContractBuilder(ast.NodeVisitor):
         self.count += 1
 
     def visit_FunctionDef(self, node: ast.FunctionDef) -> None:
+        """An :obj:`ast` node visitor."""
+
+        file: str
+        path: pathlib.Path
+        name: str
+        args: Sequence[ast.arg]
+        kwds: Sequence[ast.arg]
+        posargs: Tuple[Argument, ...]
+        keyargs: Tuple[Argument, ...]
+        returns: Tuple[RetType, ...]
         contract: Contract
 
         # We look for the corresponding ``file``.
@@ -144,18 +185,12 @@ class ContractBuilder(ast.NodeVisitor):
         name = self._build_unique_name(path, node, iter(Suffix))
 
         # We build all positional arguments.
-        posargs = functools.reduce(
-            self._build_posarg(node),
-            node.args.args,
-            (),
-            )
+        args = node.args.args
+        posargs = functools.reduce(self._build_posarg(node), args, ())
 
         # We build all keyword arguments.
-        keyargs = functools.reduce(
-            self._build_keyarg(node),
-            node.args.kwonlyargs,
-            (),
-            )
+        kwds = node.args.kwonlyargs
+        keyargs = functools.reduce(self._build_keyarg(node), kwds, ())
 
         # We build the return types.
         returns = self._build_returns(node)
@@ -172,7 +207,11 @@ class ContractBuilder(ast.NodeVisitor):
             node: ast.FunctionDef,
             suffixes: Iterator[Suffix],
             ) -> str:
+        """Builds an unique contract name."""
+
         module: str
+        name: str
+        decorator: ast.expr
 
         # We build the module name with the name of the parent path, a
         # folder, and the name of the file, without the extension.
@@ -200,6 +239,8 @@ class ContractBuilder(ast.NodeVisitor):
         return self._build_unique_name(path, node, suffixes)
 
     def _build_posarg(self, node: ast.FunctionDef) -> Callable[..., Any]:
+        """Curryfies the positional arguments builder."""
+
         return functools.partial(
             self._build_argument,
             args = node.args.args,
@@ -207,42 +248,88 @@ class ContractBuilder(ast.NodeVisitor):
             )
 
     def _build_keyarg(self, node: ast.FunctionDef) -> Callable[..., Any]:
+        """Curryfies the keyword arguments builder."""
+
         return functools.partial(
             self._build_argument,
             args = node.args.kwonlyargs,
             defaults = node.args.kw_defaults,
             )
 
-    def _build_argument(self, acc, node, args, defaults) -> Sequence[Argument]:
-        type_ = self._build(node.annotation, ArgType)
+    def _build_argument(
+            self,
+            acc: Sequence[Argument],
+            node: ast.arg,
+            args: Sequence[Any],
+            defaults: Sequence[Any],
+            ) -> Sequence[Argument]:
+        """Builds an argument."""
 
-        if type_ is not None and not isinstance(type_, tuple):
-            type_ = type_,
+        types: Optional[Tuple[ArgType, ...]]
+        default: Optional[str]
+        argument: Argument
 
-        if len(defaults) > 0 and len(args) - len(defaults) < len(acc) + 1:
-            default = defaults[
-                + len(acc)
-                + len(defaults)
-                - len(args)
-                ]
-
-            default = self._build(default)
-        else:
-            default = None
-
-        argument = Argument(node.arg, type_, default)
+        types = self._build_arg_types(node)
+        default = self._build_arg_default(len(acc), len(args), defaults)
+        argument = Argument(node.arg, types, default)
 
         return (*acc, argument)
 
+    def _build_arg_types(self, node: ast.arg) -> Optional[Tuple[ArgType, ...]]:
+        """Builds the types of an argument."""
+
+        # We try to build types from the type annotation of the node.
+        types = self._build(node.annotation, ArgType)
+
+        # We do always return a tuple of types, or None.
+        if types is not None and not isinstance(types, tuple):
+            return types,
+
+        return types
+
+    def _build_arg_default(
+            self,
+            n_acc: int,
+            n_arg: int,
+            defaults: Sequence[Any],
+            ) -> Optional[str]:
+        """Builds the default value of an argument."""
+
+        n_def: int = len(defaults)
+        index: int
+
+        # If there are no default values, we move on.
+        if n_def == 0:
+            return None
+
+        # Otherwise we would be out of index for defaults.
+        if n_arg - n_def > n_acc:
+            return None
+
+        # We define the defaults index based on the current visited argument.
+        index = n_def + n_acc - n_arg
+
+        return self._build(defaults[index])
+
     def _build_returns(self, node: ast.FunctionDef) -> Tuple[RetType, ...]:
+        """Builds a return type."""
+
+        # We try to build return types from the returns of the node.
         returns = self._build(node.returns, RetType)
 
+        # We do always return a tuple of types, or None.
         if returns is not None and not isinstance(returns, tuple):
             returns = returns,
 
         return returns
 
-    def _build(self, node: Optional[ast.expr], builder = lambda x: x):
+    def _build(
+            self,
+            node: Optional[Union[ast.expr, ast.slice]],
+            builder: Callable[..., Any] = lambda x: x,
+            ) -> Any:
+        """Generic builder."""
+
         if node is None:
             return None
 
@@ -261,26 +348,39 @@ class ContractBuilder(ast.NodeVisitor):
         if isinstance(node, ast.Str):
             return builder(str(node.s))
 
+        if isinstance(node, ast.Index):
+            return self._build(node.value, builder)
+
+        # If we get something ``like[this]``, we have to recurse in order to
+        # extract the values.
         if isinstance(node, ast.Subscript):
             return (
                 self._build(node.value, builder),
-                self._build(node.slice.value, builder),
+                self._build(node.slice, builder),
                 )
 
-        if isinstance(node, (ast.List, ast.Tuple)):
+        # Similarly, if we get a sequence or collection, we have to traverse
+        # each item recursively.
+        if isinstance(node, (ast.List, ast.Set, ast.Tuple)):
+            initial: Tuple[Any, ...] = ()
+
             return functools.reduce(
                 lambda acc, item: (*acc, self._build(item, builder)),
                 node.elts,
-                (),
+                initial,
                 )
 
         raise TypeError(ast.dump(node))
 
     def _is_unique(self, name: str) -> bool:
+        """Check if a contract's name is unique or not."""
+
+        # We add a default value to avoid a StopIteration.
         is_unique: bool = not next(self._find(name), False)
+
         return is_unique
 
-    def _find(self, name: str) -> Generator:
-        for contract in self.contracts:
-            if contract.name == name:
-                yield True
+    def _find(self, name: str) -> Generator[bool, None, None]:
+        """Check if there is a contract with ``name``."""
+
+        return (True for contract in self.contracts if contract.name == name)
