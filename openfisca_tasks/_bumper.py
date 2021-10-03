b30a0e417cdd42ee8f4d13aba8228a11689bc113
Mauko Quiroga <hello@mauko.me>
2021-10-03 20:02:14 +0200

    Document bumper

--- openfisca_tasks/_bumper.py
+++ openfisca_tasks/_bumper.py
@@ -1,4 +1,4 @@
-from typing import Type, Union
+from typing import Type
 
 from openfisca_core.indexed_enums import Enum
 
@@ -8,6 +8,8 @@ TO_STR = "-", "+", "~", ""
 
 
 class Version(Enum):
+    """An enum just to determine the required version."""
+
     MAJOR = "removed"
     MINOR = "added"
     PATCH = "diff"
@@ -18,6 +20,24 @@ class Version(Enum):
 
 
 class Bumper:
+    """Determines the required version bump.
+
+    Attributes:
+        repo: To gather versions.
+        what: An ``event`` and the associated bump requirement.
+        before: The last tagged version.
+        actual: The actual version.
+        required: The required version bump.
+
+    Examples:
+        >>> bumper = Bumper()
+        >>> bumper("removed")
+        >>> bumper.required
+        <Version.MAJOR: 'removed'>
+
+    .. versionadded:: 36.1.0
+
+    """
 
     repo: Repo
     what: Type[Version]
@@ -37,6 +57,29 @@ class Bumper:
         self.required = tuple(Version)[index]
 
     def is_acceptable(self) -> bool:
+        """Determines if the current version is acceptable or not.
+
+        Returns:
+            True: When it is acceptable.
+            False: Otherwise.
+
+        Examples:
+            >>> bumper = Bumper()
+            >>> bumper("removed")
+
+            >>> bumper.actual = "1.2.3"
+            >>> bumper.before = "1.2.3"
+            >>> bumper.is_acceptable()
+            False
+
+            >>> bumper.actual = "2.0.0"
+            >>> bumper.is_acceptable()
+            True
+
+        .. versionadded:: 36.1.0
+
+        """
+
         actual: int = int(self.actual.split(".")[self.required.index])
         before: int = int(self.before.split(".")[self.required.index])
         return actual >= before + 1
