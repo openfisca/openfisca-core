f24fe53f9244d468ae21068ba2234b8ade33bfab
Mauko Quiroga <hello@mauko.me>
2021-10-07 11:09:03 +0200

    Fix make (build should be phony)

--- openfisca_tasks/publish.mk
+++ openfisca_tasks/publish.mk
@@ -1,7 +1,7 @@
 ## Install openfisca-core for deployment and publishing.
-build: setup.py
+build:
 	@## This allows us to be sure tests are run against the packaged version
 	@## of openfisca-core, the same we put in the hands of users and reusers.
 	@$(call print_help,$@:)
-	@python $? bdist_wheel
+	@python setup.py bdist_wheel
 	@find dist -name "*.whl" -exec pip install --force-reinstall {}[dev] \;
