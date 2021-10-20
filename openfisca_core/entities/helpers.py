126ea77c8ab815558b918bc89983ef1c8dfb99bb
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2021-10-20 15:51:25 +0100

    Permute argument order in build_entity
    Co-authored-by: Mauko Quiroga <maukoquiroga@gmail.com>

--- openfisca_core/entities/helpers.py
+++ openfisca_core/entities/helpers.py
@@ -1,7 +1,7 @@
 from openfisca_core import entities
 
 
-def build_entity(key, plural, label, doc = "", roles = None, is_person = False, containing_entities = (), class_override = None):
+def build_entity(key, plural, label, doc = "", roles = None, is_person = False, class_override = None, containing_entities = ()):
     if is_person:
         return entities.Entity(key, plural, label, doc)
     else:
