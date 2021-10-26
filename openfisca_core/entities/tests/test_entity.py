3f70799b6620d44eb64a3ce21b7e9e18afcaf47f
Mauko Quiroga <hello@mauko.me>
2021-10-26 11:36:26 +0200

    Add missing tests

---
+++ openfisca_core/entities/tests/test_entity.py
@@ -0,0 +1,11 @@
+from openfisca_core.entities import Entity
+
+
+def test_init_when_doc_indented():
+    """Dedents the ``doc`` attribute if it is passed at initialisation."""
+
+    key = "\tkey"
+    doc = "\tdoc"
+    entity = Entity(key, "label", "plural", doc)
+    assert entity.key == key
+    assert entity.doc != doc
---
+++ openfisca_core/entities/tests/test_group_entity.py
@@ -0,0 +1,39 @@
+import pytest
+
+from openfisca_core.entities import GroupEntity
+
+
+@pytest.fixture
+def roles():
+    """A role-like."""
+
+    return [{"key": "parent", "subroles": ["first_parent", "second_parent"]}]
+
+
+@pytest.fixture
+def group_entity(roles):
+    """A group entity."""
+
+    return GroupEntity("key", "label", "plural", "doc", roles)
+
+
+def test_init_when_doc_indented():
+    """Dedents the ``doc`` attribute if it is passed at initialisation."""
+
+    key = "\tkey"
+    doc = "\tdoc"
+    group_entity = GroupEntity(key, "label", "plural", doc, [])
+    assert group_entity.key == key
+    assert group_entity.doc != doc
+
+
+def test_group_entity_with_roles(group_entity):
+    """Assigns a :obj:`.Role` for each role-like passed as argument."""
+
+    assert group_entity.PARENT
+
+
+def test_group_entity_with_subroles(group_entity):
+    """Assigns a :obj:`.Role` for each subrole-like passed as argument."""
+
+    assert group_entity.FIRST_PARENT
