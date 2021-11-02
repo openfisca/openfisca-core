61f44e2d7f8ddb762a4cfc3dfda9c9571489914d
Hajar AIT EL KADI <48837850+HAEKADI@users.noreply.github.com>
2021-11-02 11:44:29 +0100

    Replace wheel with build
    Co-authored-by: Mauko Quiroga <mauko@pm.me>

--- openfisca_tasks/publish.mk
+++ openfisca_tasks/publish.mk
@@ -3,7 +3,7 @@ build:
 	@## This allows us to be sure tests are run against the packaged version
 	@## of openfisca-core, the same we put in the hands of users and reusers.
 	@$(call print_help,$@:)
-	@pip install --upgrade pip twine wheel
+	@pip install --upgrade pip build twine
 	@python setup.py bdist_wheel
 	@find dist -name "*.whl" -exec pip install --force-reinstall {}[dev] \;
 	@$(call print_pass,$@:)
