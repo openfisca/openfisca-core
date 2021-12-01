5770708f2695a6f2de87213993a7bcde0d66ae4c
sandcha <sandcha@users.noreply.github.com>
2021-12-01 15:11:14 +0100

    Say that openfisca-core could be run with python 3.8

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Information Analysis',
         ],
     description = 'A versatile microsimulation free software',
