2859bdbf01eb7a0a2882ffcec9fad69b7ca5657c
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2022-01-04 16:29:04 +0100

    WIP: Fix calculate(tax_benefit_system, input_data)

--- openfisca_web_api/handlers.py
+++ openfisca_web_api/handlers.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import dpath.util
-
+import copy
 from openfisca_core.simulation_builder import SimulationBuilder
 from openfisca_core.indexed_enums import Enum
 
 
 def calculate(tax_benefit_system, input_data):
     simulation = SimulationBuilder().build_from_entities(tax_benefit_system, input_data)
-
+# BCO: 
     requested_computations = dpath.util.search(input_data, '*/*/*/*', afilter = lambda t: t is None, yielded = True)
     computation_results = {}
+    #computation_results = copy.deepcopy(input_data)
 
     for computation in requested_computations:
         path = computation[0]
         entity_plural, entity_id, variable_name, period = path.split('/')
+        #if computation_results == {}:
+        #    computation_results = {entity_plural: {entity_id:{variable_name:None}}}
+        # BCO 'households/_/housing_tax/2017'
+        # variable_name =  'housing_tax'
+        # persons/Alicia/disposable_income/2017-01
         variable = tax_benefit_system.get_variable(variable_name)
         result = simulation.calculate(variable_name, period)
         population = simulation.get_population(entity_plural)
@@ -28,7 +34,6 @@ def calculate(tax_benefit_system, input_data):
             entity_result = str(result[entity_index])
         else:
             entity_result = result.tolist()[entity_index]
-
         dpath.util.new(computation_results, path, entity_result)
 
     dpath.util.merge(input_data, computation_results)
