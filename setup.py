3038c656807d4e6961e4137aa14b08a2e65b46d7
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2022-02-03 15:32:53 +0100

    long_description

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of our README file for PyPi
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 # Please make sure to cap all dependency versions, in order to avoid unwanted
 # functional and integration breaks caused by external code updates.
@@ -52,17 +57,22 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering :: Information Analysis',
         ],
     description = 'A versatile microsimulation free software',
     keywords = 'benefit microsimulation social tax',
     license = 'https://www.fsf.org/licensing/licenses/agpl-3.0.html',
+    license_files = ("LICENSE",),
     url = 'https://github.com/openfisca/openfisca-core',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
     data_files = [
         (
             'share/openfisca/openfisca-core',
-            ['CHANGELOG.md', 'LICENSE', 'README.md'],
+            ['CHANGELOG.md', 'README.md'],
             ),
         ],
     entry_points = {
