35b090fc278e2d77bc02d211b190fb53a568d77d
Max Ghenis <mghenis@gmail.com>
2022-02-21 22:37:10 -0800

    Remove Python 3.6

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: POSIX",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Information Analysis",
