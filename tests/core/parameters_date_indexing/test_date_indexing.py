6c83c963063bec52439f1fd0ab736e431f5c7a64
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2022-03-18 22:26:49 +0100

    Translate parameters into english

--- tests/core/parameters_date_indexing/test_date_indexing.py
+++ tests/core/parameters_date_indexing/test_date_indexing.py
@@ -17,22 +17,22 @@ def get_message(error):
 
 def test_on_leaf():
     parameter_at_instant = parameters.trimtp_rg('1995-01-01')
-    date_de_naissance = numpy.array(['1930-01-01', '1935-01-01', '1940-01-01', '1945-01-01'], dtype = 'datetime64[D]')
-    assert_near(parameter_at_instant.nombre_trimestres_cibles_par_generation[date_de_naissance], [150, 152, 157, 160])
+    birthdate = numpy.array(['1930-01-01', '1935-01-01', '1940-01-01', '1945-01-01'], dtype = 'datetime64[D]')
+    assert_near(parameter_at_instant.contribution_quarters_required_by_birthdate[birthdate], [150, 152, 157, 160])
 
 
 def test_on_node():
-    date_de_naissance = numpy.array(['1950-01-01', '1953-01-01', '1956-01-01', '1959-01-01'], dtype = 'datetime64[D]')
-    parameter_at_instant = parameters.aad_rg('2012-03-01')
-    node = parameter_at_instant.age_annulation_decote_en_fonction_date_naissance[date_de_naissance]
-    assert_near(node.annee, [65, 66, 67, 67])
-    assert_near(node.mois, [0, 2, 0, 0])
+    birthdate = numpy.array(['1950-01-01', '1953-01-01', '1956-01-01', '1959-01-01'], dtype = 'datetime64[D]')
+    parameter_at_instant = parameters.full_rate_age('2012-03-01')
+    node = parameter_at_instant.full_rate_age_by_birthdate[birthdate]
+    assert_near(node.year, [65, 66, 67, 67])
+    assert_near(node.month, [0, 2, 0, 0])
 
 
 # def test_inhomogenous():
-#     date_de_naissance = numpy.array(['1930-01-01', '1935-01-01', '1940-01-01', '1945-01-01'], dtype = 'datetime64[D]')
-#     parameter_at_instant = parameters.aad_rg('2011-01-01')
-#     parameter_at_instant.age_annulation_decote_en_fonction_date_naissance[date_de_naissance]
+#     birthdate = numpy.array(['1930-01-01', '1935-01-01', '1940-01-01', '1945-01-01'], dtype = 'datetime64[D]')
+#     parameter_at_instant = parameters..full_rate_age('2011-01-01')
+#     parameter_at_instant.full_rate_age_by_birthdate[birthdate]
 #     with pytest.raises(ValueError) as error:
-#         parameter_at_instant.age_annulation_decote_en_fonction_date_naissance[date_de_naissance]
-#     assert "Cannot use fancy indexing on parameter node 'aad_rg.age_annulation_decote_en_fonction_date_naissance'" in get_message(error.value)
+#         parameter_at_instant.full_rate_age_by_birthdate[birthdate]
+#     assert "Cannot use fancy indexing on parameter node '.full_rate_age.full_rate_age_by_birthdate'" in get_message(error.value)
