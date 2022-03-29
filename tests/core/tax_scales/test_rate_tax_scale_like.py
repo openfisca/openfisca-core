ff61a9f56a1f93f7815889a78ace3c4c8f6c2c44
Pierre Gobin <dev@pierregobin.fr>
2022-03-29 22:14:03 +0200

    test: move rate_from_bracket_indice and rate_from_taxe_base tests to test_marginal_rate_tax_scale

--- tests/core/tax_scales/test_rate_tax_scale_like.py
+++ tests/core/tax_scales/test_rate_tax_scale_like.py
@@ -3,34 +3,6 @@ import numpy
 from openfisca_core import taxscales
 
 
-def test_rate_from_bracket_indice():
-    tax_base = numpy.array([0, 1_000, 1_500, 50_000])
-    tax_scale = taxscales.MarginalRateTaxScale()
-    tax_scale.add_bracket(0, 0)
-    tax_scale.add_bracket(400, 0.1)
-    tax_scale.add_bracket(15_000, 0.4)
-
-    bracket_indice = tax_scale.bracket_indices(tax_base)
-    result = tax_scale.rate_from_bracket_indice(bracket_indice)
-
-    assert isinstance(result, numpy.ndarray)
-    assert (result == numpy.array([0., 0.1, 0.1, 0.4])).all()
-
-
-def test_rate_from_tax_base():
-    tax_base = numpy.array([0, 3_000, 15_500, 500_000])
-    tax_scale = taxscales.MarginalRateTaxScale()
-    tax_scale.add_bracket(0, 0)
-    tax_scale.add_bracket(400, 0.1)
-    tax_scale.add_bracket(15_000, 0.4)
-    tax_scale.add_bracket(200_000, 0.6)
-
-    result = tax_scale.rate_from_tax_base(tax_base)
-
-    assert isinstance(result, numpy.ndarray)
-    assert (result == numpy.array([0., 0.1, 0.4, 0.6])).all()
-
-
 def test_threshold_from_tax_base():
     tax_base = numpy.array([0, 33_000, 500, 400_000])
     tax_scale = taxscales.LinearAverageRateTaxScale()
