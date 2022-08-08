7afae6cf64714348b27d7a91c26fd7252c6208e8
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2022-08-08 11:11:53 +0200

    Introduce error margin by variable in tests

--- openfisca_core/tools/test_runner.py
+++ openfisca_core/tools/test_runner.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import collections
 import warnings
 import sys
 import os
@@ -214,14 +215,28 @@ class YamlItem(pytest.Item):
 
         actual_value = self.simulation.calculate(variable_name, period)
 
+        absolute_error_margin = self.test.get('absolute_error_margin')
+        variable_absolute_error_margin = (
+            absolute_error_margin.get(variable_name, absolute_error_margin.get("default"))
+            if isinstance(absolute_error_margin, collections.Mapping)
+            else absolute_error_margin
+            )
+
+        relative_error_margin = self.test.get('relative_error_margin')
+        variable_relative_error_margin = (
+            relative_error_margin.get(variable_name, relative_error_margin.get("default"))
+            if isinstance(relative_error_margin, collections.Mapping)
+            else relative_error_margin
+            )
+
         if entity_index is not None:
             actual_value = actual_value[entity_index]
         return assert_near(
             actual_value,
             expected_value,
-            absolute_error_margin = self.test.get('absolute_error_margin'),
+            absolute_error_margin = variable_absolute_error_margin,
             message = f"{variable_name}@{period}: ",
-            relative_error_margin = self.test.get('relative_error_margin'),
+            relative_error_margin = variable_relative_error_margin,
             )
 
     def should_ignore_variable(self, variable_name: str):
