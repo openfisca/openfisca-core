04e544f59f0bbedd9bce064016be54e3fd2f5b9a
Mauko Quiroga <public@mauko.me>
2022-09-13 08:31:44 +0200

    Fix error message

--- openfisca_core/scripts/migrations/v24_to_25.py
+++ openfisca_core/scripts/migrations/v24_to_25.py
@@ -11,7 +11,7 @@ try:
 
 except ImportError:
     message = [
-        "You tried to use the 'ruamel' package, or it is not",
+        "You tried to use the 'ruamel' package, but it is not",
         "currently installed. Try running `pip install --user ruamel`.",
         ]
     warnings.warn(" ".join(message), UserWarning)
