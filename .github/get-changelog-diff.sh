087430387bcc82c6cc6319af995335eb8bef4753
Nikhil Woodruff <nikhil@policyengine.org>
2022-10-02 16:38:38 +0100

    Fix version check action

--- .github/get-changelog-diff.sh
+++ .github/get-changelog-diff.sh
@@ -1,4 +1,4 @@
-git remote add upstream https://github.com/policyengine/openfisca-us
+git remote add upstream https://github.com/policyengine/policyengine-core
 git fetch --tags upstream
 last_tagged_commit=`git describe --tags --abbrev=0 --first-parent`
 git --no-pager diff $last_tagged_commit -- CHANGELOG.md
