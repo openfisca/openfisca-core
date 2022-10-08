d815a23df1ed35d3ff0cf45ad67cbb168114eb6e
Nikhil Woodruff <nikhil@policyengine.org>
2022-10-09 00:36:45 +0100

    Add errors

--- policyengine_core/errors/__init__.py
+++ policyengine_core/errors/__init__.py
@@ -1,26 +1,3 @@
-# Transitional imports to ensure non-breaking changes.
-# Could be deprecated in the next major release.
-#
-# How imports are being used today:
-#
-#   >>> from policyengine_core.module import symbol
-#
-# The previous example provokes cyclic dependency problems
-# that prevent us from modularizing the different components
-# of the library so to make them easier to test and to maintain.
-#
-# How could them be used after the next major release:
-#
-#   >>> from policyengine_core import module
-#   >>> module.symbol()
-#
-# And for classes:
-#
-#   >>> from policyengine_core.module import Symbol
-#   >>> Symbol()
-#
-# See: https://www.python.org/dev/peps/pep-0008/#imports
-
 from .cycle_error import CycleError
 from .empty_argument_error import EmptyArgumentError
 from .nan_creation_error import NaNCreationError
@@ -32,7 +9,7 @@ from .parameter_parsing_error import ParameterParsingError
 from .period_mismatch_error import PeriodMismatchError
 from .situation_parsing_error import SituationParsingError
 from .spiral_error import SpiralError
-from .variable_name_config_error import (
+from .variable_name_conflict_error import (
     VariableNameConflictError,
     VariableNameConflictError as VariableNameConflict,
 )
