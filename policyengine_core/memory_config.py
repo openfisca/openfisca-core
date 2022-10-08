3506d6272778cdcad725a95f7ee5dad64350c61f
Nikhil Woodruff <nikhil@policyengine.org>
2022-10-08 21:06:33 +0100

    Remove deprecated modules

--- policyengine_core/memory_config.py
+++
@@ -1,9 +0,0 @@
-# The memory config module has been deprecated since X.X.X,
-# and will be removed in the future.
-#
-# Module's contents have been moved to the experimental module.
-#
-# The following are transitional imports to ensure non-breaking changes.
-# Could be deprecated in the next major release.
-
-from policyengine_core.experimental import MemoryConfig  # noqa: F401
