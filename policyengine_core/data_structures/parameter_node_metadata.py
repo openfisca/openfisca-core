20cfcc892e383964ed4c96991676bd473b2bcafa
Nikhil Woodruff <nikhil@policyengine.org>
2022-10-13 22:22:21 +0100

    Add breakdown examples

--- policyengine_core/data_structures/parameter_node_metadata.py
+++ policyengine_core/data_structures/parameter_node_metadata.py
@@ -15,4 +15,16 @@ class ParameterNodeMetadata:
     """A list of references that apply to all descendants of this node."""
 
     breakdown: List[str]
-    """The sets defining the children, grandchildren and further descendants of this node."""
+    """The sets defining the children, grandchildren and further descendants of this node.
+
+    For example:
+
+    .. code-block:: yaml
+
+        metadata:
+            breakdown:
+                - region # If `region` is an Enum-type variable with possible values `[ENGLAND, WALES]` then these children will be added.
+                - range(1, 7) # This code is `eval`uated to produce the list `[1, 2, 3, 4, 5, 6]` which are then added as grandchildren.
+                - [True, False] # This list is added as great-grandchildren (using the same `eval` method as above).
+    
+    """
