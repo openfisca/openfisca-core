3f3808df9e080edf075c0d4bbd08b91b54164a2c
Nikhil Woodruff <nikhil@policyengine.org>
2022-10-14 00:10:59 +0100

    Repair data CLI

--- policyengine_core/scripts/run_data.py
+++ policyengine_core/scripts/run_data.py
@@ -1,8 +1,21 @@
 import importlib
 from argparse import ArgumentParser
-
+from typing import List
+from policyengine_core.data.dataset import Dataset
 from policyengine_core.scripts import detect_country_package
+import pandas as pd
 
+def dataset_summary(datasets: List[Dataset]) -> str:
+    years = list(sorted(list(set(sum([ds.years for ds in datasets], [])))))
+    df = pd.DataFrame(
+        {
+            year: ["✓" if year in ds.years else "" for ds in datasets]
+            for year in years
+        },
+        index=[ds.name for ds in datasets],
+    )
+    df = df.sort_values(by=list(df.columns[::-1]), ascending=False)
+    return df.to_markdown(tablefmt="pretty")
 
 def main(parser: ArgumentParser):
     # Get arguments as well as kwargs
@@ -15,19 +28,15 @@ def main(parser: ArgumentParser):
     if country_package is None:
         country_package = detect_country_package()
 
-    datasets = importlib.import_module(country_package).DATASETS
+    datasets: List[Dataset] = importlib.import_module(country_package).DATASETS
 
     if args.dataset == "datasets" and args.action == "list":
-        print(f"Available datasets for {country_package}:")
-        for dataset in datasets:
-            print(
-                f" - {dataset.name} ({len(dataset().get_saved_datasets())} saved versions)"
-            )
+        print(dataset_summary(datasets))
         return
     dataset_by_name = {dataset.name: dataset for dataset in datasets}
-    dataset = dataset_by_name[args.dataset]()
+    dataset = dataset_by_name[args.dataset]
 
-    if args.action == "build":
+    if args.action == "generate":
         dataset.build(kwargs)
     elif args.action == "download":
         dataset.download(kwargs)
@@ -36,22 +45,17 @@ def main(parser: ArgumentParser):
     elif args.action == "remove":
         dataset.remove(kwargs)
     elif args.action == "list":
-        saved_datasets = dataset.get_saved_datasets()
-        if len(saved_datasets) == 0:
+        years = dataset.years
+        if len(years) == 0:
             print("No saved datasets.")
         else:
             print("Saved datasets:")
-            for saved_dataset in saved_datasets:
-                filepath = dataset.get_file_path(saved_dataset)
-                config_str = ", ".join(
-                    [f"{k}={v}" for k, v in saved_dataset.items()]
-                )
+            for year in years:
+                filepath = dataset.file(year).absolute()
                 print(
                     "  * "
                     + filepath.name
                     + "  | "
-                    + config_str
-                    + "  |  "
                     + str(filepath.absolute())
                 )
     else:
