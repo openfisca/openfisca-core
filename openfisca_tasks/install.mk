cf6f5b15de485daf7fa9ced346ae7ec73d8240d3
Mauko Quiroga <public@mauko.me>
2022-12-01 21:30:24 +0100

    Fix dependency conflict

--- openfisca_tasks/install.mk
+++ openfisca_tasks/install.mk
@@ -6,7 +6,7 @@ uninstall:
 ## Install project's overall dependencies
 install-deps:
 	@$(call print_help,$@:)
-	@pip install --upgrade pip twine wheel coveralls
+	@pip install --upgrade pip wheel
 
 ## Install project's development dependencies.
 install-edit:
