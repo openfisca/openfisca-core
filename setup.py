26c2307699e4043f4c4216672580a7884d1e459e
Mauko Quiroga <public@mauko.me>
2022-12-01 18:59:01 +0100

    Drop support for 3.8

--- setup.py
+++ setup.py
@@ -74,7 +74,6 @@ setup(
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: POSIX',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
