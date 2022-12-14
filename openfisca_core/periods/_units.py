7a11ece47d6419f44f67bf4ecab164ddaba917bc
Mauko Quiroga <public@mauko.me>
2022-12-14 12:23:32 +0100

    Simplify dict

--- openfisca_core/periods/_units.py
+++ openfisca_core/periods/_units.py
@@ -2,10 +2,4 @@ DAY = "day"
 MONTH = "month"
 YEAR = "year"
 ETERNITY = "eternity"
-
-UNIT_WEIGHTS = {
-    DAY: 100,
-    MONTH: 200,
-    YEAR: 300,
-    ETERNITY: 400,
-    }
+UNIT_WEIGHTS = {DAY: 100, MONTH: 200, YEAR: 300, ETERNITY: 400}
