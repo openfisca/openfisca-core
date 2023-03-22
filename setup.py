ac20442977622c2807e5ffacd92f6bfbd7432267
Emmanuel Raviart <emmanuel@raviart.com>
2023-03-22 15:19:18 +0100

    Remove Python 3.8

--- setup.py
+++ setup.py
@@ -73,7 +73,6 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: POSIX",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
