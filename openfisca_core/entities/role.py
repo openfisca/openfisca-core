56af1b379ba9cf768c785d4f332cfe58bbf74579
Mauko Quiroga <public@mauko.me>
2023-06-27 04:00:13 +0200

    Add role description

--- openfisca_core/entities/role.py
+++ openfisca_core/entities/role.py
@@ -1,7 +1,19 @@
+from __future__ import annotations
+
+import dataclasses
 import textwrap
 
 
 class Role:
+    """The role of an Entity within a GroupEntity.
+
+    Each Entity related to a GroupEntity has a Role. For example, if you have
+    a family, its roles could include a parent, a child, and so on. Or if you
+    have a tax household, its roles could include the taxpayer, a spouse,
+    several dependents, and the like.
+
+    """
+
     def __init__(self, description, entity):
         self.entity = entity
         self.key = description["key"]
@@ -11,5 +23,55 @@ class Role:
         self.max = description.get("max")
         self.subroles = None
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "Role({})".format(self.key)
+
+
+@dataclasses.dataclass(frozen=True)
+class RoleDescription:
+    """A Role's description.
+
+    Examples:
+        >>> description = {
+        ...     "key": "parent",
+        ...     "label": "Parents",
+        ...     "plural": "parents",
+        ...     "doc": "\t\t\tThe one/two adults in charge of the household.",
+        ...     "max": 2,
+        ... }
+
+        >>> role_description = RoleDescription(**description)
+
+        >>> repr(RoleDescription)
+        "<class 'openfisca_core.entities.role.RoleDescription'>"
+
+        >>> repr(role_description)
+        "RoleDescription(key='parent', plural='parents', label='Parents', ...)"
+
+        >>> str(role_description)
+        "RoleDescription(key='parent', plural='parents', label='Parents', ...)"
+
+        >>> role_description.key
+        'parent'
+
+    .. versionadded:: 40.1.0
+
+    """
+
+    #: A key to identify the Role.
+    key: str
+
+    #: The ``key``, pluralised.
+    plural: str | None
+
+    #: A summary description.
+    label: str | None
+
+    #: A full description, dedented.
+    doc: str = ""
+
+    #: Max number of members.
+    max: int | None = None
+
+    def __post_init__(self) -> None:
+        object.__setattr__(self, "doc", textwrap.dedent(self.doc))
