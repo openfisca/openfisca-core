649248db34d61e49555b083e7de0a25f0fa3ba88
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2023-08-17 11:10:35 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ dev_requirements = [
 
 setup(
     name="OpenFisca-Core",
-    version="41.0.0",
+    version="41.1.0",
     author="OpenFisca Team",
     author_email="contact@openfisca.org",
     classifiers=[
