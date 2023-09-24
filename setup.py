60ebe4d9038f3a63b83ab4ca09cb256ff9377944
Mauko Quiroga <public@mauko.me>
2023-09-25 01:23:09 +0200

    Update setup.py & setup.cfg

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ general_requirements = [
     'importlib-metadata < 4.3.0',  # Required for Python 3.7 and Flake8
     'nptyping == 1.4.4',
     'numexpr >= 2.7.0, <= 3.0',
-    'numpy >= 1.11, < 1.21',
+    "numpy >= 1.24.2, <1.25",
     'pendulum >= 2.1.2, < 3.0.0',
     'psutil >= 5.4.7, < 6.0.0',
     'pytest >= 4.4.1, < 6.0.0',  # For openfisca test
@@ -41,16 +41,15 @@ general_requirements = [
     ]
 
 api_requirements = [
-    'markupsafe == 2.0.1',  # While flask revision < 2
-    'flask == 1.1.4',
+    "flask >=2.2.3, < 3.0",
     'flask-cors == 3.0.10',
     'gunicorn >= 20.0.0, < 21.0.0',
-    'werkzeug >= 1.0.0, < 2.0.0',
+    "werkzeug >=2.2.3, < 3.0",
     ]
 
 dev_requirements = [
-    'idna >= 3.4.0, < 4.0.0',
     'autopep8 >= 1.4.0, < 1.6.0',
+    "black >=23.1.0, < 24.0",
     'coverage >= 6.0.2, < 7.0.0',
     'darglint == 1.8.0',
     'flake8 >= 4.0.0, < 4.1.0',
@@ -58,11 +57,13 @@ dev_requirements = [
     'flake8-docstrings == 1.6.0',
     'flake8-print >= 3.1.0, < 4.0.0',
     'flake8-rst-docstrings == 0.2.3',
+    'idna >= 3.4.0, < 4.0.0',
     'isort >= 5.0.0, < 6.0.0',
     'mypy == 0.910',
     'openapi-spec-validator >= 0.5.0, < 0.6.0',
     'pycodestyle >= 2.8.0, < 2.9.0',
     'pylint == 2.10.2',
+    "pyupgrade >= 3.13.0, < 4.0.0",
     'xdoctest >= 1.0.0, < 2.0.0',
     ] + api_requirements
 
@@ -76,10 +77,9 @@ setup(
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: POSIX',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Scientific/Engineering :: Information Analysis',
         ],
     description = 'A versatile microsimulation free software',
