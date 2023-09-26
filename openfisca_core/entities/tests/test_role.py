c8331465c081b3cecf34e1e47b159cb9738b2956
Mauko Quiroga <public@mauko.me>
2023-09-26 23:21:16 +0200

    Add test to role

---
+++ openfisca_core/entities/tests/test_role.py
@@ -0,0 +1,11 @@
+from openfisca_core import entities
+
+
+def test_init_when_doc_indented():
+    """De-indents the ``doc`` attribute if it is passed at initialisation."""
+
+    key = "\tkey"
+    doc = "\tdoc"
+    role = entities.Role({"key": key, "doc": doc}, object())
+    assert role.key == key
+    assert role.doc != doc
