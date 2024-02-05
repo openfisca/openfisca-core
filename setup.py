b093bd9b53f30792cfa715cfc19d46d875e4d11a
Thomas Guillet <thomas@palya.fr>
2024-02-05 11:18:54 +0100

    Update changelog and version

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ dev_requirements = [
 
 setup(
     name="OpenFisca-Core",
-    version="41.4.2",
+    version="41.5.0",
     author="OpenFisca Team",
     author_email="contact@openfisca.org",
     classifiers=[
