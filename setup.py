c837aef035755e70224327f4376b6c894667713f
Matti Schneider <matti@openfisca.org>
2024-05-02 10:37:52 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ dev_requirements = [
 
 setup(
     name="OpenFisca-Core",
-    version="41.4.3",
+    version="41.4.4",
     author="OpenFisca Team",
     author_email="contact@openfisca.org",
     classifiers=[
