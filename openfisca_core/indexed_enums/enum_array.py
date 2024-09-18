96800b8e04b7948bddd4b577e4579755acbafc25
Mauko Quiroga <public@mauko.me>
2024-09-19 01:42:08 +0200

    fix(enums): handle complex type override

--- openfisca_core/indexed_enums/enum_array.py
+++ openfisca_core/indexed_enums/enum_array.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
-from typing import Any, NoReturn
+from typing import NoReturn, overload
+from typing_extensions import TypeGuard
 
 import numpy
 
 from . import types as t
 
 
-class EnumArray(t.Array[t.ArrayEnum]):
+class EnumArray(t.EnumArray):
     """
     NumPy array subclass representing an array of enum items.
 
@@ -19,57 +20,84 @@ class EnumArray(t.Array[t.ArrayEnum]):
     # https://docs.scipy.org/doc/numpy-1.13.0/user/basics.subclassing.html#slightly-more-realistic-example-attribute-added-to-existing-array.
 
     #: Enum type of the array items.
-    possible_values: type[t.Enum] | None = None
+    possible_values: None | type[t.Enum] = None
 
     def __new__(
         cls,
         input_array: t.Array[t.ArrayEnum],
-        possible_values: type[t.Enum] | None = None,
+        possible_values: None | type[t.Enum] = None,
     ) -> EnumArray:
         obj = numpy.asarray(input_array).view(cls)
         obj.possible_values = possible_values
         return obj
 
     # See previous comment
-    def __array_finalize__(self, obj: t.Array[t.ArrayAny] | t.EnumArray | None) -> None:
+    def __array_finalize__(self, obj: None | t.EnumArray | t.Array[t.ArrayAny]) -> None:
         if obj is None:
             return None
         if isinstance(obj, EnumArray):
             self.possible_values = obj.possible_values
         return None
 
-    def __eq__(self, other: object) -> t.Array[t.ArrayBool] | bool:  # type: ignore[override]
+    @overload  # type: ignore[override]
+    def __eq__(self, other: None | t.Enum | type[t.Enum]) -> t.Array[t.ArrayBool]:
+        ...
+
+    @overload
+    def __eq__(self, other: object) -> t.Array[t.ArrayBool] | bool:
+        ...
+
+    def __eq__(self, other: object) -> t.Array[t.ArrayBool] | bool:
+        boolean_array: t.Array[t.ArrayBool]
+        boolean: bool
+
         if self.possible_values is None:
-            raise NotImplementedError
+            return NotImplemented
+
+        view: t.Array[t.ArrayEnum] = self.view(numpy.ndarray)
+
+        if other is None or self._is_an_enum_type(other):
+            boolean_array = view == other
+            return boolean_array
 
         # When comparing to an item of self.possible_values, use the item index
         # to speed up the comparison.
-        if other.__class__.__name__ is self.possible_values.__name__:
+        if self._is_an_enum(other):
             # Use view(ndarray) so that the result is a classic ndarray, not an
             # EnumArray.
-            return self.view(numpy.ndarray) == other.index
+            boolean_array = view == other.index
+            return boolean_array
 
-        return self.view(numpy.ndarray) == other
+        boolean = view == other
+        return boolean
 
-    def __ne__(self, other: Any) -> bool:
+    @overload  # type: ignore[override]
+    def __ne__(self, other: None | t.Enum | type[t.Enum]) -> t.Array[t.ArrayBool]:
+        ...
+
+    @overload
+    def __ne__(self, other: object) -> t.Array[t.ArrayBool] | bool:
+        ...
+
+    def __ne__(self, other: object) -> t.Array[t.ArrayBool] | bool:
         return numpy.logical_not(self == other)
 
-    def _forbidden_operation(self, other: Any) -> NoReturn:
+    def _forbidden_operation(self, other: object) -> NoReturn:
         raise TypeError(
             "Forbidden operation. The only operations allowed on EnumArrays "
             "are '==' and '!='.",
         )
 
-    __add__ = _forbidden_operation
-    __mul__ = _forbidden_operation
-    __lt__ = _forbidden_operation
-    __le__ = _forbidden_operation
-    __gt__ = _forbidden_operation
-    __ge__ = _forbidden_operation
-    __and__ = _forbidden_operation
-    __or__ = _forbidden_operation
+    __add__ = _forbidden_operation  # type: ignore[assignment]
+    __mul__ = _forbidden_operation  # type: ignore[assignment]
+    __lt__ = _forbidden_operation  # type: ignore[assignment]
+    __le__ = _forbidden_operation  # type: ignore[assignment]
+    __gt__ = _forbidden_operation  # type: ignore[assignment]
+    __ge__ = _forbidden_operation  # type: ignore[assignment]
+    __and__ = _forbidden_operation  # type: ignore[assignment]
+    __or__ = _forbidden_operation  # type: ignore[assignment]
 
-    def decode(self) -> numpy.object_:
+    def decode(self) -> t.Array[t.ArrayEnum]:
         """
         Return the array of enum items corresponding to self.
 
@@ -88,7 +116,7 @@ class EnumArray(t.Array[t.ArrayEnum]):
             list(self.possible_values),
         )
 
-    def decode_to_str(self) -> numpy.str_:
+    def decode_to_str(self) -> t.Array[t.ArrayStr]:
         """
         Return the array of string identifiers corresponding to self.
 
@@ -110,3 +138,15 @@ class EnumArray(t.Array[t.ArrayEnum]):
 
     def __str__(self) -> str:
         return str(self.decode_to_str())
+
+    def _is_an_enum(self, other: object) -> TypeGuard[t.Enum]:
+        return (
+            not hasattr(other, "__name__")
+            and other.__class__.__name__ is self.possible_values.__name__
+        )
+
+    def _is_an_enum_type(self, other: object) -> TypeGuard[type[t.Enum]]:
+        return (
+            hasattr(other, "__name__")
+            and other.__name__ is self.possible_values.__name__
+        )
--- openfisca_core/indexed_enums/types.py
+++ openfisca_core/indexed_enums/types.py
@@ -1,34 +1,27 @@
+from __future__ import annotations
+
 from typing import Protocol
 
-from openfisca_core.types import (
-    Array,
-    ArrayAny,
-    ArrayBool,
-    ArrayBytes,
-    ArrayEnum,
-    ArrayInt,
-    ArrayStr,
-)
+from openfisca_core.types import Array as Array
+from openfisca_core.types import ArrayAny as ArrayAny  # noqa: F401
+from openfisca_core.types import ArrayBool as ArrayBool  # noqa: F401
+from openfisca_core.types import ArrayBytes as ArrayBytes  # noqa: F401
+from openfisca_core.types import ArrayEnum as ArrayEnum
+from openfisca_core.types import ArrayInt as ArrayInt  # noqa: F401
+from openfisca_core.types import ArrayStr as ArrayStr  # noqa: F401
+
+import abc
 
 # Indexed enums
 
 
 class Enum(Protocol):
+    index: int
+
     @classmethod
     def encode(cls, array: object) -> object:
         ...
 
 
-class EnumArray(Protocol):
+class EnumArray(Array[ArrayEnum], metaclass=abc.ABCMeta):
     ...
-
-
-__all__ = [
-    "Array",
-    "ArrayAny",
-    "ArrayBool",
-    "ArrayEnum",
-    "ArrayStr",
-    "ArrayBytes",
-    "ArrayInt",
-]
