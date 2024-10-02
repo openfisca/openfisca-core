a3e49ed884449bf465b1c656189c1e8cda7aeae4
Mauko Quiroga <public@mauko.me>
2024-10-02 16:40:45 +0200

    ci: add matrix

--- openfisca_tasks/install.mk
+++ openfisca_tasks/install.mk
@@ -1,20 +1,21 @@
 ## Uninstall project's dependencies.
 uninstall:
 	@$(call print_help,$@:)
-	@pip freeze | grep -v "^-e" | sed "s/@.*//" | xargs pip uninstall -y
+	@python -m pip freeze | grep -v "^-e" | sed "s/@.*//" | xargs pip uninstall -y
 
 ## Install project's overall dependencies
 install-deps:
 	@$(call print_help,$@:)
-	@pip install --upgrade pip
+	@python -m pip install --upgrade pip
 
 ## Install project's development dependencies.
 install-edit:
 	@$(call print_help,$@:)
-	@pip install --upgrade --editable ".[dev]"
+	@python -m pip install --upgrade --editable ".[dev]"
 
 ## Delete builds and compiled python files.
 clean:
 	@$(call print_help,$@:)
 	@ls -d * | grep "build\|dist" | xargs rm -rf
+	@find . -name "__pycache__" | xargs rm -rf
 	@find . -name "*.pyc" | xargs rm -rf
