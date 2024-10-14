abeea2fa90fa481de27dcca42465d0be3eb85edf
Mauko Quiroga <public@mauko.me>
2024-10-14 17:38:11 +0200

    test: check period (#1238 #1276 #1277)

--- openfisca_core/populations/_errors.py
+++ openfisca_core/populations/_errors.py
@@ -12,4 +12,26 @@ class InvalidArraySizeError(ValueError):
         super().__init__(msg)
 
 
-__all__ = ["InvalidArraySizeError"]
+class PeriodValidityError(ValueError):
+    """Raised when a period is not valid."""
+
+    def __init__(
+        self,
+        variable_name: t.VariableName,
+        filename: str,
+        line_number: int,
+        line_of_code: int,
+    ) -> None:
+        msg = (
+            f'You requested computation of variable "{variable_name}", but '
+            f'you did not specify on which period in "{filename}:{line_number}": '
+            f"{line_of_code}. When you request the computation of a variable "
+            "within a formula, you must always specify the period as the second "
+            'parameter. The convention is to call this parameter "period". For '
+            'example: computed_salary = person("salary", period). More information at '
+            "<https://openfisca.org/doc/coding-the-legislation/35_periods.html#periods-in-variable-definition>."
+        )
+        super().__init__(msg)
+
+
+__all__ = ["InvalidArraySizeError", "PeriodValidityError"]
