1ebab94de77b2acdc5b7bf8071518c0f562603ee
benoit-cty <benoit-cty@example.com>
2024-11-05 09:19:58 +0100

    Update python version to 3.12.3

--- setup.py
+++ setup.py
@@ -81,6 +81,8 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Scientific/Engineering :: Information Analysis",
     ],
     description="A versatile microsimulation free software",
