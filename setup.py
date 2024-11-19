3ca06c31bb6aeebee84fb329ca03d627829d04fb
Mauko Quiroga <public@mauko.me>
2024-11-19 09:53:23 +0100

    chore: version bump (fixes #1291)

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ dev_requirements = [
 
 setup(
     name="OpenFisca-Core",
-    version="43.2.4",
+    version="43.2.5",
     author="OpenFisca Team",
     author_email="contact@openfisca.org",
     classifiers=[
