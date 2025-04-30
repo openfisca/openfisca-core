63df9f3b71d9de1e08d0d3199bbef2ebd8b8aa71
benoit-cty <4-benoit-cty@users.noreply.git.leximpact.dev>
2025-04-30 07:59:47 +0200

    Rollback publish.mk

--- tasks/publish.mk
+++ tasks/publish.mk
@@ -3,7 +3,7 @@
 ## Install project's build dependencies.
 install-dist:
 	@$(call print_help,$@:)
-	@python -m pip install .[ci,dev] build
+	@python -m pip install .[ci,dev]
 	@$(call print_pass,$@:)
 
 ## Build & install openfisca-core for deployment and publishing.
@@ -11,9 +11,6 @@ build:
 	@## This allows us to be sure tests are run against the packaged version
 	@## of openfisca-core, the same we put in the hands of users and reusers.
 	@$(call print_help,$@:)
-	@python -m pip install --upgrade pip
-	@python -m pip install build
-	@python -m pip list | grep build
 	@python -m build
 	@python -m pip uninstall --yes openfisca-core
 	@find dist -name "*.whl" -exec python -m pip install --no-deps {} \;
