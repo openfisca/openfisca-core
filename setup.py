1e4c3f58c27b1e07928357d4a0b70613dd23a61a
benoit-cty <benoit-cty@leximpact.dev>
2025-07-11 14:48:23 +0200

    bump

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ dev_requirements = [
 
 setup(
     name="OpenFisca-Core",
-    version="43.3.8",
+    version="43.3.9",
     author="OpenFisca Team",
     author_email="contact@openfisca.org",
     classifiers=[
