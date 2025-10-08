43a2e97e20b6c1df96eeb17c2db720b2916f1b64
Thomas Guillet <thomas@palya.fr>
2025-10-08 22:40:07 +0200

    Lint

--- tests/core/test_commons.py
+++ tests/core/test_commons.py
@@ -4,12 +4,12 @@ from openfisca_core.commons import eval_expression
 
 
 def test_sum() -> None:
-    assert (eval_expression("1 + 2") == 3)
+    assert eval_expression("1 + 2") == 3
 
 
 def test_string() -> None:
-    assert (eval_expression("salary") == "salary")
+    assert eval_expression("salary") == "salary"
 
 
 def test_string_with_space() -> None:
-    assert (eval_expression("date of birth") == "date of birth")
+    assert eval_expression("date of birth") == "date of birth"
