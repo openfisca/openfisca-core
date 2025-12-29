29d0cbae81c3ebb507520849cc6b81473170f748
benoit-cty <anne@onyme.fr>
2025-12-29 18:08:53 +0100

    bump

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ dev_requirements = [
 
 setup(
     name="OpenFisca-Core",
-    version="44.0.2",
+    version="44.0.3",
     author="OpenFisca Team",
     author_email="contact@openfisca.org",
     classifiers=[
