29c4cca17c036a466da47ed3eead3f3a89574690
Thomas Guillet <thomas@palya.fr>
2026-09-23 12:59:56 +0200

    Fix PT031 errors

--- tests/core/tax_scales/test_abstract_rate_tax_scale.py
+++ tests/core/tax_scales/test_abstract_rate_tax_scale.py
@@ -6,4 +6,4 @@ from openfisca_core import taxscales
 def test_abstract_tax_scale() -> None:
     with pytest.warns(DeprecationWarning):
         result = taxscales.AbstractRateTaxScale()
-        assert isinstance(result, taxscales.AbstractRateTaxScale)
+    assert isinstance(result, taxscales.AbstractRateTaxScale)
